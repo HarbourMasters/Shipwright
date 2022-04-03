b1e70ded80eeb05e8b42b9b2e6a8f2b42f6ba659
Josh Bodner <jbodner09@outlook.com>
2022-04-03 16:15:41 -0700

    Fixing rebase conflict

--- libultraship/libultraship/SohImGuiImpl.cpp
+++ libultraship/libultraship/SohImGuiImpl.cpp
@@ -353,6 +353,11 @@ namespace SohImGui {
                     needs_save = true;
                 }
 
+                if (ImGui::Checkbox("DPad Support in Ocarina and Text Choice", &Game::Settings.controller.dpad_ocarina_text)) {
+                    CVar_SetS32(const_cast<char*>("gDpadOcarinaText"), Game::Settings.controller.dpad_ocarina_text);
+                    needs_save = true;
+                }
+
                 ImGui::EndMenu();
             }
 
