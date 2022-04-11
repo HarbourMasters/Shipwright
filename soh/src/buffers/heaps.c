beb454d0002c9e6ef5c7f708ae7f088c15aa59eb
GaryOderNichts <12049776+GaryOderNichts@users.noreply.github.com>
2022-04-11 19:46:42 +0200

    Formatting fixes

--- soh/src/buffers/heaps.c
+++ soh/src/buffers/heaps.c
@@ -10,22 +10,21 @@
 #define SYSTEM_HEAP_SIZE (1024 * 1024 * 128)
 
 u8* gAudioHeap;
-
 u8* gSystemHeap;
 
 void Heaps_Alloc(void)
 {
 #ifdef _MSC_VER
-    gAudioHeap = (u8*)_aligned_malloc(AUDIO_HEAP_SIZE, 16);
-    gSystemHeap = (u8*)_aligned_malloc(SYSTEM_HEAP_SIZE, 16);
+    gAudioHeap = (u8*)_aligned_malloc(AUDIO_HEAP_SIZE, 0x10);
+    gSystemHeap = (u8*)_aligned_malloc(SYSTEM_HEAP_SIZE, 0x10);
 #elif defined(_POSIX_VERSION) && (_POSIX_VERSION >= 200112L)
-    if (posix_memalign((void**)&gAudioHeap, 16, AUDIO_HEAP_SIZE) != 0)
+    if (posix_memalign((void**)&gAudioHeap, 0x10, AUDIO_HEAP_SIZE) != 0)
         gAudioHeap = NULL;
-    if (posix_memalign((void**)&gSystemHeap, 16, SYSTEM_HEAP_SIZE) != 0)
+    if (posix_memalign((void**)&gSystemHeap, 0x10, SYSTEM_HEAP_SIZE) != 0)
         gSystemHeap = NULL;
 #else
-    gAudioHeap = (u8*)memalign(16, AUDIO_HEAP_SIZE);
-    gSystemHeap = (u8*)memalign(16, SYSTEM_HEAP_SIZE);
+    gAudioHeap = (u8*)memalign(0x10, AUDIO_HEAP_SIZE);
+    gSystemHeap = (u8*)memalign(0x10, SYSTEM_HEAP_SIZE);
 #endif
 
     assert(gAudioHeap != NULL);
