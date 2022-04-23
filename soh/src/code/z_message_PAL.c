0b65405c613cb6607e8013b7139fcff338e48dab
vaguerant <vaguerant@users.noreply.github.com>
2022-04-23 16:31:02 +1000

    Credits message order fix by rink

--- soh/src/code/z_message_PAL.c
+++ soh/src/code/z_message_PAL.c
@@ -390,7 +390,7 @@ void Message_FindCreditsMessage(GlobalContext* globalCtx, u16 textId) {
         if (messageTableEntry->textId == textId) {
             foundSeg = messageTableEntry->segment;
             font->charTexBuf[0] = messageTableEntry->typePos;
-            messageTableEntry++;
+            //messageTableEntry++;
             nextSeg = messageTableEntry->segment;
             font->msgOffset = messageTableEntry->segment;
             font->msgLength = messageTableEntry->msgSize;
