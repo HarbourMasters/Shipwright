c6200572579c0053abfc0d8644fce65c747ec134
Baoulettes <perlouzerie@hotmail.fr>
2022-04-27 03:12:07 +0200

    Register the Cvar

--- soh/soh/Enhancements/bootcommands.c
+++ soh/soh/Enhancements/bootcommands.c
@@ -29,6 +29,7 @@ void BootCommands_Init()
     CVar_RegisterS32("gUniformLR", 1);
     CVar_RegisterS32("gNewDrops", 0);
     CVar_RegisterS32("gVisualAgony", 0);
+    CVar_RegisterS32("gLanguages", 0); //0 = English / 1 = German / 2 = French
 }
 
 //void BootCommands_ParseBootArgs(char* str)
