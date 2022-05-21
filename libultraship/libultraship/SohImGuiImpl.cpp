d1357b902d6a9a1038e6e4b572ed0824a93592f4
Baoulettes <perlouzerie@hotmail.fr>
2022-05-21 19:32:21 +0200

    DEV Tool : Fast Save Loading (#369)
    * Add the whole loading system there
    
    * Add Cvar checkbox
    
    * Add support for ID:0 = FileSelect
    
    * Add 0 in range for slider.

--- libultraship/libultraship/SohImGuiImpl.cpp
+++ libultraship/libultraship/SohImGuiImpl.cpp
@@ -817,7 +817,12 @@ namespace SohImGui {
             {
                 EnhancementCheckbox("OoT Debug Mode", "gDebugEnabled");
                 Tooltip("Enables Debug Mode, allowing you to select maps with L + R + Z, noclip with L + D-pad Right,\nand open the debug menu with L on the pause screen");
-                ImGui::Separator();
+                EnhancementCheckbox("Fast File Select", "gSkipLogoTitle");
+                Tooltip("Directly load the game to selected slot bellow\nUse slot number 4 to load directly in Zelda Map Select\n(Do not require debug menu but you will be unable to save there)\n(you can also load Zelda map select with Debug mod + slot 0).\nWith Slot : 0 you can go directly in File Select menu\nAttention, Loading an empty save will result in crash");
+                if (CVar_GetS32("gSkipLogoTitle",0)) {
+                    EnhancementSliderInt("Loading %d", "##SaveFileID", "gSaveFileID", 0, 4, "");
+                }
+		ImGui::Separator();
                 EnhancementCheckbox("Stats", "gStatsEnabled");
                 Tooltip("Shows the stats window, with your FPS and frametimes, and the OS you're playing on");
                 EnhancementCheckbox("Console", "gConsoleEnabled");
