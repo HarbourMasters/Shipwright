3348bd44525914e9d3f2132c15bc0ddaed90a517
KiritoDv <nohomoboi01@gmail.com>
2022-05-22 13:53:39 -0500

    Fixed kaleido on 64 bit

--- libultraship/libultraship/Lib/Fast3D/gfx_pc.h
+++ libultraship/libultraship/Lib/Fast3D/gfx_pc.h
@@ -6,10 +6,8 @@
 #include <unordered_map>
 #include <list>
 #include "U64/PR/ultra64/types.h"
-
-#ifdef __SWITCH__
 #include <cstddef>
-#endif
+
 
 struct GfxRenderingAPI;
 struct GfxWindowManagerAPI;
