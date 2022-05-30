4177721f369164fb763bde0d181b695935082063
Jeffrey Crowell <github@crowell.biz>
2022-05-30 16:36:54 -0400

    set the platform to macos

--- libultraship/libultraship/SohImGuiImpl.cpp
+++ libultraship/libultraship/SohImGuiImpl.cpp
@@ -804,7 +804,11 @@ namespace SohImGui {
 #ifdef _WIN32
             ImGui::Text("Platform: Windows");
 #else
+#ifdef __APPLE__
+            ImGui::Text("Platform: MacOS");
+#else
             ImGui::Text("Platform: Linux");
+#endif
 #endif
             ImGui::Text("Status: %.3f ms/frame (%.1f FPS)", 1000.0f / framerate, framerate);
             ImGui::End();
