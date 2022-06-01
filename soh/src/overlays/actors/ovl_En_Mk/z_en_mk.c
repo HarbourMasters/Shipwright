8a8770c79408d48c268e09efa216b9219dcb92c3
briaguya <briaguya@alice>
2022-06-01 02:50:37 -0400

    comment

--- soh/src/overlays/actors/ovl_En_Mk/z_en_mk.c
+++ soh/src/overlays/actors/ovl_En_Mk/z_en_mk.c
@@ -197,6 +197,7 @@ void func_80AACFA0(EnMk* this, GlobalContext* globalCtx) {
         this->actionFunc = func_80AACA40;
         gSaveContext.itemGetInf[1] |= 1;
     } else {
+        // not sure when/how/if this is getting called
         func_8002F434(&this->actor, globalCtx, gSaveContext.n64ddFlag ? GetRandomizedItemIdFromKnownCheck(LH_LAB_DIVE, GI_HEART_PIECE) : GI_HEART_PIECE, 10000.0f, 50.0f);
     }
 }
