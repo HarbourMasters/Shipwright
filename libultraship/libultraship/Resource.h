34ef1d2fd0b01c72e35631ca090f771e4cd88fb2
Nicholas Estelami <NEstelami@users.noreply.github.com>
2022-06-16 19:09:07 -0400

    Updated resource types

--- libultraship/libultraship/Resource.h
+++ libultraship/libultraship/Resource.h
@@ -30,10 +30,10 @@ namespace Ship
         Array            =   0x4F415252,     // OARR
         Text             =   0x4F545854,     // OTXT
         Blob             =   0x4F424C42,     // OBLB
-        Audio            =   'OAUD',
-        AudioSample      =   'OSMP',
-        AudioSoundFont   =   'OSFT',
-        AudioSequence    =   'OSEQ',
+        Audio            =   0x4F415544,     // OAUD
+        AudioSample      =   0x4F534D50,     // OSMP
+        AudioSoundFont   =   0x4F534654,     // OSFT
+        AudioSequence    =   0x4F534551,     // OSEQ
     };
 
     enum class DataType
