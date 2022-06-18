f81a86fc2164faa0c9df040280ac2e754d59fbe4
Dog <5172592+Dog@users.noreply.github.com>
2022-06-17 20:28:59 -0700

    cleanup

--- soh/include/z64player.h
+++ soh/include/z64player.h
@@ -612,7 +612,6 @@ typedef struct Player {
     /* 0x0A86 */ s8         unk_A86;
     /* 0x0A87 */ u8         unk_A87;
     /* 0x0A88 */ Vec3f      unk_A88; // previous body part 0 position
-    /* 0x0A89 */ u8         stateFlags4; // OOTRando
-} Player; // size = 0xA95
+} Player; // size = 0xA94
 
 #endif
