73ff7ef3dae83ec85afb20ec6453c69967bb0b96
louist103 <35883445+louist103@users.noreply.github.com>
2022-06-20 13:39:06 -0400

    Guard vision (#466)
    * Vision
    
    * Wrap it in a CVAR
    
    * Cleanup the code and fix a savestate bug

--- libultraship/libultraship/SohImGuiImpl.cpp
+++ libultraship/libultraship/SohImGuiImpl.cpp
@@ -905,6 +905,7 @@ namespace SohImGui {
                     Tooltip("The default response to Kaepora Gaebora is always that you understood what he said");
                     EnhancementCheckbox("Link's Cow in Both Time Periods", "gCowOfTime");
                     Tooltip("Allows the Lon Lon Ranch obstacle course reward to be shared across time periods");
+                    EnhancementCheckbox("Enable visible guard vision", "gGuardVision");
                     EnhancementCheckbox("Enable passage of time on file select", "gTimeFlowFileSelect");
                     ImGui::EndMenu();
                 }
