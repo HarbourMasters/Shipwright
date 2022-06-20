80c8b3cf664cdba93da78576aeb2f67c86c62c0f
Kenix3 <kenixwhisperwind@gmail.com>
2022-06-20 13:40:14 -0400

    Update libultraship/libultraship/TexLoader.cpp

--- libultraship/libultraship/TexLoader.cpp
+++ libultraship/libultraship/TexLoader.cpp
@@ -15,48 +15,50 @@
 
 namespace fs = std::filesystem;
 
-struct TexEntry {
-	Ship::TexIOLoader* loader;
-	std::string path;
-};
+namespace Ship {
+	struct TexEntry {
+		Ship::TexIOLoader* loader;
+		std::string path;
+	};
 
-std::unordered_map<std::string, TexEntry> QueryCache;
-std::unordered_map<std::string, int> FileCache;
-std::unordered_map<std::string, Ship::TexIOLoader*> IOLoaders = {
-	{ "dds", new Ship::DDSTexLoader },
-	{ "stb", new Ship::DefaultTexLoader },
-};
+	std::unordered_map<std::string, TexEntry> QueryCache;
+	std::unordered_map<std::string, int> FileCache;
+	std::unordered_map<std::string, Ship::TexIOLoader*> IOLoaders = {
+		{ "dds", new Ship::DDSTexLoader },
+		{ "stb", new Ship::DefaultTexLoader },
+	};
 
-void TexLoader::Init() {
-	for (auto &path : Ship::GlobalCtx2::GetInstance()->GetResourceManager()->GetArchive()->ListFiles("*.*")) {
-		std::string rPath = path.cFileName;
-		std::vector<std::string> raw = StringHelper::Split(rPath, ".");
-		std::string ext = raw[raw.size() - 1];
-		std::string nPath = rPath.substr(0, rPath.size() - (ext.size() + 1));
+	void TexLoader::Init() {
+		for (auto &path : Ship::GlobalCtx2::GetInstance()->GetResourceManager()->GetArchive()->ListFiles("*.*")) {
+			std::string rPath = path.cFileName;
+			std::vector<std::string> raw = StringHelper::Split(rPath, ".");
+			std::string ext = raw[raw.size() - 1];
+			std::string nPath = rPath.substr(0, rPath.size() - (ext.size() + 1));
 
-		// OTRTODO:Remove this when the new audio system get merged!
-		replace(nPath.begin(), nPath.end(), '\\', '/');
-		QueryCache[nPath] = { IOLoaders[ext == "dds" ? "dds" : "stb"], rPath };
+			// OTRTODO:Remove this when the new audio system get merged!
+			replace(nPath.begin(), nPath.end(), '\\', '/');
+			QueryCache[nPath] = { IOLoaders[ext == "dds" ? "dds" : "stb"], rPath };
+		}
 	}
-}
 
-bool TexLoader::LoadReplacement(int tile, const char* path, GfxRenderingAPI* api, TextureCacheNode** node, uint32_t fmt, uint32_t siz, uint32_t palette, const uint8_t* orig_addr) {
+	bool TexLoader::LoadReplacement(int tile, const char* path, GfxRenderingAPI* api, TextureCacheNode** node, uint32_t fmt, uint32_t siz, uint32_t palette, const uint8_t* orig_addr) {
 
-	if (path == nullptr || FileCache.contains(path)) return false;
+		if (path == nullptr || FileCache.contains(path)) return false;
 
-	FileCache[path] = 0;
+		FileCache[path] = 0;
 
-	if (!QueryCache.contains(path)) return false;
+		if (!QueryCache.contains(path)) return false;
 
-	TexEntry tEntry = QueryCache[path];
-	uint32_t textureId = tEntry.loader->UploadTexture(tile, tEntry.path.c_str(), api);
+		TexEntry tEntry = QueryCache[path];
+		uint32_t textureId = tEntry.loader->UploadTexture(tile, tEntry.path.c_str(), api);
 
-	if (textureId == (uint32_t) -1) return false;
+		if (textureId == (uint32_t) -1) return false;
 
-	TextureCacheKey key = { orig_addr, { }, static_cast<uint8_t>(fmt), static_cast<uint8_t>(siz), static_cast<uint8_t>(palette) };
-	TextureCacheValue value = { textureId, 0, 0, false };
-	const auto entry = new TextureCacheNode(key, value);
-	*node = entry;
+		TextureCacheKey key = { orig_addr, { }, static_cast<uint8_t>(fmt), static_cast<uint8_t>(siz), static_cast<uint8_t>(palette) };
+		TextureCacheValue value = { textureId, 0, 0, false };
+		const auto entry = new TextureCacheNode(key, value);
+		*node = entry;
 
-	return true;
+		return true;
+	}
 }
