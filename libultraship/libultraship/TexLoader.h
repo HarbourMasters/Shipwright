425bbd8c5235c8f7c602776014a13c3ac91ef969
Kenix3 <kenixwhisperwind@gmail.com>
2022-06-20 13:40:05 -0400

    Update libultraship/libultraship/TexLoader.h

--- libultraship/libultraship/TexLoader.h
+++ libultraship/libultraship/TexLoader.h
@@ -2,8 +2,10 @@
 #include <PR/ultra64/gbi.h>
 #include "Lib/Fast3D/gfx_pc.h"
 
-class TexLoader {
-public:
-	static void Init();
-	static bool LoadReplacement(int tile, const char* path, GfxRenderingAPI* api, TextureCacheNode** node, uint32_t fmt, uint32_t siz, uint32_t palette, const uint8_t* orig_addr);
-};
+namespace Ship {
+	class TexLoader {
+	public:
+		static void Init();
+		static bool LoadReplacement(int tile, const char* path, GfxRenderingAPI* api, TextureCacheNode** node, uint32_t fmt, uint32_t siz, uint32_t palette, const uint8_t* orig_addr);
+	};
+)
