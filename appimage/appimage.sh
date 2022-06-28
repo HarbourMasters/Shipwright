812280fb2e2b89c51a855edbf2993400f5e459c4
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2022-06-28 05:46:48 -0700

    update appimage script for Jenkins

--- appimage/appimage.sh
+++ appimage/appimage.sh
@@ -14,7 +14,7 @@ mkdir -p AppDir/usr/share/icons/hicolor/scalable/apps
 mkdir -p AppDir/usr/lib
 
 mv AppDir/soh.sh AppDir/usr/bin
-cp -r Release/* AppDir/usr/bin
+cp -r build/* AppDir/usr/bin
 
 chmod +x AppDir/usr/bin/{soh.elf,OTRGui,soh.sh}
  
@@ -22,19 +22,15 @@ cd AppDir && ln -s ./usr/bin/soh.sh ./AppRun && cd ..
 
 export UPD_INFO="gh-releases-zsync|HarbourMasters|Shipwright-linux|zelda64|SOH-Linux.AppImage.zsync"
 ./linuxdeploy-x86_64.AppImage                   \
-  --appdir="$GITHUB_WORKSPACE"/AppDir/          \
-  -d "$GITHUB_WORKSPACE"/AppDir/soh.desktop     \
-  -i "$GITHUB_WORKSPACE"/AppDir/soh.png         \
-  -e "$GITHUB_WORKSPACE"/AppDir/usr/bin/soh.elf \
-  -e "$GITHUB_WORKSPACE"/AppDir/usr/bin/assets/extractor/ZAPD.out
+  --appdir=./AppDir/          \
+  -d ./AppDir/soh.desktop     \
+  -i ./AppDir/soh.png         \
+  -e ./AppDir/usr/bin/soh.elf \
+  -e ./AppDir/usr/bin/assets/extractor/ZAPD.out
  
-mv "$GITHUB_WORKSPACE"/AppDir/usr/bin/ZAPD.out "$GITHUB_WORKSPACE"/AppDir/usr/bin/assets/extractor/ZAPD.out
-cd "$GITHUB_WORKSPACE"/AppDir/usr/bin/assets/extractor/
+mv ./AppDir/usr/bin/ZAPD.out ./AppDir/usr/bin/assets/extractor/ZAPD.out
+cd ./AppDir/usr/bin/assets/extractor/
 patchelf --set-rpath "\$ORIGIN/../../../lib" ZAPD.out
-cd "$GITHUB_WORKSPACE"
+cd /soh
  
-./appimagetool-x86_64.AppImage "$GITHUB_WORKSPACE"/AppDir "SOH-Linux.AppImage" -u "gh-releases-zsync|HarbourMasters|Shipwright-linux|zelda64|SOH-Linux.AppImage.zsync"
-
-mkdir -p "$GITHUB_WORKSPACE"/artifacts/ 
-mv SOH-Linux.AppImage* "$GITHUB_WORKSPACE"/artifacts/
-ls -al artifacts
+./appimagetool-x86_64.AppImage ./AppDir "SOH-Linux.AppImage" -u "gh-releases-zsync|HarbourMasters|Shipwright-linux|zelda64|SOH-Linux.AppImage.zsync"
