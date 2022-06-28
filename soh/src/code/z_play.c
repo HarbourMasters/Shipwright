58d99a0b9d84efea32e9f06af5f136f7dfded707
briaguya <briaguya@alice>
2022-06-28 03:20:51 -0400

    replace eventChkInf checks with medallion checks

--- soh/src/code/z_play.c
+++ soh/src/code/z_play.c
@@ -207,8 +207,8 @@ void GivePlayerRandoRewardSongOfTime(GlobalContext* globalCtx, RandomizerCheck c
 void GivePlayerRandoRewardNocturne(GlobalContext* globalCtx, RandomizerCheck check) {
     Player* player = GET_PLAYER(globalCtx);
 
-    if ((gSaveContext.entranceIndex == 0x00DB) && LINK_IS_ADULT && (gSaveContext.eventChkInf[4] & 0x0100) &&
-        (gSaveContext.eventChkInf[4] & 0x0200) && (gSaveContext.eventChkInf[4] & 0x0400) && player != NULL &&
+    if ((gSaveContext.entranceIndex == 0x00DB) && LINK_IS_ADULT && CHECK_QUEST_ITEM(QUEST_MEDALLION_FOREST) &&
+        CHECK_QUEST_ITEM(QUEST_MEDALLION_FIRE) && CHECK_QUEST_ITEM(QUEST_MEDALLION_WATER) && player != NULL &&
         !Player_InBlockingCsMode(globalCtx, player) && !Flags_GetEventChkInf(0xAA)) {
         GetItemID getItemId = GetRandomizedItemIdFromKnownCheck(check, GI_NOCTURNE_OF_SHADOW);
         GiveItemWithoutActor(globalCtx, getItemId);
