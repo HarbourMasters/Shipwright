1f50eab0f6f06a2db28f02bdb72d85858ec92a04
PurpleHato <linkvssangoku.jr@gmail.com>
2022-06-29 03:02:51 +0200

    FIX: avoiding stranginess with the previous fix

--- soh/soh/OTRGlobals.cpp
+++ soh/soh/OTRGlobals.cpp
@@ -1504,12 +1504,14 @@ extern "C" int CopyScrubMessage(u16 scrubTextId, char* buffer, const int maxBuff
         scrubText += "un ";
         scrubText += 0x05; // change the color
         scrubText += 0x42; // green
-        scrubText += "objet mysté\x96rieux";
+        scrubText += "objet mysté\x96";
+        scrubText += "rieux";
         scrubText += 0x05; // change the color
         scrubText += 0x40; // white
         scrubText += "?";
         scrubText += 0x01; // newline
-        scrubText += "\x84\a fera ";
+        scrubText += "\x84";
+        scrubText += "a fera ";
         scrubText += 0x05; // change the color
         scrubText += 0x41; // red
         scrubText += std::to_string(price) + " Rubis";
