9e32ee4cf4ac6659d39864ab332f7d0540a694fd
David Chavez <david@dcvz.io>
2022-07-01 01:16:10 +0200

    Fix modified import for SDL

--- libultraship/libultraship/Lib/ImGui/backends/imgui_impl_sdl.cpp
+++ libultraship/libultraship/Lib/ImGui/backends/imgui_impl_sdl.cpp
@@ -68,10 +68,13 @@
 
 // SDL
 // (the multi-viewports feature requires SDL features supported from SDL 2.0.4+. SDL 2.0.5+ is highly recommended)
-#include <SDL.h>
-#include <SDL_syswm.h>
 #if defined(__APPLE__)
 #include <TargetConditionals.h>
+#include <SDL.h>
+#include <SDL_syswm.h>
+#else
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_syswm.h>
 #endif
 
 #if SDL_VERSION_ATLEAST(2,0,4) && !defined(__EMSCRIPTEN__) && !defined(__ANDROID__) && !(defined(__APPLE__) && TARGET_OS_IOS) && !defined(__amigaos4__)
@@ -888,7 +891,11 @@ static void ImGui_ImplSDL2_SwapBuffers(ImGuiViewport* viewport, void*)
 // Vulkan support (the Vulkan renderer needs to call a platform-side support function to create the surface)
 // SDL is graceful enough to _not_ need <vulkan/vulkan.h> so we can safely include this.
 #if SDL_HAS_VULKAN
+#if defined(__APPLE__)
 #include <SDL_vulkan.h>
+#elif
+#include <SDL2/SDL_vulkan.h>
+#endif
 static int ImGui_ImplSDL2_CreateVkSurface(ImGuiViewport* viewport, ImU64 vk_instance, const void* vk_allocator, ImU64* out_vk_surface)
 {
     ImGui_ImplSDL2_ViewportData* vd = (ImGui_ImplSDL2_ViewportData*)viewport->PlatformUserData;
