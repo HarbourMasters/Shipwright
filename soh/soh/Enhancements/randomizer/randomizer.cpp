0d70424d9cbb2591705658a7606248126f3a153f
Christopher Leggett <chris@leggett.dev>
2022-07-08 19:09:24 -0400

    Changes Ganon's Trials Count setting to a checkbox
    The checkbox is whether or not to skip all of them. Leaving the box unchecked will mean doing all of them. Eventually this will be switched back to a slider once we implement the logic for which trials start out completed.

--- soh/soh/Enhancements/randomizer/randomizer.cpp
+++ soh/soh/Enhancements/randomizer/randomizer.cpp
@@ -3691,9 +3691,12 @@ void DrawRandoEditor(bool& open) {
                         SohImGui::EnhancementCombobox("gRandomizeGanonTrial", randoGanonsTrial, 2, 0);
                         if (CVar_GetS32("gRandomizeGanonTrial", 0) == 0) {
                         */
-                            SohImGui::EnhancementSliderInt("Ganon's Trial Count: %d", "##RandoTrialCount",
-                                                           "gRandomizeGanonTrialCount", 0, 6, "");
-                            InsertHelpHoverText("Set the number of trials required to enter\nGanon's Tower.");
+                            //SohImGui::EnhancementSliderInt("Ganon's Trial Count: %d", "##RandoTrialCount",
+                            //                               "gRandomizeGanonTrialCount", 0, 6, "");
+                            //InsertHelpHoverText("Set the number of trials required to enter\nGanon's Tower.");
+                        SohImGui::EnhancementCheckbox("Skip Ganon's Trials?", "gRandomizeGanonTrialCount");
+                        InsertHelpHoverText(
+                            "Sets whether or not Ganon's Castle Trials are required\nto enter Ganon's Tower.");
 
                         // }
                         ImGui::Separator();
