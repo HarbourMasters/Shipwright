ba6343318ef01776d08c217e3575c484a82497a7
earthcrafterman <banddstudios@gmail.com>
2022-07-11 20:30:58 -0400

    Split damage multiplication into its own PR

--- libultraship/libultraship/ImGuiImpl.cpp
+++ libultraship/libultraship/ImGuiImpl.cpp
@@ -91,6 +91,39 @@ namespace SohImGui {
         "None"
     };
 
+    const char* powers256[9] = {
+        "Vanilla (1x)",
+        "Double (2x)",
+        "Quadruple (4x)",
+        "Octuple (8x)",
+        "Hexadecuple (16x)",
+        "Duotrigintuple (32x)",
+        "Quattuorsexagintuple (64x)",
+        "Octoviginticentuple (128x)",
+        "Hexaquinquagintiducentuple (256x)"
+    };
+
+    const char* powers128[8] = {
+        "Vanilla (1x)",
+        "Double (2x)",
+        "Quadruple (4x)",
+        "Octuple (8x)",
+        "Hexadecuple (16x)",
+        "Duotrigintuple (32x)",
+        "Quattuorsexagintuple (64x)",
+        "Octoviginticentuple (128x)"
+    };
+
+    const char* powers64[7] = {
+        "Vanilla (1x)",
+        "Double (2x)",
+        "Quadruple (4x)",
+        "Octuple (8x)",
+        "Hexadecuple (16x)",
+        "Duotrigintuple (32x)",
+        "Quattuorsexagintuple (64x)"
+    };
+
     std::map<std::string, std::vector<std::string>> windowCategories;
     std::map<std::string, CustomWindow> customWindows;
 
@@ -937,12 +970,36 @@ namespace SohImGui {
 
                     if (ImGui::BeginMenu("Difficulty Options"))
                     {
-                        EnhancementSliderInt("Damage Multiplier %dx", "##DAMAGEMUL", "gDamageMul", 1, 4, "");
-                        Tooltip("Modifies all sources of damage not affected by other sliders");
-                        EnhancementSliderInt("Fall Damage Multiplier %dx", "##FALLDAMAGEMUL", "gFallDamageMul", 1, 4, "");
-                        Tooltip("Modifies all fall damage");
-                        EnhancementSliderInt("Void Damage Multiplier %dx", "##VOIDDAMAGEMUL", "gVoidDamageMul", 1, 4, "");
-                        Tooltip("Modifies damage taken after falling into a void");
+                        ImGui::Text("Damage Multiplier");
+                        EnhancementCombobox("gDamageMul", powers256, 9, 0);
+                        Tooltip("Modifies all sources of damage not affected by other sliders\n\
+2x: Can survive all common attacks from the start of the game\n\
+4x: Dies in 1 hit to any substantial attack from the start of the game\n\
+8x: Can only survive trivial damage from the start of the game\n\
+16x: Can survive all common attacks with max health without double defense\n\
+32x: Can survive all common attacks with max health and double defense\n\
+64x: Can survive trivial damage with max health without double defense\n\
+128x: Can survive trivial damage with max health and double defense\n\
+256x: Cannot survive damage");
+                        ImGui::Text("Fall Damage Multiplier");
+                        EnhancementCombobox("gFallDamageMul", powers128, 8, 0);
+                        Tooltip("Modifies all fall damage\n\
+2x: Can survive all fall damage from the start of the game\n\
+4x: Can only survive short fall damage from the start of the game\n\
+8x: Cannot survive any fall damage from the start of the game\n\
+16x: Can survive all fall damage with max health without double defense\n\
+32x: Can survive all fall damage with max health and double defense\n\
+64x: Can survive short fall damage with double defense\n\
+128x: Cannot survive fall damage");
+                        ImGui::Text("Void Damage Multiplier");
+                        EnhancementCombobox("gVoidDamageMul", powers64, 7, 0);
+                        Tooltip("Modifies damage taken after falling into a void\n\
+2x: Can survive void damage from the start of the game\n\
+4x: Cannot survive void damage from the start of the game\n\
+8x: Can survive void damage twice with max health without double defense\n\
+16x: Can survive void damage with max health without double defense\n\
+32x: Can survive void damage with max health and double defense\n\
+64x: Cannot survive void damage");
 
                         EnhancementCheckbox("No Random Drops", "gNoRandomDrops");
                         Tooltip("Disables random drops, except from the Goron Pot, Dampe, and bosses");
