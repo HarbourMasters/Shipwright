84c59b960e504b508614f2481cee0a58cf49739f
KiritoDv <kiritodev01@gmail.com>
2022-07-18 00:23:11 -0500

    Added physical slots into controller name

--- libultraship/libultraship/SDLController.cpp
+++ libultraship/libultraship/SDLController.cpp
@@ -25,11 +25,16 @@ namespace Ship {
 
         char GuidBuf[33];
         SDL_JoystickGetGUIDString(SDL_JoystickGetDeviceGUID(physicalSlot), GuidBuf, sizeof(GuidBuf));
-        GUID = std::string(GuidBuf);
         Cont = NewCont;
         wCamX = 0;
         wCamY = 0;
-
+#ifdef __SWITCH__
+        GUID = StringHelper::Sprintf("%s:%d", GuidBuf, physicalSlot);
+        ControllerName = StringHelper::Sprintf("%s #%d", SDL_GameControllerNameForIndex(physicalSlot), physicalSlot + 1);
+#else
+        GUID = std::string(GuidBuf);
+        ControllerName = std::string(SDL_GameControllerNameForIndex(physicalSlot));
+#endif
         return true;
     }
 
@@ -441,7 +446,7 @@ namespace Ship {
     }
 
     const char* SDLController::GetControllerName() {
-        return SDL_GameControllerNameForIndex(physicalSlot);
+        return strdup(ControllerName.c_str());
     }
 
     void SDLController::CreateDefaultBinding(int32_t slot) {
