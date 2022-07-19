e8ecd6b6bbeae1ea9e23dc8d1b2a2d48a4fd5bab
Vague Rant <vaguerant@users.noreply.github.com>
2022-07-20 00:11:45 +1000

    More freecam friendly

--- soh/src/overlays/actors/ovl_player_actor/z_player.c
+++ soh/src/overlays/actors/ovl_player_actor/z_player.c
@@ -13101,11 +13101,14 @@ void func_8084ECA4(Player* this, GlobalContext* globalCtx) {
     if (LinkAnimation_Update(globalCtx, &this->skelAnime)) {
         if (this->unk_84F != 0) {
             if (this->unk_850 == 0) {
-                if (!CVar_GetS32("gFastDrops", 0)) { Message_StartTextbox(globalCtx, D_80854A04[this->unk_84F - 1].textId, &this->actor); }
+                if (CVar_GetS32("gFastDrops", 0))
+                    { this->unk_84F = 0; }
+                else
+                    { Message_StartTextbox(globalCtx, D_80854A04[this->unk_84F - 1].textId, &this->actor); }
                 Audio_PlayFanfare(NA_BGM_ITEM_GET | 0x900);
                 this->unk_850 = 1;
             }
-            else if (Message_GetState(&globalCtx->msgCtx) == TEXT_STATE_CLOSING || CVar_GetS32("gFastDrops", 0)) {
+            else if (Message_GetState(&globalCtx->msgCtx) == TEXT_STATE_CLOSING) {
                 this->unk_84F = 0;
                 func_8005B1A4(Gameplay_GetCamera(globalCtx, 0));
             }
