f822019346205c5a74157027094b44e3a87b15a3
aMannus <mannusmenting@gmail.com>
2022-07-26 01:02:11 +0200

    Moved functions back to ImGuilmpl.cpp

--- soh/soh/Enhancements/applypresets.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-void applyEnhancementPresets(void);
-void applyEnhancementPresetDefault(void);
-void applyEnhancementPresetVanillaPlus(void);
-void applyEnhancementPresetEnhanced(void);
-void applyEnhancementPresetRandomizer(void);
-void applyEnhancementPresetIronman(void);
