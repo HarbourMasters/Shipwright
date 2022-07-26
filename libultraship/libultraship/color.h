3bb234e6a66a368a2bfaeb2935993c29b553f98a
Nicholas Estelami <NEstelami@users.noreply.github.com>
2022-07-25 23:11:45 -0400

    Fixed merge issues.

--- libultraship/libultraship/color.h
+++ libultraship/libultraship/color.h
@@ -4,11 +4,11 @@
 #include "endianness.h"
 
 typedef struct {
-    u8 r, g, b;
+    uint8_t r, g, b;
 } Color_RGB8;
 
 typedef struct {
-    u8 r, g, b, a;
+    uint8_t r, g, b, a;
 } Color_RGBA8;
 
 // only use when necessary for alignment purposes
@@ -20,21 +20,21 @@ typedef union {
         u8 a, b, g, r;
 #endif
     };
-    u32 rgba;
+    uint32_t rgba;
 } Color_RGBA8_u32;
 
 typedef struct {
-    f32 r, g, b, a;
+    float r, g, b, a;
 } Color_RGBAf;
 
 typedef union {
     struct {
-        u16 r : 5;
-        u16 g : 5;
-        u16 b : 5;
-        u16 a : 1;
+        uint16_t r : 5;
+        uint16_t g : 5;
+        uint16_t b : 5;
+        uint16_t a : 1;
     };
-    u16 rgba;
+    uint16_t rgba;
 } Color_RGBA16;
 
 #ifdef __cplusplus
