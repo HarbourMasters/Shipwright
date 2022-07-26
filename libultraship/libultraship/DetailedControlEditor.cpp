b7fc9d9640cb90065d8642ed76e44ca2dc1fe0e6
lilDavid <1337lilDavid@gmail.com>
2022-07-26 00:42:08 -0500

    Allow resizing detailed control window

--- libultraship/libultraship/DetailedControlEditor.cpp
+++ libultraship/libultraship/DetailedControlEditor.cpp
@@ -8,8 +8,6 @@
 
 namespace Ship {
 
-	float TableCellWidth = 300.0f; //1 Col
-
 	void Table_InitHeader(bool has_header = true) {
 		if (has_header) {
 			ImGui::TableHeadersRow();
@@ -21,6 +19,13 @@ namespace Ship {
 	}
 
 	void Table_NextCol() {
+		ImGui::TableNextColumn();
+		ImGui::AlignTextToFramePadding();
+		ImGui::PushItemWidth(ImGui::GetContentRegionAvail().x);
+	}
+	
+	void Table_NextLine() {
+		ImGui::TableNextRow();
 		ImGui::TableNextColumn();
 		ImGui::AlignTextToFramePadding();
 		ImGui::PushItemWidth(ImGui::GetContentRegionAvail().x);
@@ -98,55 +103,72 @@ namespace Ship {
 	}
 
 	void DetailedControlEditor::DrawOcarinaControlPanel() {
-		SohImGui::BeginGroupPanel("Ocarina Controls");
-
-		ImGui::SetCursorPosX(ImGui::GetCursorPosX() + 5);
-		SohImGui::EnhancementCheckbox("Customize Ocarina Controls", "gCustomOcarinaControls");
-		if (CVar_GetS32("gCustomOcarinaControls", 0) == 1) {
-			float width;
-
-			SohImGui::BeginGroupPanel("Notes", ImVec2(158, 20));
-				width = ImGui::CalcTextSize("D5").x + 10;
-				DrawOcarinaMapping("D5", "D5", BTN_CUP, width);
-				DrawOcarinaMapping("B4", "B4", BTN_CLEFT, width);
-				DrawOcarinaMapping("A4", "A4", BTN_CRIGHT, width);
-				DrawOcarinaMapping("F4", "F4", BTN_CDOWN, width);
-				DrawOcarinaMapping("D4", "D4", BTN_A, width);
-				ImGui::Dummy(ImVec2(0, 5));
-			SohImGui::EndGroupPanel();
-
-			ImGui::SameLine();
-
-			const ImVec2 cursor = ImGui::GetCursorPos();
-			SohImGui::BeginGroupPanel("Misc.", ImVec2(158, 20));
-				width = ImGui::CalcTextSize("Disable songs").x + 10;
-				DrawOcarinaMapping("Disable songs", "Disable", BTN_L, width);
-				DrawOcarinaMapping("Pitch up", "Sharp", BTN_R, width);
-				DrawOcarinaMapping("Pitch down", "Flat", BTN_Z, width);
-				ImGui::Dummy(ImVec2(0, 5));
-			SohImGui::EndGroupPanel(48);
-		} else {
-			ImGui::Dummy(ImVec2(0, 5));
-			ImGui::SetCursorPosX(ImGui::GetCursorPosX() + 5);
-			ImGui::TextWrapped("To modify the main ocarina controls, select the \"Customize Ocarina Controls\" checkbox.");
-			ImGui::Dummy(ImVec2(0, 5));
+		if (!ImGui::CollapsingHeader("Ocarina Controls")) {
+			return;
 		}
 
-		SohImGui::BeginGroupPanel("Alternate controls", ImVec2(391.5, 0));
-		if (ImGui::BeginTable("tableOcarinaAlternateControls", 2)) {
-			ImGui::TableSetupColumn("D-pad", ImGuiTableColumnFlags_WidthStretch | ImGuiTableColumnFlags_IndentEnable | ImGuiTableColumnFlags_NoSort, TableCellWidth);
-			ImGui::TableSetupColumn("Right stick", ImGuiTableColumnFlags_WidthStretch | ImGuiTableColumnFlags_IndentEnable | ImGuiTableColumnFlags_NoSort, TableCellWidth);
+		if (ImGui::BeginTable("tableCustomOcarinaControls", 1, ImGuiTableFlags_BordersH | ImGuiTableFlags_BordersV)) {
+			ImGui::TableSetupColumn("Custom Ocarina Controls", ImGuiTableColumnFlags_WidthStretch | ImGuiTableColumnFlags_IndentEnable | ImGuiTableColumnFlags_NoSort);
 			Table_InitHeader(false);
+			
 			ImGui::SetCursorPosX(ImGui::GetCursorPosX() + 5);
-			SohImGui::EnhancementCheckbox("Play with D-pad", "gDpadOcarina");
-			Table_NextCol();
-			SohImGui::EnhancementCheckbox("Play with camera stick", "gRStickOcarina");
-			ImGui::EndTable();
-		}
-		ImGui::Dummy(ImVec2(0, 5));
-		SohImGui::EndGroupPanel();
+			SohImGui::EnhancementCheckbox("Customize Ocarina Controls", "gCustomOcarinaControls");
 
-		SohImGui::EndGroupPanel(6);
+			if (CVar_GetS32("gCustomOcarinaControls", 0) == 1) {
+				float labelWidth;
+
+				if (ImGui::BeginTable("tableCustomMainOcarinaControls", 2, ImGuiTableFlags_SizingStretchProp)) {
+					ImGui::TableSetupColumn("Custom Ocarina Notes", ImGuiTableColumnFlags_IndentEnable | ImGuiTableColumnFlags_NoSort);
+					ImGui::TableSetupColumn("Custom Ocarina Modifiers", ImGuiTableColumnFlags_IndentEnable | ImGuiTableColumnFlags_NoSort);
+					Table_InitHeader(false);
+
+					SohImGui::BeginGroupPanel("Notes", ImGui::GetContentRegionAvail());
+					labelWidth = ImGui::CalcTextSize("D5").x + 10;
+
+					DrawOcarinaMapping("D5", "D5", BTN_CUP, labelWidth);
+					DrawOcarinaMapping("B4", "B4", BTN_CLEFT, labelWidth);
+					DrawOcarinaMapping("A4", "A4", BTN_CRIGHT, labelWidth);
+					DrawOcarinaMapping("F4", "F4", BTN_CDOWN, labelWidth);
+					DrawOcarinaMapping("D4", "D4", BTN_A, labelWidth);
+					ImGui::Dummy(ImVec2(0, 5));
+					float cursorY = ImGui::GetCursorPosY();
+					SohImGui::EndGroupPanel();
+
+					Table_NextCol();
+
+					SohImGui::BeginGroupPanel("Modifiers", ImGui::GetContentRegionAvail());
+					labelWidth = ImGui::CalcTextSize("Disable songs").x + 10;
+					DrawOcarinaMapping("Disable songs", "Disable", BTN_L, labelWidth);
+					DrawOcarinaMapping("Pitch up", "Sharp", BTN_R, labelWidth);
+					DrawOcarinaMapping("Pitch down", "Flat", BTN_Z, labelWidth);
+					ImGui::Dummy(ImVec2(0, 5));
+					SohImGui::EndGroupPanel(cursorY - ImGui::GetCursorPosY() + 2);
+
+					ImGui::EndTable();
+				}
+			} else {
+				ImGui::Dummy(ImVec2(0, 5));
+				ImGui::SetCursorPosX(ImGui::GetCursorPosX() + 5);
+				ImGui::TextWrapped("To modify the main ocarina controls, select the \"Customize Ocarina Controls\" checkbox.");
+				ImGui::Dummy(ImVec2(0, 5));
+			}
+
+			SohImGui::BeginGroupPanel("Alternate controls", ImGui::GetContentRegionAvail());
+			if (ImGui::BeginTable("tableOcarinaAlternateControls", 2, ImGuiTableFlags_SizingFixedSame)) {
+				ImGui::TableSetupColumn("D-pad", ImGuiTableColumnFlags_IndentEnable | ImGuiTableColumnFlags_NoSort);
+				ImGui::TableSetupColumn("Right stick", ImGuiTableColumnFlags_IndentEnable | ImGuiTableColumnFlags_NoSort);
+				Table_InitHeader(false);
+				ImGui::SetCursorPosX(ImGui::GetCursorPosX() + 5);
+				SohImGui::EnhancementCheckbox("Play with D-pad", "gDpadOcarina");
+				Table_NextCol();
+				SohImGui::EnhancementCheckbox("Play with camera stick", "gRStickOcarina");
+				ImGui::EndTable();
+			}
+			ImGui::Dummy(ImVec2(0, 5));
+			SohImGui::EndGroupPanel();
+		}
+
+		ImGui::EndTable();
 	}
 
 	void DetailedControlEditor::DrawHud() {
@@ -154,8 +176,8 @@ namespace Ship {
 			return;
 		}
 
-		ImGui::SetNextWindowSizeConstraints(ImVec2(420, 0), ImVec2(1200, 290));
-		if (ImGui::Begin("Detailed Control Configuration", nullptr, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_AlwaysAutoResize)) {
+		ImGui::SetNextWindowSize(ImVec2(420, 300), ImGuiCond_FirstUseEver);
+		if (ImGui::Begin("Detailed Control Configuration", nullptr)) {
 			DrawOcarinaControlPanel();
 		}
 		ImGui::End();
