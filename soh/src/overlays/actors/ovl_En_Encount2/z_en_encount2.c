695b75d14ff239655f8cda38891daf51e62b8f6c
briaguya <briaguya@alice>
2022-07-27 00:15:39 -0400

    fix build

--- soh/src/overlays/actors/ovl_En_Encount2/z_en_encount2.c
+++ soh/src/overlays/actors/ovl_En_Encount2/z_en_encount2.c
@@ -2,6 +2,7 @@
 #include "overlays/actors/ovl_En_Fire_Rock/z_en_fire_rock.h"
 #include "vt.h"
 #include "objects/object_efc_star_field/object_efc_star_field.h"
+#include "soh/frame_interpolation.h"
 
 #define FLAGS (ACTOR_FLAG_4 | ACTOR_FLAG_5)
 
