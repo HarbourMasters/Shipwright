d397c1d8718f64e25e438e2348d9c4b71b749af1
Kenix3 <kenixwhisperwind@gmail.com>
2022-08-04 17:35:49 -0400

    Increment version

--- soh/src/boot/build.c
+++ soh/src/boot/build.c
@@ -1,4 +1,4 @@
-const char gBuildVersion[] = "RACHAEL ALFA (3.0.0)";
+const char gBuildVersion[] = "RACHAEL BRAVO (3.0.1)";
 const char gBuildTeam[] = "github.com/harbourmasters";
 const char gBuildDate[] = __DATE__ " " __TIME__;
 const char gBuildMakeOption[] = "";
