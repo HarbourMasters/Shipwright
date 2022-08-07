6e8b45d1293ab7bfd357ceb86e7642354b76e60b
Sarge-117 <adam_branston@outlook.com>
2022-08-07 15:50:53 -0700

    Fix text colours (Navi text is always light blue)
    Navi text should always be \x44

--- soh/src/code/z_message_RandoNaviTips.c
+++ soh/src/code/z_message_RandoNaviTips.c
@@ -12,29 +12,29 @@ void RandoNaviTip(GlobalContext* globalCtx) {
     if (randNaviTip == 0) {
         switch (gSaveContext.language) {
             case LANGUAGE_FRA:
-                strcpy(font->msgBuf, "\x08French tip about playing rando!\x02");
+                strcpy(font->msgBuf, "\x08\x05\x44""French tip about playing rando!\x02");
                 break;
             case LANGUAGE_GER:
-                strcpy(font->msgBuf, "\x08German tip about playing rando!\x02");
+                strcpy(font->msgBuf, "\x08\x05\x44German tip about playing rando!\x02");
                 break;
             case LANGUAGE_ENG:
             default:
-                strcpy(font->msgBuf, "\x08Missing a small key in a dungeon?\x01Maybe the\x05\x43 "
-                                     "boss\x05\x40 has it!\x02");
+                strcpy(font->msgBuf, "\x08\x05\x44Missing a small key in a dungeon?\x01Maybe the\x05\x41 "
+                                     "boss\x05\x44 has it!\x02");
                 break;
         }
     }
     if (randNaviTip == 1) {
         switch (gSaveContext.language) {
             case LANGUAGE_FRA:
-                strcpy(font->msgBuf, "\x08French tip about playing rando!\x02");
+                strcpy(font->msgBuf, "\x08\x05\x44""French tip about playing rando!\x02");
                 break;
             case LANGUAGE_GER:
                 strcpy(font->msgBuf, "\x08German tip about playing rando!\x02");
                 break;
             case LANGUAGE_ENG:
             default:
-                strcpy(font->msgBuf, "\x08Sometimes you can use the \x05\x41Megaton\x01Hammer \x05\x40"
+                strcpy(font->msgBuf, "\x08\x05\x44Sometimes you can use the \x05\x41Megaton\x01Hammer \x05\x44"
                                      "instead of bombs!\x02");
                 break;
         }
@@ -42,7 +42,7 @@ void RandoNaviTip(GlobalContext* globalCtx) {
     if (randNaviTip == 2) {
         switch (gSaveContext.language) {
             case LANGUAGE_FRA:
-                strcpy(font->msgBuf, "\x08French tip about playing rando!\x02");
+                strcpy(font->msgBuf, "\x08\x05\x44""French tip about playing rando!\x02");
                 break;
             case LANGUAGE_GER:
                 strcpy(font->msgBuf, "\x08German tip about playing rando!\x02");
@@ -50,8 +50,8 @@ void RandoNaviTip(GlobalContext* globalCtx) {
             case LANGUAGE_ENG:
             default:
                 strcpy(font->msgBuf,
-                       "\x08There are three\x05\x42 business scrubs\x05\x40 in\x01"
-                       "Hyrule who sell \x05\x44mysterious items\x05\x40. Do\x01you know where they are?\x02");
+                       "\x08\x05\x44There are three\x05\x42 business scrubs\x05\x44 in\x01"
+                       "Hyrule who sell \x05\x46mysterious items\x05\x44. Do\x01you know where they are?\x02");
                 break;
         }
     }
