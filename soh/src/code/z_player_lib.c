179c432ee1482221edd1239968250890cd55a788
aMannus <mannusmenting@gmail.com>
2022-08-24 22:20:20 +0200

    Comment

--- soh/src/code/z_player_lib.c
+++ soh/src/code/z_player_lib.c
@@ -1170,6 +1170,7 @@ void Player_DrawGetItemImpl(GlobalContext* globalCtx, Player* this, Vec3f* refPo
     Matrix_RotateZYX(0, globalCtx->gameplayFrames * 1000, 0, MTXMODE_APPLY);
     Matrix_Scale(0.2f, 0.2f, 0.2f, MTXMODE_APPLY);
 
+    // RANDOTODO: Make this more flexible for easier toggling of individual item recolors in the future.
     if (this->getItemEntry.drawFunc != NULL &&
         (CVar_GetS32("gRandoMatchKeyColors", 0) || this->getItemEntry.getItemId == RG_DOUBLE_DEFENSE)) {
         this->getItemEntry.drawFunc(globalCtx, &this->getItemEntry);
