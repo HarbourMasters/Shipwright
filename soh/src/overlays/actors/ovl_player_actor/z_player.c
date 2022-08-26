2c4443498b5cd68bf58c139def363cf53cea200a
Christopher Leggett <chris@leggett.dev>
2022-08-26 13:00:52 -0400

    Fixes ice trap gidata not getting cleared after trap is sprung.

--- soh/src/overlays/actors/ovl_player_actor/z_player.c
+++ soh/src/overlays/actors/ovl_player_actor/z_player.c
@@ -6121,6 +6121,8 @@ s32 func_8083E5A8(Player* this, GlobalContext* globalCtx) {
                     this->actor.colChkInfo.damage = 0;
                     func_80837C0C(globalCtx, this, 3, 0.0f, 0.0f, 0, 20);
                     Player_SetPendingFlag(this, globalCtx);
+                    this->getItemId == GI_NONE;
+                    this->getItemEntry = (GetItemEntry)GET_ITEM_NONE;
                     return 1;
                 }
 
@@ -12866,6 +12868,8 @@ void func_8084E6D4(Player* this, GlobalContext* globalCtx) {
                 } else {
                     this->actor.colChkInfo.damage = 0;
                     func_80837C0C(globalCtx, this, 3, 0.0f, 0.0f, 0, 20);
+                    this->getItemId == GI_NONE;
+                    this->getItemEntry = (GetItemEntry)GET_ITEM_NONE;
                 }
                 return;
             }
