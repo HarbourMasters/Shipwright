65e76dcfeec3924ca9977c931703b4c3232a9733
Baoulettes <perlouzerie@hotmail.fr>
2022-08-27 17:45:09 +0200

    few fix and paper Link

--- soh/soh/Enhancements/debugconsole.cpp
+++ soh/soh/Enhancements/debugconsole.cpp
@@ -33,7 +33,8 @@ extern GlobalContext* gGlobalCtx;
 
 uint32_t giantLink;
 uint32_t minishLink;
-uint32_t gravityLevel;
+uint32_t paperLink;
+uint32_t gravityLevel = 1;
 uint32_t resetLinkScale;
 uint32_t invisibleLink;
 uint32_t oneHitKO;
@@ -656,11 +657,18 @@ static bool PaperLinkHandler(std::shared_ptr<Ship::Console> Console, const std::
         return CMD_FAILED;
     }
 
-    // TODO: Implement
+    try {
+        paperLink = Ship::Math::clamp(std::stoi(args[1], nullptr, 10), 0.0f, 2.0f);
+        return CMD_SUCCESS;
+    } catch (std::invalid_argument const& ex) {
+        SohImGui::console->SendErrorMessage("[SOH] Paper Link value must be a number.");
+        return CMD_FAILED;
+    }
 }
 
 static bool RainstormHandler(std::shared_ptr<Ship::Console> Console, const std::vector<std::string>& args) {
     // TODO: Implement
+    return CMD_FAILED;
 }
 
 static bool ReverseControlsHandler(std::shared_ptr<Ship::Console> Console, const std::vector<std::string>& args) {
--- soh/soh/Enhancements/debugconsole.h
+++ soh/soh/Enhancements/debugconsole.h
@@ -7,6 +7,7 @@ extern "C" {
 #endif
 extern uint32_t giantLink;
 extern uint32_t minishLink;
+extern uint32_t paperLink;
 extern uint32_t gravityLevel;
 extern uint32_t resetLinkScale;
 extern uint32_t invisibleLink;
