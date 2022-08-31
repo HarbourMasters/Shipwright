6544e4fe5ae6a4e59262eb56d59a4768fff89842
Oliver Schall <oliverschall22@googlemail.com>
2022-08-31 15:40:55 +0200

    Reverted linux assert fix for PR

--- soh/include/functions.h
+++ soh/include/functions.h
@@ -64,7 +64,7 @@ u32 Locale_IsRegionNative(void);
 #ifdef __WIIU__
 void _assert(const char* exp, const char* file, s32 line);
 #elif !defined(__APPLE__) && !defined(__SWITCH__)
-void __assert(const char* exp, const char* file, s32 line) __THROW __attribute__ ((__noreturn__));
+void __assert(const char* exp, const char* file, s32 line);
 #endif
 #if defined(__APPLE__) && defined(NDEBUG)
 void __assert(const char* exp, const char* file, s32 line);
