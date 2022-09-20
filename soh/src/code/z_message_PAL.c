ae37e5bb1218e511b7a655bca7abcd7e34406d6f
Christopher Leggett <chris@leggett.dev>
2022-09-19 23:46:06 -0400

    Removes unneeded commented out function calls.

--- soh/src/code/z_message_PAL.c
+++ soh/src/code/z_message_PAL.c
@@ -1281,7 +1281,6 @@ void Message_Decode(GlobalContext* globalCtx) {
     MessageContext* msgCtx = &globalCtx->msgCtx;
     Font* font = &globalCtx->msgCtx.font;
 
-    //gSPInvalidateTexCache(globalCtx->state.gfxCtx->polyOpa.p++, font->fontBuf);
     for (u32 i = 0; i < FONT_CHAR_TEX_SIZE * 120; i += FONT_CHAR_TEX_SIZE) {
         gSPInvalidateTexCache(globalCtx->state.gfxCtx->polyOpa.p++, &font->charTexBuf[i]);
     }
@@ -1681,7 +1680,6 @@ void Message_OpenText(GlobalContext* globalCtx, u16 textId) {
     }
 
     sMessageHasSetSfx = D_8014B2F4 = sTextboxSkipped = sTextIsCredits = 0;
-    //gSPInvalidateTexCache(globalCtx->state.gfxCtx->polyOpa.p++, font->fontBuf);
 
     if (textId >= 0x0500 && textId < 0x0600) { // text ids 0500 to 0600 are reserved for credits
         sTextIsCredits = true;
