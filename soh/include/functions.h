7329564c89b3585dcfa6226777969531bffa0d1d
RaelCappra <rael.cappra@gmail.com>
2022-09-23 13:44:49 -0300

    Add check for Linux in changed definition

--- soh/include/functions.h
+++ soh/include/functions.h
@@ -64,8 +64,10 @@ u32 func_80001F8C(void);
 u32 Locale_IsRegionNative(void);
 #ifdef __WIIU__
 void _assert(const char* exp, const char* file, s32 line);
-#elif !defined(__APPLE__) && !defined(__SWITCH__)
+#elif defined(__linux__)
 void __assert(const char* exp, const char* file, s32 line) __THROW;
+#elif !defined(__APPLE__) && !defined(__SWITCH__)
+void __assert(const char* exp, const char* file, s32 line);
 #endif
 #if defined(__APPLE__) && defined(NDEBUG)
 void __assert(const char* exp, const char* file, s32 line);
