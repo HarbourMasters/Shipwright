5c2c4b3652f1ce1678babd5a513f66f1996170d2
Christopher Leggett <chris@leggett.dev>
2022-10-07 16:18:03 -0400

    Makes appimage create both vanilla and mq otrs
    If either rom is present, it makes the corresponding OTR. If both are present,
    it will make both. If one OTR is present but both roms are present, it will
    create the missing OTR.

--- scripts/linux/appimage/soh.sh
+++ scripts/linux/appimage/soh.sh
@@ -9,54 +9,92 @@ if [ -z ${SHIP_HOME+x} ]; then
 export SHIP_HOME=$PWD
 fi
 
-while [[ ! -e "$SHIP_HOME"/oot.otr ]]; do
-	export ASSETDIR="$(mktemp -d /tmp/assets-XXXXX)"
-	ln -s "$HERE"/usr/bin/{assets,soh.elf,OTRGui} "$ASSETDIR"
-	export OLDPWD="$PWD"
-	mkdir -p "$ASSETDIR"/tmp
-	mkdir -p "$ASSETDIR"/Extract
-	if [ -e "$SHIP_HOME"/*.*64 ]; then
-		ln -s "$SHIP_HOME"/*.*64 "$ASSETDIR"/tmp/rom.z64
-		cp -r "$ASSETDIR"/assets/game "$ASSETDIR"/Extract/assets
-		cd "$ASSETDIR"
-		ROMHASH=$(sha1sum -b "$ASSETDIR"/tmp/rom.z64 | awk '{ print $1 }')
-		case "$ROMHASH" in
-		cee6bc3c2a634b41728f2af8da54d9bf8cc14099)
-			ROM=GC_NMQ_D;;
-		0227d7c0074f2d0ac935631990da8ec5914597b4)
-			ROM=GC_NMQ_PAL_F;;
-		50bebedad9e0f10746a52b07239e47fa6c284d03)
-			ROM=GC_MQ_D;;
-		079b855b943d6ad8bd1eb026c0ed169ecbdac7da)
-			ROM=GC_MQ_D;;	
-		*)
-  			if [ -n "$ZENITY" ]; then
-				zenity --error --timeout=10 --text="ROM hash <b>$ROMHASH</b> does not match" --title="Incorrect ROM file" --width=500 --width=200
-			else
-  				echo -e "\nrom hash does not match\n"
-  			fi
-  			exit;;
-		esac
-		if [ -n "$ZENITY" ]; then
-			(echo "# 25%"; echo "25"; sleep 2; echo "# 50%"; echo "50"; sleep 3; echo "# 75%"; echo "75"; sleep 2; echo "# 100%"; echo "100"; sleep 3) |
-			zenity --progress --title="OTR Generating..." --timeout=10 --percentage=0 --icon-name=soh --window-icon=soh.png --height=80 --width=400 &
-		else
-			echo "Processing..."
-		fi
-		assets/extractor/ZAPD.out ed -eh -i assets/extractor/xmls/"${ROM}" -b tmp/rom.z64 -fl assets/extractor/filelists -o placeholder -osf placeholder -gsf 1 -rconf assets/extractor/Config_"${ROM}".xml -se OTR > /dev/null 2>&1
-		cp "$ASSETDIR"/oot.otr "$SHIP_HOME"
-		echo "Restart $APPIMAGE to play!" 
-		sleep 3
-		rm -r "$ASSETDIR"
-		break
-	else
-		if [ -n "$ZENITY" ]; then
-			zenity --error --timeout=5 --text="Place ROM in $SHIP_HOME" --title="Missing ROM file" --width=500 --width=200
-		else
-			echo -e "\nPlace ROM in this folder\n"
-		fi	
-		exit
-	fi
+while [[ (! -e "$SHIP_HOME"/oot.otr) || (! -e "$SHIP_HOME"/oot-mq.otr) ]]; do
+        for romfile in "$SHIP_HOME"/*.*64
+        do
+            if [[ -e $romfile ]]; then
+                export ASSETDIR="$(mktemp -d /tmp/assets-XXXXX)"
+                ln -s "$HERE"/usr/bin/{assets,soh.elf,OTRGui} "$ASSETDIR"
+                export OLDPWD="$PWD"
+                mkdir -p "$ASSETDIR"/tmp
+	            mkdir -p "$ASSETDIR"/Extract
+                ln -s $romfile "$ASSETDIR"/tmp/rom.z64
+                cd "$ASSETDIR"
+                ROMHASH=$(sha1sum -b "$ASSETDIR"/tmp/rom.z64 | awk '{ print $1 }')
+                case "$ROMHASH" in
+                cee6bc3c2a634b41728f2af8da54d9bf8cc14099)
+                    if [[ ! -e "$SHIP_HOME"/oot.otr ]]; then
+                        ROM=GC_NMQ_D
+                        OTRNAME="oot.otr"
+                    fi
+                    ;;
+                0227d7c0074f2d0ac935631990da8ec5914597b4)
+                    if [[ ! -e "$SHIP_HOME"/oot.otr ]]; then
+                        ROM=GC_NMQ_PAL_F
+                        OTRNAME="oot.otr"
+                    else
+                        continue
+                    fi
+                    ;;
+                50bebedad9e0f10746a52b07239e47fa6c284d03)
+                    if [[ ! -e "$SHIP_HOME"/oot-mq.otr ]]; then
+                        ROM=GC_MQ_D
+                        OTRNAME="oot-mq.otr"
+                    else
+                        continue
+                    fi
+                    ;;
+                079b855b943d6ad8bd1eb026c0ed169ecbdac7da)
+                    if [[ ! -e "$SHIP_HOME"/oot-mq.otr ]]; then
+                        ROM=GC_MQ_D
+                        OTRNAME="oot-mq.otr"
+                    else
+                        continue
+                    fi
+                    ;;
+                517bd9714c73cb96c21e7c2ef640d7b55186102f)
+                    if [[ ! -e "$SHIP_HOME"/oot-mq.otr ]]; then
+                        ROM=GC_MQ_D
+                        OTRNAME="oot-mq.otr"
+                    else
+                        continue
+                    fi
+                    ;;
+                *)
+                    echo -e "\n$romfile - $ROMHASH rom hash does not match\n"
+                    continue;;
+                esac
+                cp -r "$ASSETDIR"/assets/game "$ASSETDIR"/Extract/assets
+                if [ -n "$ZENITY" ]; then
+                    (echo "# 25%"; echo "25"; sleep 2; echo "# 50%"; echo "50"; sleep 3; echo "# 75%"; echo "75"; sleep 2; echo "# 100%"; echo "100"; sleep 3) |
+                    zenity --progress --title="OTR Generating..." --timeout=10 --percentage=0 --icon-name=soh --window-icon=soh.png --height=80 --width=400 &
+                else
+                    echo "Processing..."
+                fi
+                assets/extractor/ZAPD.out ed -eh -i assets/extractor/xmls/"${ROM}" -b tmp/rom.z64 -fl assets/extractor/filelists -o placeholder -osf placeholder -gsf 1 -rconf assets/extractor/Config_"${ROM}".xml -se OTR --otrfile "${OTRNAME}" > /dev/null 2>&1
+                cp "$ASSETDIR"/"$OTRNAME" "$SHIP_HOME"
+            else
+                if [ -n "$ZENITY" ]; then
+                    zenity --error --timeout=5 --text="Place ROM in $SHIP_HOME" --title="Missing ROM file" --width=500 --width=200
+                else
+                    echo -e "\nPlace ROM in this folder\n"
+                fi	
+                exit
+            fi
+        done
+        if [[ (! -e "$SHIP_HOME"/oot.otr) && (! -e "$SHIP_HOME"/oot-mq.otr) ]]; then
+            if [ -n "$ZENITY" ]; then
+                zenity --error --timeout=10 --text="No valid ROMs were provided, No OTR was generated." --title="Incorrect ROM file" --width=500 --width=200
+            else
+                echo "No valid roms provided, no OTR was generated."
+            fi
+            rm -r "$ASSETDIR"
+            exit
+        else
+            (cd "$HERE/usr/bin"; ./soh.elf)
+            exit
+        fi
+        rm -r "$ASSETDIR"
 done	
 	(cd "$HERE/usr/bin"; ./soh.elf)
 exit
