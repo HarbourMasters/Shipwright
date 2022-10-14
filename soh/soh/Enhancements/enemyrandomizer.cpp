87c1b68ef26d552074213479dcd2c25bb72b4413
aMannus <mannusmenting@gmail.com>
2022-10-14 10:38:47 +0200

    More enemies & indentation fix

--- soh/soh/Enhancements/enemyrandomizer.cpp
+++ soh/soh/Enhancements/enemyrandomizer.cpp
@@ -7,143 +7,144 @@ extern "C" {
 uint32_t enemyFound = 0;
 
 static enemyEntry randomizedEnemySpawnTable[RANDOMIZED_ENEMY_SPAWN_TABLE_SIZE] = {
-	{ACTOR_EN_FIREFLY, 2},		// Regular Keese
-	{ACTOR_EN_FIREFLY, 1},		// Fire Keese
-	{ACTOR_EN_FIREFLY, 4},		// Ice Keese
-    {ACTOR_EN_TEST, 2},			// Stalfos
-	{ACTOR_EN_TITE, 0},			// Tektite
-	// Broken {ACTOR_EN_POH, 0},		// Poe (requires actor changes)
-	// Broken {ACTOR_EN_POH, 2},		// Poe (composer Sharp) (requires actor changes)
-	// Broken {ACTOR_EN_POH, 3},		// Poe (composer Flat) (requires actor changes)
+    { ACTOR_EN_FIREFLY, 2 }, // Regular Keese
+    { ACTOR_EN_FIREFLY, 1 }, // Fire Keese
+    { ACTOR_EN_FIREFLY, 4 }, // Ice Keese
+    { ACTOR_EN_TEST, 2 },    // Stalfos
+    { ACTOR_EN_TITE, 0 },    // Tektite
+    // Broken {ACTOR_EN_POH, 0},		// Poe (requires actor changes)
+    // Broken {ACTOR_EN_POH, 2},		// Poe (composer Sharp) (requires actor changes)
+    // Broken {ACTOR_EN_POH, 3},		// Poe (composer Flat) (requires actor changes)
     // Broken {ACTOR_EN_OKUTA, 0},		// Octorok (requires actor changes)
-	{ACTOR_EN_WALLMAS, 1},		// Wallmaster
+    { ACTOR_EN_WALLMAS, 1 },  // Wallmaster
     // Broken {ACTOR_EN_REEBA, 0},		// Leever (requires actor changes)
     // Broken {ACTOR_EN_PEEHAT, 0},		// Flying Peahat, spawns larva (requires more testing)
     // Broken {ACTOR_EN_PEEHAT, 1},		// Flying Peahat Larva (requires actor changes)
-	{ACTOR_EN_ZF, -1},			// Lizalfos
-	{ACTOR_EN_ZF, -2},			// Dinolfos
-	{ACTOR_EN_GOMA, 7},			// Gohma larva (non-gohma rooms)
-	{ACTOR_EN_BUBBLE, 0},		// Shabom (bubble)
-	{ACTOR_EN_DODOJR, 0},		// Baby Dodongo
-	{ACTOR_EN_TORCH2, 0},		// Dark Link
-	{ACTOR_EN_BILI, 0},			// Biri (jellyfish)
-	{ACTOR_EN_TP, -1},			// Electric Tailparasan
-	{ACTOR_EN_ST, 0},			// Skulltula (normal)
-	{ACTOR_EN_ST, 1},			// Skulltula (big)
-	{ACTOR_EN_ST, 2},			// Skulltula (invisible)
-	{ACTOR_EN_BW, 0},			// Torch Slug
-	{ACTOR_EN_EIYER, 10},		// Stinger (land) (One in formation, sink under floor and do not activate)
-	{ACTOR_EN_MB, 0},			// Moblins (Club)
-	{ACTOR_EN_DEKUBABA, 0},		// Deku Baba (small)
-	{ACTOR_EN_DEKUBABA, 1},		// Deku Baba (large)
-    /* Partly broken */ { ACTOR_EN_DEKUNUTS, 768 },	// Mad Scrub (triple attack) (projectiles don't work)
-	{ACTOR_EN_VALI, -1},		// Bari (big jellyfish) (problems with splitting and positioning - spawning in the air)
-	{ACTOR_EN_BB, -1},			// Bubble (flying skull enemy) (blue)
-	{ACTOR_EN_YUKABYUN, 0},		// Flying Floor Tile
-	{ACTOR_EN_VM, 1280},		// Beamos
-	{ACTOR_EN_FLOORMAS, 0},		// Floormaster
-	{ACTOR_EN_RD, 1},			// Redead (standing)
-	{ACTOR_EN_RD, 32766},		// Gibdo (standing)
-	{ACTOR_EN_SW, 0},			// Skullwalltula
-	{ACTOR_EN_FD, 0},			// Flare Dancer
-	{ACTOR_EN_KAREBABA, 0},		// Withered Deku Baba
-	{ACTOR_EN_RR, 0},			// Like-Like
-	{ACTOR_EN_NY, 0},			// Spike (rolling enemy)
-	{ACTOR_EN_IK, 2},			// Iron Knuckle (black, standing)
-	{ACTOR_EN_IK, 3},			// Iron Knuckle (white, standing)
-	{ACTOR_EN_TUBO_TRAP, 0},	// Flying pot
-	{ACTOR_EN_FZ, 0},			// Freezard
-	{ACTOR_EN_CLEAR_TAG, 0}		// Arwing
-	//{ACTOR_EN_WEIYER, 0}		// Stinger (Water)
+    { ACTOR_EN_ZF, -1 },      // Lizalfos
+    { ACTOR_EN_ZF, -2 },      // Dinolfos
+    { ACTOR_EN_GOMA, 7 },     // Gohma larva (non-gohma rooms)
+    { ACTOR_EN_BUBBLE, 0 },   // Shabom (bubble)
+    { ACTOR_EN_DODOJR, 0 },   // Baby Dodongo
+    { ACTOR_EN_TORCH2, 0 },   // Dark Link
+    { ACTOR_EN_BILI, 0 },     // Biri (jellyfish)
+    { ACTOR_EN_TP, -1 },      // Electric Tailparasan
+    { ACTOR_EN_ST, 0 },       // Skulltula (normal)
+    { ACTOR_EN_ST, 1 },       // Skulltula (big)
+    { ACTOR_EN_ST, 2 },       // Skulltula (invisible)
+    { ACTOR_EN_BW, 0 },       // Torch Slug
+    { ACTOR_EN_EIYER, 10 },   // Stinger (land) (One in formation, sink under floor and do not activate)
+    { ACTOR_EN_MB, 0 },       // Moblins (Club)
+    { ACTOR_EN_DEKUBABA, 0 }, // Deku Baba (small)
+    { ACTOR_EN_DEKUBABA, 1 }, // Deku Baba (large)
+    /* Partly broken */ { ACTOR_EN_DEKUNUTS, 768 }, // Mad Scrub (triple attack) (projectiles don't work)
+    { ACTOR_EN_VALI, -1 },     // Bari (big jellyfish) (problems with splitting and positioning - spawning in the air)
+    { ACTOR_EN_BB, -1 },       // Bubble (flying skull enemy) (blue)
+    { ACTOR_EN_YUKABYUN, 0 },  // Flying Floor Tile
+    { ACTOR_EN_VM, 1280 },     // Beamos
+    { ACTOR_EN_FLOORMAS, 0 },  // Floormaster
+    { ACTOR_EN_RD, 1 },        // Redead (standing)
+    { ACTOR_EN_RD, 32766 },    // Gibdo (standing)
+    { ACTOR_EN_SW, 0 },        // Skullwalltula
+    { ACTOR_EN_FD, 0 },        // Flare Dancer
+    { ACTOR_EN_KAREBABA, 0 },  // Withered Deku Baba
+    { ACTOR_EN_RR, 0 },        // Like-Like
+    { ACTOR_EN_NY, 0 },        // Spike (rolling enemy)
+    { ACTOR_EN_IK, 2 },        // Iron Knuckle (black, standing)
+    { ACTOR_EN_IK, 3 },        // Iron Knuckle (white, standing)
+    { ACTOR_EN_TUBO_TRAP, 0 }, // Flying pot
+    { ACTOR_EN_FZ, 0 },        // Freezard
+    { ACTOR_EN_CLEAR_TAG, 1 }, // Arwing
+    { ACTOR_EN_WF, 1 }         // Wolfos
 };
 
 static int enemiesToRandomize[] = {
-	ACTOR_EN_FIREFLY,		// Keese (including fire/ice)
-    ACTOR_EN_TEST,			// Stalfos
-	ACTOR_EN_TITE,			// Tektite
-	ACTOR_EN_POH,			// Poe (normal, blue rupee, composers
-    ACTOR_EN_OKUTA,			// Octorok
-	ACTOR_EN_WALLMAS,		// Wallmaster
-    ACTOR_EN_REEBA,			// Leever
-    ACTOR_EN_PEEHAT,		// Flying Peahat, big one spawning larva, larva
-	ACTOR_EN_ZF,			// Lizalfos, dinolfos
-	ACTOR_EN_GOMA,			// Gohma larva (normal, eggs, gohma eggs)
-	ACTOR_EN_BUBBLE,		// Shabom (bubble)
-	ACTOR_EN_DODOJR,		// Baby Dodongo
-	ACTOR_EN_TORCH2,		// Dark Link
-	ACTOR_EN_BILI,			// Biri (small jellyfish)
-	ACTOR_EN_TP,			// Electric Tailparasan
-	ACTOR_EN_ST,			// Skulltula (normal, big, invisible)
-	ACTOR_EN_BW,			// Torch Slug
-	ACTOR_EN_EIYER,			// Stinger (land)
-	ACTOR_EN_MB,			// Moblins (Club, spear)
-	ACTOR_EN_DEKUBABA,		// Deku Baba (small, large)
-	ACTOR_EN_DEKUNUTS,		// Mad Scrub (single attack, triple attack)
-    ACTOR_EN_VALI,			// Bari (big jellyfish) (spawns very high up)
-	ACTOR_EN_BB,			// Bubble (flying skull enemy) (all colors)
-	ACTOR_EN_YUKABYUN,		// Flying Floor Tile
-	ACTOR_EN_VM,			// Beamos
-	ACTOR_EN_FLOORMAS,		// Floormaster
-	ACTOR_EN_RD,			// Redead, Gibdo
-    ACTOR_EN_SW,			// Skullwalltula
-	ACTOR_EN_FD,			// Flare Dancer
-	ACTOR_EN_KAREBABA,		// Withered Deku Baba
-	ACTOR_EN_RR,			// Like-Like
-	ACTOR_EN_NY,			// Spike (rolling enemy)
-	ACTOR_EN_IK,			// Iron Knuckle
-	ACTOR_EN_TUBO_TRAP,		// Flying pot
-	ACTOR_EN_FZ,			// Freezard
-	ACTOR_EN_WEIYER			// Stinger (Water)
+    ACTOR_EN_FIREFLY,   // Keese (including fire/ice)
+    ACTOR_EN_TEST,      // Stalfos
+    ACTOR_EN_TITE,      // Tektite
+    ACTOR_EN_POH,       // Poe (normal, blue rupee, composers
+    ACTOR_EN_OKUTA,     // Octorok
+    ACTOR_EN_WALLMAS,   // Wallmaster
+    ACTOR_EN_REEBA,     // Leever
+    ACTOR_EN_PEEHAT,    // Flying Peahat, big one spawning larva, larva
+    ACTOR_EN_ZF,        // Lizalfos, dinolfos
+    ACTOR_EN_GOMA,      // Gohma larva (normal, eggs, gohma eggs)
+    ACTOR_EN_BUBBLE,    // Shabom (bubble)
+    ACTOR_EN_DODOJR,    // Baby Dodongo
+    ACTOR_EN_TORCH2,    // Dark Link
+    ACTOR_EN_BILI,      // Biri (small jellyfish)
+    ACTOR_EN_TP,        // Electric Tailparasan
+    ACTOR_EN_ST,        // Skulltula (normal, big, invisible)
+    ACTOR_EN_BW,        // Torch Slug
+    ACTOR_EN_EIYER,     // Stinger (land)
+    ACTOR_EN_MB,        // Moblins (Club, spear)
+    ACTOR_EN_DEKUBABA,  // Deku Baba (small, large)
+    ACTOR_EN_DEKUNUTS,  // Mad Scrub (single attack, triple attack)
+    ACTOR_EN_VALI,      // Bari (big jellyfish) (spawns very high up)
+    ACTOR_EN_BB,        // Bubble (flying skull enemy) (all colors)
+    ACTOR_EN_YUKABYUN,  // Flying Floor Tile
+    ACTOR_EN_VM,        // Beamos
+    ACTOR_EN_FLOORMAS,  // Floormaster
+    ACTOR_EN_RD,        // Redead, Gibdo
+    ACTOR_EN_SW,        // Skullwalltula
+    ACTOR_EN_FD,        // Flare Dancer
+    ACTOR_EN_KAREBABA,  // Withered Deku Baba
+    ACTOR_EN_RR,        // Like-Like
+    ACTOR_EN_NY,        // Spike (rolling enemy)
+    ACTOR_EN_IK,        // Iron Knuckle
+    ACTOR_EN_TUBO_TRAP, // Flying pot
+    ACTOR_EN_FZ,        // Freezard
+    ACTOR_EN_WEIYER,    // Stinger (Water)
+    ACTOR_EN_WF         // Wolfos
 };
 
 extern "C" enemyEntry GetRandomizedEnemy(void) {
-    return randomizedEnemySpawnTable[rand() % RANDOMIZED_ENEMY_SPAWN_TABLE_SIZE];
-    //return randomizedEnemySpawnTable[39];
+    // return randomizedEnemySpawnTable[rand() % RANDOMIZED_ENEMY_SPAWN_TABLE_SIZE];
+    return randomizedEnemySpawnTable[39];
 }
 
 extern "C" uint8_t IsEnemyFoundToRandomize(int actorId = 0, int param = 0) {
 
-	uint8_t isEnemyFound = 0;
+    uint8_t isEnemyFound = 0;
 
-	for (int i = 0; i < sizeof(enemiesToRandomize) / sizeof(enemiesToRandomize[0]); i++) {
+    for (int i = 0; i < sizeof(enemiesToRandomize) / sizeof(enemiesToRandomize[0]); i++) {
         if (actorId == enemiesToRandomize[i]) {
             isEnemyFound = 1;
         }
     }
 
-	if (isEnemyFound) {
-		switch (actorId) {
-			// Only randomize the main component of Electric Tailparasans, not the tail segments they spawn
-			case ACTOR_EN_TP:
-				if (param != -1) {
+    if (isEnemyFound) {
+        switch (actorId) {
+            // Only randomize the main component of Electric Tailparasans, not the tail segments they spawn
+            case ACTOR_EN_TP:
+                if (param != -1) {
                     return 0;
                 }
                 break;
-			// Only randomize the initial deku scrub actor (single and triple attack), not the flower they spawn.
-			case ACTOR_EN_DEKUNUTS:
-				if (param != -256 && param != 768) {
+            // Only randomize the initial deku scrub actor (single and triple attack), not the flower they spawn.
+            case ACTOR_EN_DEKUNUTS:
+                if (param != -256 && param != 768) {
                     return 0;
-				}
+                }
                 break;
-			// Only randomize initial floormaster actor (it can split and does some spawning on init)
-			case ACTOR_EN_FLOORMAS:
-				if (param != 0) {
+            // Only randomize initial floormaster actor (it can split and does some spawning on init)
+            case ACTOR_EN_FLOORMAS:
+                if (param != 0) {
                     return 0;
-				}
+                }
                 break;
-			// Only randomize initial egg spawn, not the enemy that comes out of the egg
+            // Only randomize initial egg spawn, not the enemy that comes out of the egg
             case ACTOR_EN_GOMA:
-				if (param != 0 && param != 6 && param != 8) {
+                if (param != 0 && param != 6 && param != 8) {
                     return 0;
-				}
+                }
                 break;
-			// Only randomize Skullwalltulas, not Golden Skulltulas
+            // Only randomize Skullwalltulas, not Golden Skulltulas
             case ACTOR_EN_SW:
-				if (param != 0) {
+                if (param != 0) {
                     return 0;
-				}
+                }
                 break;
-			// Don't randomize Nabooru because it'll break cutscenes and progression
+            // Don't randomize Nabooru because it'll break cutscenes and progression
             case ACTOR_EN_IK:
                 if (param == 1280) {
                     return 0;
@@ -151,12 +152,12 @@ extern "C" uint8_t IsEnemyFoundToRandomize(int actorId = 0, int param = 0) {
                 break;
             default:
                 break;
-		}
+        }
 
-		// Enemy found.
+        // Enemy found.
         return 1;
-	}
+    }
 
-	// If no enemy is found, don't randomize the actor.
-	return 0;
+    // If no enemy is found, don't randomize the actor.
+    return 0;
 }
--- soh/soh/Enhancements/enemyrandomizer.h
+++ soh/soh/Enhancements/enemyrandomizer.h
@@ -7,7 +7,7 @@ typedef struct enemyEntry {
     int16_t enemyParam;
 } enemyEntry;
 
-enum { RANDOMIZED_ENEMY_SPAWN_TABLE_SIZE = 40 };
+enum { RANDOMIZED_ENEMY_SPAWN_TABLE_SIZE = 41 };
 
 #ifndef __cplusplus
 uint8_t IsEnemyFoundToRandomize(int actorId, int param);
