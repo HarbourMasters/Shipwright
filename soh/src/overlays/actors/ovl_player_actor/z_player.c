8d57ef4bede8b3ad5dd1d170e439cb8291e33e5d
RaelCappra <rael.cappra@gmail.com>
2022-11-07 05:02:42 -0300

    Restore QPA glitched damage (#1789)

--- soh/src/overlays/actors/ovl_player_actor/z_player.c
+++ soh/src/overlays/actors/ovl_player_actor/z_player.c
@@ -3597,7 +3597,12 @@ void func_80837948(PlayState* play, Player* this, s32 arg2) {
     }
 
     if ((arg2 >= 16) && (arg2 < 20)) {
-        flags = D_80854488[temp][1];
+        if (CVar_GetS32("gRestoreQPA", 1) && temp == -1) {
+            flags = 0x16171617;
+        }
+        else {
+            flags = D_80854488[temp][1];
+        }
     } else {
         flags = D_80854488[temp][0];
     }
