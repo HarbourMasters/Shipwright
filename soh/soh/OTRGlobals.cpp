df8fa0f5db92d2847fc35bacb6262c6bf10c5e04
Garrett Cox <garrettjcox@gmail.com>
2022-11-07 09:37:06 -0600

    GlobalCtx -> Playstate

--- soh/soh/OTRGlobals.cpp
+++ soh/soh/OTRGlobals.cpp
@@ -1809,7 +1809,7 @@ extern "C" int GetEquipNowMessage(char* buffer, char* src, const int maxBufferSi
     return 0;
 }
 
-extern "C" u8 GetNextChildTradeItem(GlobalContext* globalCtx, u8 forward) {
+extern "C" u8 GetNextChildTradeItem(u8 forward) {
     std::vector<u8> possibleItems;
 
     if (gSaveContext.sohStats.weirdEggHasHatched) {
--- soh/src/overlays/misc/ovl_kaleido_scope/z_kaleido_item.c
+++ soh/src/overlays/misc/ovl_kaleido_scope/z_kaleido_item.c
@@ -369,11 +369,11 @@ void KaleidoScope_DrawItemSelect(PlayState* play) {
                         if (((pauseCtx->stickRelX > 30 || pauseCtx->stickRelY > 30) ||
                              dpad && CHECK_BTN_ANY(input->press.button, BTN_DRIGHT | BTN_DUP))) {
                             Audio_PlaySoundGeneral(NA_SE_SY_CURSOR, &D_801333D4, 4, &D_801333E0, &D_801333E0, &D_801333E8);
-                            INV_CONTENT(ITEM_TRADE_CHILD) = GetNextChildTradeItem(globalCtx, 1);
+                            INV_CONTENT(ITEM_TRADE_CHILD) = GetNextChildTradeItem(1);
                         } else if (((pauseCtx->stickRelX < -30 || pauseCtx->stickRelY < -30) ||
                                     dpad && CHECK_BTN_ANY(input->press.button, BTN_DLEFT | BTN_DDOWN))) {
                             Audio_PlaySoundGeneral(NA_SE_SY_CURSOR, &D_801333D4, 4, &D_801333E0, &D_801333E0, &D_801333E8);
-                            INV_CONTENT(ITEM_TRADE_CHILD) = GetNextChildTradeItem(globalCtx, 0);
+                            INV_CONTENT(ITEM_TRADE_CHILD) = GetNextChildTradeItem(0);
                         }
                         for (uint16_t cSlotIndex = 0; cSlotIndex < ARRAY_COUNT(gSaveContext.equips.cButtonSlots); cSlotIndex++) {
                             if (gSaveContext.equips.cButtonSlots[cSlotIndex] == SLOT_TRADE_CHILD) {
