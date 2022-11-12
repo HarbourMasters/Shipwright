1225d491e163d847e67fa0cffe2230df8a3d4899
Oliver Schall <oliverschall22@googlemail.com>
2022-11-12 20:07:30 +0100

    Added disable-switch for sliders, shooting gallery difficulty options are now disabled when customize behavior is turned off instead of hidden

--- soh/soh/GameMenuBar.cpp
+++ soh/soh/GameMenuBar.cpp
@@ -989,16 +989,16 @@ namespace GameMenuBar {
                     if (ImGui::BeginMenu("Shooting Gallery")) {
                         UIWidgets::EnhancementCheckbox("Customize Behavior", "gCustomizeShootingGallery");
                         UIWidgets::Tooltip("Turn on/off changes to the shooting gallery behavior");
-                        if (CVar_GetS32("gCustomizeShootingGallery", 0)) {
-                            UIWidgets::EnhancementCheckbox("Instant Win", "gInstantShootingGalleryWin");
-                            UIWidgets::Tooltip("Skips the shooting gallery minigame");
-                            UIWidgets::EnhancementCheckbox("No Rupee Randomization", "gConstantAdultGallery");
-                            UIWidgets::Tooltip("Forces the rupee order to not be randomized as adult, making it the same as chlid");
-                            UIWidgets::PaddedEnhancementSliderInt("Child Starting Ammunition: %d", "##cShootingGalleryAmmunition", "gChildShootingGalleryAmmunition", 10, 30, "", 15, false, true, false);
-                            UIWidgets::Tooltip("The ammunition at the start of the shooting gallery minigame as a child");
-                            UIWidgets::PaddedEnhancementSliderInt("Adult Starting Ammunition: %d", "##aShootingGalleryAmmunition", "gAdultShootingGalleryAmmunition", 10, 30, "", 15, false, true, false);
-                            UIWidgets::Tooltip("The ammunition at the start of the shooting gallery minigame as an adult");
-                        }
+                        bool disabled = CVar_GetS32("gCustomizeShootingGallery", 0) == 0;
+                        const char* disabledTooltip = "This option is disabled because \"Customize Behavior\" is turned off";
+                        UIWidgets::EnhancementCheckbox("Instant Win", "gInstantShootingGalleryWin", disabled, disabledTooltip);
+                        UIWidgets::Tooltip("Skips the shooting gallery minigame");
+                        UIWidgets::EnhancementCheckbox("No Rupee Randomization", "gConstantAdultGallery", disabled, disabledTooltip);
+                        UIWidgets::Tooltip("Forces the rupee order to not be randomized as adult, making it the same as chlid");
+                        UIWidgets::PaddedEnhancementSliderInt("Child Starting Ammunition: %d", "##cShootingGalleryAmmunition", "gChildShootingGalleryAmmunition", 10, 30, "", 15, false, true, false, disabled, disabledTooltip);
+                        UIWidgets::Tooltip("The ammunition at the start of the shooting gallery minigame as a child");
+                        UIWidgets::PaddedEnhancementSliderInt("Adult Starting Ammunition: %d", "##aShootingGalleryAmmunition", "gAdultShootingGalleryAmmunition", 10, 30, "", 15, false, true, false, disabled, disabledTooltip);
+                        UIWidgets::Tooltip("The ammunition at the start of the shooting gallery minigame as an adult");
                         ImGui::EndMenu();
                     }
 
--- soh/soh/UIWidgets.hpp
+++ soh/soh/UIWidgets.hpp
@@ -38,9 +38,9 @@ namespace UIWidgets {
     void EnhancementCombo(const std::string& name, const char* cvarName, const std::vector<std::string>& items, int defaultValue = 0);
     bool EnhancementCombobox(const char* name, const char* ComboArray[], size_t arraySize, uint8_t FirstTimeValue);
     void PaddedText(const char* text, bool padTop = true, bool padBottom = true);
-    void EnhancementSliderInt(const char* text, const char* id, const char* cvarName, int min, int max, const char* format, int defaultValue = 0, bool PlusMinusButton = false);
-    void PaddedEnhancementSliderInt(const char* text, const char* id, const char* cvarName, int min, int max, const char* format, int defaultValue = 0, bool PlusMinusButton = false, bool padTop = true, bool padBottom = true);
-    void EnhancementSliderFloat(const char* text, const char* id, const char* cvarName, float min, float max, const char* format, float defaultValue, bool isPercentage, bool PlusMinusButton = false);
+    void EnhancementSliderInt(const char* text, const char* id, const char* cvarName, int min, int max, const char* format, int defaultValue = 0, bool PlusMinusButton = false, bool disabled = false, const char* disabledTooltipText = "");
+    void PaddedEnhancementSliderInt(const char* text, const char* id, const char* cvarName, int min, int max, const char* format, int defaultValue = 0, bool PlusMinusButton = false, bool padTop = true, bool padBottom = true, bool disabled = false, const char* disabledTooltipText = "");
+    void EnhancementSliderFloat(const char* text, const char* id, const char* cvarName, float min, float max, const char* format, float defaultValue, bool isPercentage, bool PlusMinusButton = false, bool disabled = false, const char* disabledTooltipText = "");
     void EnhancementRadioButton(const char* text, const char* cvarName, int id);
 
     bool EnhancementColor(const char* text, const char* cvarName, ImVec4 ColorRGBA, ImVec4 default_colors, bool allow_rainbow = true, bool has_alpha=false, bool TitleSameLine=false);
