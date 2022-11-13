cd0909ef18ec895066c30e44fad556bfe3cd2dec
Thomas Hiller <thomas.hiller@gmx.de>
2022-11-13 13:32:56 +0100

    remove sub process

--- scripts/linux/appimage/build.sh
+++ scripts/linux/appimage/build.sh
@@ -4,4 +4,6 @@ cmake --no-warn-unused-cli -H. -Bbuild-cmake -GNinja -DCMAKE_BUILD_TYPE:STRING=R
 cmake --build build-cmake --target OTRGui --
 cmake --build build-cmake --config Release --
 
-(cd build-cmake && cpack -G External --trace-expand --debug)
+pushd
+cd build-cmake && cpack -G External --trace-expand --debug
+popd
