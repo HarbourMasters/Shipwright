7a461709502408ce1505846c5bd8b2e65a835ced
RaelCappra <rael.cappra@gmail.com>
2022-11-13 23:50:51 -0300

    Fixes most known bugs associated with FreeCam
    Co-authored-by: MelonSpeedruns <melonspeedruns@outlook.com>

--- soh/src/code/z_camera.c
+++ soh/src/code/z_camera.c
@@ -1458,8 +1458,8 @@ s32 Camera_Free(Camera* camera) {
     if (RELOAD_PARAMS) {
         OLib_Vec3fDiffToVecSphGeo(&spA8, &camera->at, &camera->eye);
 
-        camera->play->camX = spA8.yaw;
-        camera->play->camY = spA8.pitch;
+        //camera->globalCtx->camX = spA8.yaw;
+        //camera->globalCtx->camY = spA8.pitch;
 
         CameraModeValue* values = sCameraSettings[camera->setting].cameraModes[camera->mode].values;
         f32 yNormal = (1.0f + PCT(OREG(46))) - (PCT(OREG(46)) * (68.0f / playerHeight));
@@ -1483,7 +1483,7 @@ s32 Camera_Free(Camera* camera) {
 
     sCameraInterfaceFlags = 1;
 
-    camera->animState = 1;
+    camera->animState = 0;
 
     f32 newCamX = -D_8015BD7C->state.input[0].cur.right_stick_x * 10.0f * (CVar_GetFloat("gCameraSensitivity", 1.0f));
     f32 newCamY = D_8015BD7C->state.input[0].cur.right_stick_y * 10.0f * (CVar_GetFloat("gCameraSensitivity", 1.0f));
@@ -1569,6 +1569,7 @@ s32 Camera_Normal1(Camera* camera) {
 
     OLib_Vec3fDiffToVecSphGeo(&atEyeGeo, at, eye);
     OLib_Vec3fDiffToVecSphGeo(&atEyeNextGeo, at, eyeNext);
+
 
     switch (camera->animState) {
         case 0x14:
