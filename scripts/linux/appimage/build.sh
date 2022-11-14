6d6e94c5d7de964e7b69e849df78fc268d3e7ee8
Thomas Hiller <thomas.hiller@gmx.de>
2022-11-14 08:44:35 +0100

    switch to Ninja Multi-Config

--- scripts/linux/appimage/build.sh
+++ scripts/linux/appimage/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cmake --no-warn-unused-cli -H. -Bbuild-cmake -GNinja -DCMAKE_BUILD_TYPE:STRING=Release
+cmake --no-warn-unused-cli -H. -Bbuild-cmake -G"Ninja Multi-Config" -DCMAKE_BUILD_TYPE:STRING=Release
 cmake --build build-cmake --target OTRGui --
 cmake --build build-cmake --config Release --
 
 pushd
-cd build-cmake && cpack -G External --trace-expand --debug
+cd build-cmake && cpack -G External -C Release --trace-expand --debug
 popd
