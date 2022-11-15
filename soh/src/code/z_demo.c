6ba1ac3c9d83ed05b861a3cb058f98701f8716a4
Josh Bodner <30329717+jbodner09@users.noreply.github.com>
2022-11-14 17:55:51 -0800

    Add documentation

--- soh/src/code/z_demo.c
+++ soh/src/code/z_demo.c
@@ -514,6 +514,8 @@ void Cutscene_Command_Terminator(GlobalContext* globalCtx, CutsceneContext* csCt
         playCutscene = true;
     } else if (CVar_GetS32("gCreditsFix", 1)) {
         u16 delay = 0;
+        
+        // HACK:  Align visual timing with audio curing credits sequence
         switch (cmd->base) {
             case 55: // Gerudo fortress (second scene of credits roll)
                 delay = 20;
