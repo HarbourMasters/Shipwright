2be19c15d8397b5c21039e3f78aa92f560720ebb
Garrett Cox <garrettjcox@gmail.com>
2022-11-14 19:54:04 -0600

    Cosmetic Editor v3

--- soh/soh/UIWidgets.hpp
+++ soh/soh/UIWidgets.hpp
@@ -40,7 +40,7 @@ namespace UIWidgets {
     void PaddedText(const char* text, bool padTop = true, bool padBottom = true);
     void EnhancementSliderInt(const char* text, const char* id, const char* cvarName, int min, int max, const char* format, int defaultValue = 0, bool PlusMinusButton = false, bool disabled = false, const char* disabledTooltipText = "");
     void PaddedEnhancementSliderInt(const char* text, const char* id, const char* cvarName, int min, int max, const char* format, int defaultValue = 0, bool PlusMinusButton = false, bool padTop = true, bool padBottom = true, bool disabled = false, const char* disabledTooltipText = "");
-    void EnhancementSliderFloat(const char* text, const char* id, const char* cvarName, float min, float max, const char* format, float defaultValue, bool isPercentage, bool PlusMinusButton = false, bool disabled = false, const char* disabledTooltipText = "");
+    bool EnhancementSliderFloat(const char* text, const char* id, const char* cvarName, float min, float max, const char* format, float defaultValue, bool isPercentage, bool PlusMinusButton = false, bool disabled = false, const char* disabledTooltipText = "");
     void EnhancementRadioButton(const char* text, const char* cvarName, int id);
 
     bool EnhancementColor(const char* text, const char* cvarName, ImVec4 ColorRGBA, ImVec4 default_colors, bool allow_rainbow = true, bool has_alpha=false, bool TitleSameLine=false);
--- soh/src/overlays/actors/ovl_Bg_Haka/z_bg_haka.h
+++ soh/src/overlays/actors/ovl_Bg_Haka/z_bg_haka.h
@@ -11,6 +11,7 @@ typedef void (*BgHakaActionFunc)(struct BgHaka*, PlayState*);
 typedef struct BgHaka {
     /* 0x0000 */ DynaPolyActor dyna;
     /* 0x0164 */ BgHakaActionFunc actionFunc;
+    /*        */ u8 state;
 } BgHaka; // size = 0x0168
 
 #endif
