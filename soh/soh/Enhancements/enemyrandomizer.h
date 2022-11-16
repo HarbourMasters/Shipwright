5f2075e082bcf1876f13b8fd861a8d585cd61acc
aMannus <mannusmenting@gmail.com>
2022-11-16 23:25:03 +0100

    (╯°□°)╯︵ ┻━┻

--- soh/soh/Enhancements/enemyrandomizer.h
+++ soh/soh/Enhancements/enemyrandomizer.h
@@ -1,29 +1,29 @@
 #pragma once
 
-#include <global.h>
+#include <stdint.h>
 
 typedef struct EnemyEntry {
-    s16 id;
-    s16 params;
+    int16_t id;
+    int16_t params;
 } EnemyEntry;
 
 typedef struct RandomizedEnemy {
-    s16 id;
-    f32 posX;
-    f32 posY;
-    f32 posZ;
-    s16 rotX;
-    s16 rotY;
-    s16 rotZ;
-    s16 params;
+    int16_t id;
+    float posX;
+    float posY;
+    float posZ;
+    int16_t rotX;
+    int16_t rotY;
+    int16_t rotZ;
+    int16_t params;
 } RandomizedEnemy;
 
 #define RANDOMIZED_ENEMY_SPAWN_TABLE_SIZE 49
 
-uint8_t IsEnemyFoundToRandomize(s16 sceneNum, s16 actorId, s16 param, f32 posX);
-uint8_t IsEnemyAllowedToSpawn(s16 sceneNum, EnemyEntry newEnemy);
-EnemyEntry GetRandomizedEnemyEntry(f32 seed1, f32 seed2, f32 seed3);
+uint8_t IsEnemyFoundToRandomize(int16_t sceneNum, int16_t actorId, int16_t params, float posX);
+uint8_t IsEnemyAllowedToSpawn(int16_t sceneNum, EnemyEntry enemy);
+EnemyEntry GetRandomizedEnemyEntry(float seed1, float seed2, float seed3);
 
 #ifndef __cplusplus
-RandomizedEnemy GetRandomizedEnemy(PlayState* play, s16 actorId, f32 posX, f32 posY, f32 posZ, s16 rotX, s16 rotY, s16 rotZ, s16 params);
+RandomizedEnemy GetRandomizedEnemy(PlayState* play, int16_t actorId, f32 posX, f32 posY, f32 posZ, int16_t rotX, int16_t rotY, int16_t rotZ, int16_t params);
 #endif
