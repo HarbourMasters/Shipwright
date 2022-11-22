cd56168eb943250a52561c2b7f421033eb330d96
Sarge-117 <adam_branston@outlook.com>
2022-11-21 16:36:26 -0800

    Stop counting button presses when Ganon defeated

--- soh/soh/Enhancements/gameplaystats.h
+++ soh/soh/Enhancements/gameplaystats.h
@@ -11,16 +11,16 @@ typedef enum {
     // 0x00 to 0x9B (0 to 155) used for getting items,
     // piggybacked off enum "ItemID" in z64item.h
 
-    /* 0xA0 */ TIMESTAMP_DEFEAT_GOHMA = 0xA0,
-    /* 0xA1 */ TIMESTAMP_DEFEAT_KING_DODONGO,
-    /* 0xA2 */ TIMESTAMP_DEFEAT_BARINADE,
-    /* 0xA3 */ TIMESTAMP_DEFEAT_PHANTOM_GANON,
-    /* 0xA4 */ TIMESTAMP_DEFEAT_VOLVAGIA,
-    /* 0xA5 */ TIMESTAMP_DEFEAT_MORPHA,
-    /* 0xA6 */ TIMESTAMP_DEFEAT_BONGO_BONGO,
-    /* 0xA7 */ TIMESTAMP_DEFEAT_TWINROVA,
-    /* 0xA8 */ TIMESTAMP_DEFEAT_GANONDORF,
-    /* 0xA9 */ TIMESTAMP_DEFEAT_GANON,
+    /* 0xA0 */ TIMESTAMP_DEFEAT_GOHMA = 0xA0,   // z_boss_goma.c
+    /* 0xA1 */ TIMESTAMP_DEFEAT_KING_DODONGO,   // z_boss_dodongo.c
+    /* 0xA2 */ TIMESTAMP_DEFEAT_BARINADE,       // z_boss_va.c
+    /* 0xA3 */ TIMESTAMP_DEFEAT_PHANTOM_GANON,  // z_boss_ganondrof.c
+    /* 0xA4 */ TIMESTAMP_DEFEAT_VOLVAGIA,       // z_boss_fd2.c
+    /* 0xA5 */ TIMESTAMP_DEFEAT_MORPHA,         // z_boss_mo.c
+    /* 0xA6 */ TIMESTAMP_DEFEAT_BONGO_BONGO,    // z_boss_sst.c
+    /* 0xA7 */ TIMESTAMP_DEFEAT_TWINROVA,       // z_boss_tw.c
+    /* 0xA8 */ TIMESTAMP_DEFEAT_GANONDORF,      // z_boss_ganon.c
+    /* 0xA9 */ TIMESTAMP_DEFEAT_GANON,          // z_boss_ganon2.c
     /* 0xAA */ TIMESTAMP_MAX
 
 }GameplayStatTimestamp;
