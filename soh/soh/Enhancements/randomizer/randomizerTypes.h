0424c5aaf3f3f55d390d1045bd001bb1e8fe98b6
briaguya <70942617+briaguya-ai@users.noreply.github.com>
2022-11-28 16:41:33 -0500

    fix: rando door of time enum usage (#2032)

--- soh/soh/Enhancements/randomizer/randomizerTypes.h
+++ soh/soh/Enhancements/randomizer/randomizerTypes.h
@@ -1085,10 +1085,10 @@ typedef enum RandoOption {
   RO_FOREST_CLOSED_DEKU,
   RO_FOREST_OPEN,
 
-  //Door of Time settings (open, song only, closed)
-  RO_DOOROFTIME_OPEN = 0,
+  //Door of Time settings (closed, song only, open)
+  RO_DOOROFTIME_CLOSED = 0,
   RO_DOOROFTIME_SONGONLY,
-  RO_DOOROFTIME_CLOSED,
+  RO_DOOROFTIME_OPEN,
   
   //Zora's Fountain settings (closed, closed as child, open)
   RO_ZF_CLOSED = 0,
