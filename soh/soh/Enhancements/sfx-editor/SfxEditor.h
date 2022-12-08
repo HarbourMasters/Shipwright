c62e93e1aa9c64a4a1c71d26d95f1de0c6af2b12
Christopher Leggett <chris@leggett.dev>
2022-12-08 13:54:38 -0500

    fixes formatting in SfxEditor.h
    Co-authored-by: briaguya <70942617+briaguya-ai@users.noreply.github.com>

--- soh/soh/Enhancements/sfx-editor/SfxEditor.h
+++ soh/soh/Enhancements/sfx-editor/SfxEditor.h
@@ -9,15 +9,15 @@ void SfxEditor_AddSequence(char *otrPath, uint16_t seqNum);
 #define INSTRUMENT_OFFSET 0x81
 #define MAX_AUTHENTIC_SEQID 110
 
-    enum SeqType {
-        SEQ_NOSHUFFLE = 0,
-        SEQ_BGM_WORLD = 1 << 0,
-        SEQ_BGM_EVENT = 1 << 1,
-        SEQ_BGM_BATTLE = 1 << 2,
-        SEQ_OCARINA = 1 << 3,
-        SEQ_FANFARE = 1 << 4,
-        SEQ_BGM_ERROR = 1 << 5,
-        SEQ_SFX = 1 << 6,
-        SEQ_INSTRUMENT = 1 << 7,
-        SEQ_BGM_CUSTOM = SEQ_BGM_WORLD | SEQ_BGM_EVENT | SEQ_BGM_BATTLE,
-    };
+enum SeqType {
+    SEQ_NOSHUFFLE  = 0,
+    SEQ_BGM_WORLD  = 1 << 0,
+    SEQ_BGM_EVENT  = 1 << 1,
+    SEQ_BGM_BATTLE = 1 << 2,
+    SEQ_OCARINA    = 1 << 3,
+    SEQ_FANFARE    = 1 << 4,
+    SEQ_BGM_ERROR  = 1 << 5,
+    SEQ_SFX        = 1 << 6,
+    SEQ_INSTRUMENT = 1 << 7,
+    SEQ_BGM_CUSTOM = SEQ_BGM_WORLD | SEQ_BGM_EVENT | SEQ_BGM_BATTLE,
+};
