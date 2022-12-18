e41d2a7e6deee68d7b92dc55db2ae050d5472cf9
Christopher Leggett <chris@leggett.dev>
2022-12-17 20:06:52 -0500

    Re-adds the mistakenly removed const specifier.

--- soh/soh/Enhancements/sfx-editor/SfxEditor.cpp
+++ soh/soh/Enhancements/sfx-editor/SfxEditor.cpp
@@ -181,7 +181,7 @@ std::map<u16, std::tuple<std::string, std::string, SeqType>> sfxEditorSequenceMa
     {NA_SE_EV_CHICKEN_CRY_A,       {"Chicken Cry",                         "NA_SE_EV_CHICKEN_CRY_A",         SEQ_SFX}},
 };
 
-void Draw_SfxTab(const std::string& tabId, std::map<u16, std::tuple<std::string, std::string, SeqType>>& map, SeqType type) {
+void Draw_SfxTab(const std::string& tabId, const std::map<u16, std::tuple<std::string, std::string, SeqType>>& map, SeqType type) {
     const std::string hiddenTabId = "##" + tabId;
     const std::string resetAllButton = "Reset All" + hiddenTabId;
     const std::string randomizeAllButton = "Randomize All" + hiddenTabId;
