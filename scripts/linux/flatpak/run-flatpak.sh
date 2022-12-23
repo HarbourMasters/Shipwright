ef177517a1f7031d49394e3a13eb76c5ec924219
Thomas Hiller <thomas.hiller@gmx.de>
2022-12-23 10:05:40 +0100

    fix script; remove old versions

--- scripts/linux/flatpak/run-flatpak.sh
+++ scripts/linux/flatpak/run-flatpak.sh
@@ -1,7 +1,9 @@
 flatpak remote-add --if-not-exists --user flathub https://flathub.org/repo/flathub.flatpakrepo
 
+flatpak remove --user -y --noninteractive com.shipofharkinian.soh
+
 #../flatpak-builder/flatpak-builder build -v --install-deps-from=flathub --force-clean --ccache --install com.shipofharkinian.soh.yaml
 flatpak install --user -y --noninteractive flathub org.flatpak.Builder
-~/.local/share/flatpak/exports/bin/org.flatpak.Builder build -v --install-deps-from=flathub --force-clean --ccache --install --user com.shipofharkinian.soh.yaml
+~/.local/share/flatpak/exports/bin/org.flatpak.Builder build -v --install-deps-from=flathub --force-clean --ccache --install --user scripts/linux/flatpak/com.shipofharkinian.soh.yaml
 
-flatpak build-bundle ~/.local/share/flatpak/repo/ soh.flatpak com.shipofharkinian.soh Stable
+flatpak build-bundle ~/.local/share/flatpak/repo/ soh.flatpak com.shipofharkinian.soh stable
