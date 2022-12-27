eb4d66d3f98559f4fd20c4a5e260358a127694ac
briaguya <briaguya@alice>
2022-12-27 17:36:25 -0500

    extractassets generates otrs

---
+++ ZAPDTR/ZAPD/resource/type/Animation.cpp
@@ -0,0 +1,20 @@
+#include "Animation.h"
+
+namespace Ship {
+void* Animation::GetPointer() {
+    return &animationData;
+}
+
+size_t Animation::GetPointerSize() {
+    switch(type) {
+        case AnimationType::Normal:
+            return sizeof(animationData.animationHeader);
+        case AnimationType::Link:
+            return  sizeof(animationData.linkAnimationHeader);
+        case AnimationType::Curve:
+            return sizeof(animationData.transformUpdateIndex);
+        case AnimationType::Legacy:
+            return 0;
+    }
+}
+} // namespace Ship
