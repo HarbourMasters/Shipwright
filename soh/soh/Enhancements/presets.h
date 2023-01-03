a7eb19fc8196991fadd093b842e0f6a149ccecd6
Adam Bird <archez39@me.com>
2023-01-03 16:24:30 -0500

    add boss shuffle to preset clear

--- soh/soh/Enhancements/presets.h
+++ soh/soh/Enhancements/presets.h
@@ -223,6 +223,7 @@ const std::vector<const char*> randomizerCvars = {
     "gRandomizeShopsanity",
     "gRandomizeShuffleAdultTrade",
     "gRandomizeShuffleBeans",
+    "gRandomizeShuffleBossEntrances",
     "gRandomizeShuffleCows",
     "gRandomizeShuffleDungeonReward",
     "gRandomizeShuffleDungeonsEntrances",
