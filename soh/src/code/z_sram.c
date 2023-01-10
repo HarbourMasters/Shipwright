853bb1cb84bfb576db2f8897bd21ec4ff328d828
Adam Bird <archez39@me.com>
2023-01-09 20:26:53 -0500

    adjust rando locked door logic

--- soh/src/code/z_sram.c
+++ soh/src/code/z_sram.c
@@ -471,7 +471,6 @@ void Sram_InitSave(FileChooseContext* fileChooseCtx) {
         }
 
         if(Randomizer_GetSettingValue(RSK_KEYSANITY) == RO_DUNGEON_ITEM_LOC_STARTWITH) {
-            // TODO: If master quest there are different key counts
             gSaveContext.inventory.dungeonKeys[SCENE_BMORI1] = FOREST_TEMPLE_SMALL_KEY_MAX; // Forest
             gSaveContext.sohStats.dungeonKeys[SCENE_BMORI1]     = FOREST_TEMPLE_SMALL_KEY_MAX; // Forest
             gSaveContext.inventory.dungeonKeys[SCENE_HIDAN] = FIRE_TEMPLE_SMALL_KEY_MAX; // Fire
@@ -516,12 +515,23 @@ void Sram_InitSave(FileChooseContext* fileChooseCtx) {
         gSaveContext.infTable[20] |= 4;
 
         // Go away ruto (water temple first cutscene)
-        gSaveContext.sceneFlags[05].swch |= (1 << 0x10);
+        gSaveContext.sceneFlags[SCENE_MIZUSIN].swch |= (1 << 0x10);
 
-        // Opens locked Water Temple door to prevent softlocks
+        // Open lowest Vanilla Fire Temple locked door (to prevent key logic lockouts)
+        // Not done on keysanity since this lockout is a non issue when Fire keys can be found outside the temple
+        u8 keysanity = Randomizer_GetSettingValue(RSK_KEYSANITY) == RO_DUNGEON_ITEM_LOC_ANYWHERE ||
+                       Randomizer_GetSettingValue(RSK_KEYSANITY) == RO_DUNGEON_ITEM_LOC_OVERWORLD ||
+                       Randomizer_GetSettingValue(RSK_KEYSANITY) == RO_DUNGEON_ITEM_LOC_ANY_DUNGEON;
+        if (!ResourceMgr_IsSceneMasterQuest(SCENE_HIDAN) && !keysanity) {
+            gSaveContext.sceneFlags[SCENE_HIDAN].swch |= (1 << 0x17);
+        }
+
+        // Opens locked Water Temple door in vanilla to prevent softlocks
         // West door on the middle level that leads to the water raising thing
         // Happens in 3DS rando and N64 rando as well
-        gSaveContext.sceneFlags[05].swch |= (1 << 0x15);
+        if (!ResourceMgr_IsSceneMasterQuest(SCENE_MIZUSIN)) {
+            gSaveContext.sceneFlags[SCENE_MIZUSIN].swch |= (1 << 0x15);
+        }
 
         // Skip intro cutscene when bombing mud wall in Dodongo's cavern
         // this also makes the lower jaw render, and the eyes react to explosives
