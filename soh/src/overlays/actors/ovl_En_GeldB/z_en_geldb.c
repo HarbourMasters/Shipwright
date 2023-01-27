e6aa03ba51619d22a5ef44d3efe3e7810eaea162
Adam Bird <archez39@me.com>
2023-01-27 15:12:06 -0500

    add entrance include

--- soh/src/overlays/actors/ovl_En_GeldB/z_en_geldb.c
+++ soh/src/overlays/actors/ovl_En_GeldB/z_en_geldb.c
@@ -6,6 +6,7 @@
 
 #include "z_en_geldb.h"
 #include "objects/object_geldb/object_geldb.h"
+#include "soh/Enhancements/randomizer/randomizer_entrance.h"
 
 #define FLAGS (ACTOR_FLAG_0 | ACTOR_FLAG_2 | ACTOR_FLAG_4)
 
