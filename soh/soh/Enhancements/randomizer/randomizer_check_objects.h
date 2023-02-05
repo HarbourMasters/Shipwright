c209b4dd2694ffa9a1b0445083f08f6779825891
Adam Bird <archez39@me.com>
2023-02-05 12:20:10 -0500

    add more exclude locations and fix broken ones

--- soh/soh/Enhancements/randomizer/randomizer_check_objects.h
+++ soh/soh/Enhancements/randomizer/randomizer_check_objects.h
@@ -25,6 +25,10 @@ typedef enum {
     RCTYPE_CHEST_GAME, //todo replace this once we implement it, just using it to exclude for now
     RCTYPE_LINKS_POCKET, //todo this feels hacky
     RCTYPE_GOSSIP_STONE,
+    RCTYPE_SONG_LOCATION, // Song locations
+    RCTYPE_SONG_REWARD, // Song dungeon rewards
+    RCTYPE_DUNGEON_REWARD, // Dungeon rewards
+    RCTYPE_OCARINA, // Ocarina locations
 } RandomizerCheckType;
 
 typedef enum {
