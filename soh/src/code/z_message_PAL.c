84f1c9480b9bbb0f194d7c3048d219e789b9b9da
David Chavez <david@dcvz.io>
2023-02-16 15:11:03 +0100

    Handle messages with choices

--- soh/src/code/z_message_PAL.c
+++ soh/src/code/z_message_PAL.c
@@ -3071,17 +3071,39 @@ void Message_Draw(PlayState* play) {
     CLOSE_DISPS(play->state.gfxCtx);
 }
 
-static uint8_t ttsHasNewMessage;
-static uint8_t ttsMessageBuf[256];
-
-void Message_TTS_Decode(uint8_t* sourceBuf, uint8_t* destBuf, uint16_t size) {
+void Message_TTS_Decode(uint8_t* sourceBuf, uint8_t* destBuf, uint16_t startOfset, uint16_t size) {
     uint32_t destWriteIndex = 0;
+    uint8_t isListingChoices = 0;
     
     for (uint16_t i = 0; i < size; i++) {
-        uint8_t cchar = sourceBuf[i];
+        uint8_t cchar = sourceBuf[i + startOfset];
         
         if (cchar < ' ') {
-            
+            switch (cchar) {
+                case MESSAGE_NEWLINE:
+                    destBuf[destWriteIndex++] = (isListingChoices) ? '\n' : ' ';
+                    break;
+                case MESSAGE_THREE_CHOICE:
+                case MESSAGE_TWO_CHOICE:
+                    destBuf[destWriteIndex++] = '\n';
+                    isListingChoices = 1;
+                    break;
+                case MESSAGE_COLOR:
+                case MESSAGE_SHIFT:
+                case MESSAGE_TEXT_SPEED:
+                case MESSAGE_BOX_BREAK_DELAYED:
+                case MESSAGE_FADE:
+                case MESSAGE_ITEM_ICON:
+                    i++;
+                    break;
+                case MESSAGE_FADE2:
+                case MESSAGE_SFX:
+                case MESSAGE_TEXTID:
+                    i += 2;
+                    break;
+                default:
+                    break;
+            }
         } else {
             destBuf[destWriteIndex++] = cchar;
         }
@@ -3089,6 +3111,11 @@ void Message_TTS_Decode(uint8_t* sourceBuf, uint8_t* destBuf, uint16_t size) {
     
     destBuf[destWriteIndex] = 0;
 }
+
+static uint8_t ttsHasMessage;
+static uint8_t ttsHasNewMessage;
+static uint8_t ttsMessageBuf[256];
+static int8_t ttsCurrentHighlightedChoice;
 
 void Message_TTS_Update(PlayState* play) {
     MessageContext *msgCtx = &play->msgCtx;
@@ -3102,16 +3129,60 @@ void Message_TTS_Update(PlayState* play) {
                || msgCtx->msgMode == MSGMODE_DISPLAY_SONG_PLAYED_TEXT || msgCtx->msgMode == MSGMODE_DISPLAY_SONG_PLAYED_TEXT || msgCtx->msgMode == MSGMODE_SONG_PLAYED_ACT_BEGIN || msgCtx->msgMode == MSGMODE_SONG_PLAYED_ACT || msgCtx->msgMode == MSGMODE_SONG_PLAYBACK_STARTING || msgCtx->msgMode == MSGMODE_SONG_PLAYBACK || msgCtx->msgMode == MSGMODE_SONG_DEMONSTRATION_STARTING || msgCtx->msgMode == MSGMODE_SONG_DEMONSTRATION_SELECT_INSTRUMENT || msgCtx->msgMode == MSGMODE_SONG_DEMONSTRATION
     ) {
         if (ttsHasNewMessage) {
+            ttsHasMessage = 1;
             ttsHasNewMessage = 0;
+            ttsCurrentHighlightedChoice = 0;
             
             uint16_t size = msgCtx->decodedTextLen;
-            Message_TTS_Decode(msgCtx->msgBufDecoded, ttsMessageBuf, size);
+            Message_TTS_Decode(msgCtx->msgBufDecoded, ttsMessageBuf, 0, size);
             SpeechSynthesizerSpeak(ttsMessageBuf);
+        } else if (msgCtx->msgMode == MSGMODE_TEXT_DONE && msgCtx->choiceNum > 0 && msgCtx->choiceIndex != ttsCurrentHighlightedChoice) {
+            ttsCurrentHighlightedChoice = msgCtx->choiceIndex;
+            uint16_t startOffset = 0;
+            uint16_t endOffset = 0;
+            
+            while (startOffset < msgCtx->decodedTextLen) {
+                if (msgCtx->msgBufDecoded[startOffset] == MESSAGE_TWO_CHOICE || msgCtx->msgBufDecoded[startOffset] == MESSAGE_THREE_CHOICE) {
+                    startOffset++;
+                    break;
+                }
+                startOffset++;
+            }
+            
+            if (startOffset < msgCtx->decodedTextLen) {
+                uint8_t i = msgCtx->choiceIndex;
+                while (i-- > 0) {
+                    while (startOffset < msgCtx->decodedTextLen) {
+                        if (msgCtx->msgBufDecoded[startOffset] == MESSAGE_NEWLINE) {
+                            startOffset++;
+                            break;
+                        }
+                        startOffset++;
+                    }
+                }
+                
+                endOffset = startOffset;
+                while (endOffset < msgCtx->decodedTextLen) {
+                    if (msgCtx->msgBufDecoded[endOffset] == MESSAGE_NEWLINE) {
+                        break;
+                    }
+                    endOffset++;
+                }
+                
+                if (startOffset < msgCtx->decodedTextLen && startOffset != endOffset) {
+                    uint16_t size = endOffset - startOffset;
+                    Message_TTS_Decode(msgCtx->msgBufDecoded, ttsMessageBuf, startOffset, size);
+                    SpeechSynthesizerSpeak(ttsMessageBuf);
+                }
+            }
         }
-    } else if (ttsHasNewMessage) {
+    } else if (ttsHasMessage) {
+        ttsHasMessage = 0;
         ttsHasNewMessage = 0;
         
-        // TODO: cancel TTS?
+        if (msgCtx->decodedTextLen < 3 || (msgCtx->msgBufDecoded[msgCtx->decodedTextLen - 2] != MESSAGE_FADE && msgCtx->msgBufDecoded[msgCtx->decodedTextLen - 3] != MESSAGE_FADE2)) {
+            SpeechSynthesizerSpeak(""); // cancel current speech (except for faded out messages)
+        }
     }
 }
 
