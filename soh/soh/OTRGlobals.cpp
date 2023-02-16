1e6ec1bddadfb75ad0d57a44255bf62ff729a784
briaguya <70942617+briaguya-ai@users.noreply.github.com>
2023-02-16 09:49:45 -0500

    exclude audio from shuffle (#2411)
    * Rename "SFX Editor" to "Audio Editor"
    * Move some functionality out into a new class `AudioCollection`
    * Add a tab to exclude sfx/sequences from shuffle pool 
    
    ---------
    
    Co-authored-by: briaguya <briaguya@alice>
    Co-authored-by: briaguya <briaguya>
    Co-authored-by: David Chavez <david@dcvz.io>

--- soh/soh/OTRGlobals.cpp
+++ soh/soh/OTRGlobals.cpp
@@ -29,7 +29,8 @@
 #include <AudioPlayer.h>
 #include "Enhancements/controls/GameControlEditor.h"
 #include "Enhancements/cosmetics/CosmeticsEditor.h"
-#include "Enhancements/sfx-editor/SfxEditor.h"
+#include "Enhancements/audio/AudioCollection.h"
+#include "Enhancements/audio/AudioEditor.h"
 #include "Enhancements/debugconsole.h"
 #include "Enhancements/debugger/debugger.h"
 #include "Enhancements/randomizer/randomizer.h"
@@ -109,6 +110,7 @@ SaveManager* SaveManager::Instance;
 CustomMessageManager* CustomMessageManager::Instance;
 ItemTableManager* ItemTableManager::Instance;
 GameInteractor* GameInteractor::Instance;
+AudioCollection* AudioCollection::Instance;
 
 extern "C" char** cameraStrings;
 std::vector<std::shared_ptr<std::string>> cameraStdStrings;
@@ -568,13 +570,14 @@ extern "C" void InitOTR() {
     CustomMessageManager::Instance = new CustomMessageManager();
     ItemTableManager::Instance = new ItemTableManager();
     GameInteractor::Instance = new GameInteractor();
+    AudioCollection::Instance = new AudioCollection();
 
     clearMtx = (uintptr_t)&gMtxClear;
     OTRMessage_Init();
     OTRAudio_Init();
     InitCosmeticsEditor();
     GameControlEditor::Init();
-    InitSfxEditor();
+    InitAudioEditor();
     DebugConsole_Init();
     Debug_Init();
     Rando_Init();
