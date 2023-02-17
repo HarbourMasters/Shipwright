bc6449cfdd389e479a1f445080bbb28ac998f8b3
Pepe20129 <72659707+Pepe20129@users.noreply.github.com>
2023-02-17 19:27:15 +0100

    Add bugs don't despawn cheat

--- soh/soh/GameMenuBar.cpp
+++ soh/soh/GameMenuBar.cpp
@@ -1079,6 +1079,8 @@ namespace GameMenuBar {
             UIWidgets::Tooltip("This syncs the ingame time with the real world time");
             UIWidgets::PaddedEnhancementCheckbox("Fish don't despawn", "gNoFishDespawn", true, false);
             UIWidgets::Tooltip("Prevents fish from automatically despawning after a while when dropped");
+            UIWidgets::PaddedEnhancementCheckbox("Bugs don't despawn", "gNoBugsDespawn", true, false);
+            UIWidgets::Tooltip("Prevents bugs from automatically despawning after a while when dropped");
 
             {
                 static int32_t betaQuestEnabled = CVarGetInteger("gEnableBetaQuest", 0);
