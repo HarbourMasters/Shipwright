6e4b1aec2a6cfeb486b3bd801996302fd90b6dee
Adam Bird <Archez@users.noreply.github.com>
2023-02-23 01:53:18 -0500

    fix excluded locations being removed if you haven't viewed the location tab (#2518)

--- soh/soh/Enhancements/randomizer/randomizer.cpp
+++ soh/soh/Enhancements/randomizer/randomizer.cpp
@@ -2950,6 +2950,9 @@ void GenerateRandomizerImgui(std::string seed = "") {
         excludedLocations.insert((RandomizerCheck)std::stoi(excludedLocationString));
     }
 
+    // Update the visibilitiy before removing conflicting excludes (in case the locations tab wasn't viewed)
+    RandomizerCheckObjects::UpdateImGuiVisibility();
+
     // Remove excludes for locations that are no longer allowed to be excluded
     for (auto [randomizerCheck, rcObject] : RandomizerCheckObjects::GetAllRCObjects()) {
         auto elfound = excludedLocations.find(rcObject.rc);
