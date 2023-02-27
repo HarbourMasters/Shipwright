f7bb80794017d1f8a21e93be3f8f4c155f037ebf
Adam Bird <Archez@users.noreply.github.com>
2023-02-26 23:04:57 -0500

    [Game Interactor] Add LoadGame and ExitGame hooks (#2542)
    * add loadgame/exitgame GI hooks
    
    * implement loadgame/exitgame hooks in game code
    
    * move entrance tracker data lifecycle to hooks
    
    * update cosmetic editor to update onloadgame hook

--- soh/soh/Enhancements/game-interactor/GameInteractor.h
+++ soh/soh/Enhancements/game-interactor/GameInteractor.h
@@ -84,13 +84,15 @@ public:
         }
     }
 
+    DEFINE_HOOK(OnLoadGame, void(int32_t fileNum));
+    DEFINE_HOOK(OnExitGame, void(int32_t fileNum));
     DEFINE_HOOK(OnReceiveItem, void(u8 item));
     DEFINE_HOOK(OnSceneInit, void(s16 sceneNum));
     
     
-    DEFINE_HOOK(OnSaveFile, void(int fileNum));
-    DEFINE_HOOK(OnLoadFile, void(int fileNum));
-    DEFINE_HOOK(OnDeleteFile, void(int fileNum));
+    DEFINE_HOOK(OnSaveFile, void(int32_t fileNum));
+    DEFINE_HOOK(OnLoadFile, void(int32_t fileNum));
+    DEFINE_HOOK(OnDeleteFile, void(int32_t fileNum));
 
     // Helpers
     static bool IsSaveLoaded();
