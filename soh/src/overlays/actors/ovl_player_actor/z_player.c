b549706e2baf905eaec67305b705013ebba54a51
Kenix3 <kenixwhisperwind@gmail.com>
2023-03-07 23:46:44 -0500

    Update soh/src/overlays/actors/ovl_player_actor/z_player.c

--- soh/src/overlays/actors/ovl_player_actor/z_player.c
+++ soh/src/overlays/actors/ovl_player_actor/z_player.c
@@ -348,7 +348,6 @@ static s32 D_80858AA4;
 static Vec3f D_80858AA8;
 static Input* sControlInput;
 
-
 // .data
 
 static u8 D_80853410[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
