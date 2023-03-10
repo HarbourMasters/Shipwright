9049484f5d43dfe99536bdcca21aa63739594396
aMannus <mannusmenting@gmail.com>
2023-03-10 15:31:10 +0100

    Small code cleanup

--- soh/src/overlays/actors/ovl_En_Clear_Tag/z_en_clear_tag.c
+++ soh/src/overlays/actors/ovl_En_Clear_Tag/z_en_clear_tag.c
@@ -477,9 +477,7 @@ void EnClearTag_Update(Actor* thisx, PlayState* play2) {
 
                     // Introduce a range requirement in Enemy Rando so Arwings don't shoot the player from
                     // across the map. Especially noticeable in big maps like Lake Hylia and Hyrule Field.
-                    uint8_t enemyRandoShootLaser =
-                        !CVarGetInteger("gRandomizedEnemies", 0) ||
-                        (CVarGetInteger("gRandomizedEnemies", 0) && this->actor.xzDistToPlayer < 1000.0f);
+                    uint8_t enemyRandoShootLaser = !CVarGetInteger("gRandomizedEnemies", 0) || this->actor.xzDistToPlayer < 1000.0f;
 
                     // Check if the Arwing should fire its laser.
                     if ((this->frameCounter % 4) == 0 && (Rand_ZeroOne() < 0.75f) &&
