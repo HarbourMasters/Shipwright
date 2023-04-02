04d0cd8532c70143511fd09cfca8a1091cc31d4b
Malkierian <malkierian@gmail.com>
2023-04-02 01:47:23 -0700

    Adds `OnTransitionEnd` hook to `GameInteractor`, called at the end of a transition after all mid-transition setup is called. (#2635)
    Transitions final autosave location to `OnTransitionEnd`.
    
    Adds functionality to autosave to set a delayed save flag if AutoSave is set to Major or All Items and one is obtained in a grotto, to avoid grotto autosaving but still provide for the autosave when location-based saving is off.
    
    Fixes small bug with item lookup where sometimes an item's `modIndex` would sometimes be reported one way, but the way Randomizer does things it would be in a the other `modIndex`, and the lookup would fail.
    
    Minor variable name clarification in ItemTableManager.
    
    Modifies AutoSave to account for item ID overlap from `MOD_RANDOMIZER` table (all items in the randomizer table is considered major for AutoSave purposes except ice traps).

--- soh/soh/Enhancements/game-interactor/GameInteractor.h
+++ soh/soh/Enhancements/game-interactor/GameInteractor.h
@@ -146,6 +146,7 @@ public:
     DEFINE_HOOK(OnGameFrameUpdate, void());
     DEFINE_HOOK(OnItemReceive, void(GetItemEntry itemEntry));
     DEFINE_HOOK(OnSaleEnd, void(GetItemEntry itemEntry));
+    DEFINE_HOOK(OnTransitionEnd, void(int16_t sceneNum));
     DEFINE_HOOK(OnSceneInit, void(int16_t sceneNum));
     DEFINE_HOOK(OnPlayerUpdate, void());
     DEFINE_HOOK(OnActorUpdate, void(void* actor));
--- soh/soh/Enhancements/game-interactor/GameInteractor_Hooks.h
+++ soh/soh/Enhancements/game-interactor/GameInteractor_Hooks.h
@@ -6,6 +6,7 @@ extern "C" void GameInteractor_ExecuteOnExitGame(int32_t fileNum);
 extern "C" void GameInteractor_ExecuteOnGameFrameUpdate();
 extern "C" void GameInteractor_ExecuteOnItemReceiveHooks(GetItemEntry itemEntry);
 extern "C" void GameInteractor_ExecuteOnSaleEndHooks(GetItemEntry itemEntry);
+extern "C" void GameInteractor_ExecuteOnTransitionEndHooks(int16_t sceneNum);
 extern "C" void GameInteractor_ExecuteOnSceneInit(int16_t sceneNum);
 extern "C" void GameInteractor_ExecuteOnPlayerUpdate();
 extern "C" void GameInteractor_ExecuteOnActorUpdate(void* actor);
