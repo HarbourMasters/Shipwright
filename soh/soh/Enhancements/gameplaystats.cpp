9ce5ce42b6d8cce03383ac8620955a884a17e5d6
Ralphie Morell <rafael.morell@techfield.us>
2023-04-06 22:25:53 -0400

    More solid definition of upper bound

--- soh/soh/Enhancements/gameplaystats.cpp
+++ soh/soh/Enhancements/gameplaystats.cpp
@@ -251,7 +251,7 @@ std::string ResolveSceneID(int sceneID, int roomID){
     } else if (sceneID == SCENE_HAKASITARELAY) {
         //Only the last room of Dampe's Grave (rm 6) is considered the windmill
         scene = roomID == 6 ? "Windmill" : "Dampe's Grave";
-    } else if (sceneID < sceneMappings.size()) {
+    } else if (sceneID < SCENE_ID_MAX) {
         scene = sceneMappings[sceneID];
     } else {
         scene = "???";
