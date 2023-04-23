046323bfff7e4f8abe74c1f5917a83b787119f29
RaelCappra <rael.cappra@gmail.com>
2023-04-22 22:18:45 -0300

    Tooltip clarity update

--- soh/soh/GameMenuBar.cpp
+++ soh/soh/GameMenuBar.cpp
@@ -403,7 +403,7 @@ namespace GameMenuBar {
                     UIWidgets::PaddedEnhancementCheckbox("Prevent Bombchus Forcing First-Person", "gDisableFirstPersonChus", true, false);
                     UIWidgets::Tooltip("Prevent bombchus from forcing the camera into first-person mode when released.");
                     UIWidgets::PaddedEnhancementCheckbox("Aiming reticle for the bow/slingshot", "gBowReticle", true, false);
-                    UIWidgets::Tooltip("Aiming with a bow or slingshot will display a reticle as with the hookshot.");
+                    UIWidgets::Tooltip("Aiming with a bow or slingshot will display a reticle as with the hookshot when the projectile is ready to fire.");
                     ImGui::EndMenu();
                 }
 
