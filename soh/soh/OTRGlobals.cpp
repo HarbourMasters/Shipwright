d1c930b5e56b68a5ca21bd2e2e566974cc916930
briaguya <70942617+briaguya-ai@users.noreply.github.com>
2023-05-10 21:32:15 -0400

    reserved thread count (#2868)
    * reserved thread count
    
    * add comment, use latest main
    
    ---------
    
    Co-authored-by: briaguya <briaguya@alice>

--- soh/soh/OTRGlobals.cpp
+++ soh/soh/OTRGlobals.cpp
@@ -240,7 +240,8 @@ OTRGlobals::OTRGlobals() {
         OOT_PAL_GC_DBG1,
         OOT_PAL_GC_DBG2
     };
-    context = LUS::Context::CreateInstance("Ship of Harkinian", "soh", OTRFiles);
+    // tell LUS to reserve 3 SoH specific threads (Game, Audio, Save)
+    context = LUS::Context::CreateInstance("Ship of Harkinian", "soh", OTRFiles, {}, 3);
 
     context->GetResourceManager()->GetResourceLoader()->RegisterResourceFactory(LUS::ResourceType::SOH_Animation, "Animation", std::make_shared<LUS::AnimationFactory>());
     context->GetResourceManager()->GetResourceLoader()->RegisterResourceFactory(LUS::ResourceType::SOH_PlayerAnimation, "PlayerAnimation", std::make_shared<LUS::PlayerAnimationFactory>());
