5488b8a98e4264e35f887b75dfb5a436175d9038
Ryanoberlein <bobaboooooye3@gmail.com>
2023-05-25 00:15:53 -0400

    Added more actors

--- soh/soh/Enhancements/accessible-actors/AccessibleActorList.cpp
+++ soh/soh/Enhancements/accessible-actors/AccessibleActorList.cpp
@@ -15,12 +15,20 @@ void accessible_en_NPC_Gen(AccessibleActor* actor) {
 
 }
 
+void accessible_en_gerudo(AccessibleActor* actor) {
+    ActorAccessibility_PlaySpecialSound(actor, NA_SE_VO_NB_LAUGH);//update sound for ones that detect you
+}
+
 void accessible_en_Sign(AccessibleActor* actor) {
     ActorAccessibility_PlaySpecialSound(actor, NA_SE_IT_REFLECTION_WOOD);
 }
 
 void accessible_en_pickups(AccessibleActor* actor) {
     ActorAccessibility_PlaySpecialSound(actor, NA_SE_EN_NUTS_DAMAGE);
+}
+
+void accessible_test(AccessibleActor* actor) {
+    ActorAccessibility_PlaySpecialSound(actor, NA_SE_EV_DROP_FALL);
 }
 
 void ActorAccessibility_Init() {
@@ -35,8 +43,16 @@ void ActorAccessibility_Init() {
     ActorAccessibility_AddSupportedActor(ACTOR_EN_KANBAN, "Sign", accessible_en_Sign, 40, 1000, 1.6);
     ActorAccessibility_AddSupportedActor(ACTOR_EN_A_OBJ, "Sign", accessible_en_Sign, 40, 1000, 1.6);
     // will probably just get replaced with ghost actors anyways
-    // ActorAccessibility_AddSupportedActor(ACTOR_EN_HOLL, "Room Changing Plane", NULL, 30, 1000, 1.0, 1.0,
+    // ActorAccessibility_AddSupporte dActor(ACTOR_EN_HOLL, "Room Changing Plane", NULL, 30, 1000, 1.0, 1.0,
     //                                      NA_SE_EV_STONEDOOR_STOP /*NOT SURE YET*/);
-    ActorAccessibility_AddSupportedActor(ACTOR_OBJ_BEAN, "Bean patch", NULL, 60, 1000, 1.3, 1.0, NA_SE_EN_MUSI_WALK);
+    ActorAccessibility_AddSupportedActor(ACTOR_OBJ_BEAN, "Bean patch", NULL, 60, 2400, 1.3, 1.0,
+                                         NA_SE_EN_MUSI_SINK); 
     ActorAccessibility_AddSupportedActor(ACTOR_EN_ITEM00, "Collectible", accessible_en_pickups, 40, 1000, 1.4);
+    ActorAccessibility_AddSupportedActor(ACTOR_EN_GS, "Gossip Stone", accessible_en_NPC_Gen, Npc_Frames, 1000, 0.5);
+    ActorAccessibility_AddSupportedActor(ACTOR_EN_GE1, "Gerudo Guard", accessible_en_gerudo, Npc_Frames, 1000, 1.1);
+    ActorAccessibility_AddSupportedActor(ACTOR_EN_SKJ, "Skull Kid", accessible_en_NPC_Gen, Npc_Frames, 1000,
+                                         1.1); // TODO Different sound if params==7167 or 6143(skullkid deku game platform)
+    ActorAccessibility_AddSupportedActor(ACTOR_OBJ_BOMBIWA, "Brown Bombable Rock", accessible_en_ishi, 30, 1000, 0.7);//Improve?
+    //needs to only play while open
+    //ActorAccessibility_AddSupportedActor(ACTOR_DOOR_ANA, "Grotto Door", accessible_test, 30, 1000, 1.0); 
 }
