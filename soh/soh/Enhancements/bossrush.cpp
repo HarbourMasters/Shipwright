abfe2927f423dd716cbbabee9fdaaeca82adb747
aMannus <mannusmenting@gmail.com>
2023-05-26 16:37:20 +0200

    Offset final BR time by 0.1 second from boss timestamps

--- soh/soh/Enhancements/bossrush.cpp
+++ soh/soh/Enhancements/bossrush.cpp
@@ -258,6 +258,7 @@ extern "C" void BossRush_HandleCompleteBoss(PlayState* play) {
 
     if ((CheckDungeonCount() == 3 && gSaveContext.bossRushOptions[BR_OPTIONS_BOSSES] == BR_CHOICE_BOSSES_CHILD) ||
         play->sceneNum == SCENE_GANON_DEMO) {
+        gSaveContext.sohStats.playTimer += 2;
         gSaveContext.sohStats.gameComplete = 1;
         gSaveContext.sohStats.itemTimestamp[TIMESTAMP_BOSSRUSH_FINISH] = GAMEPLAYSTAT_TOTAL_TIME;
     }
