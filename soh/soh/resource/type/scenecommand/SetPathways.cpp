e5670c33d285f4969f7ebb2d24f6ec777f26f896
Kenix <kenixwhisperwind@gmail.com>
2023-05-29 00:07:15 -0400

    Fixes issue in SetPathways::GetPointerSize

--- soh/soh/resource/type/scenecommand/SetPathways.cpp
+++ soh/soh/resource/type/scenecommand/SetPathways.cpp
@@ -6,6 +6,6 @@ PathData** SetPathways::GetPointer() {
 }
 
 size_t SetPathways::GetPointerSize() {
-    return paths.size() * sizeof(std::shared_ptr<LUS::Path>);
+    return paths.size() * sizeof(PathData*);
 }
 } // namespace LUS
