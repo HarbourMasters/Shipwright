b240e2bac9976b61a6b132328f01686a4933287c
briaguya <briaguya@alice>
2023-06-11 06:15:48 -0400

    just use the one if block

--- soh/soh/z_play_otr.cpp
+++ soh/soh/z_play_otr.cpp
@@ -102,9 +102,7 @@ void OTRPlay_InitScene(PlayState* play, s32 spawn) {
         for (auto objectName : objects) {
             OTRGlobals::Instance->context->GetResourceManager()->LoadDirectoryAsync("alt/objects/" + objectName + "/*");
         }
-    }
 
-    if (CVarGetInteger("gAltAssets", 0)) {
         std::unordered_set<std::string> objects;
         if (play->numSetupActors != 0) {
             auto actorEntry = &play->setupActorList[0];
