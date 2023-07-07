2ad93f690877b45c2d6755bd2f529e657cb44fcb
Pepe20129 <72659707+Pepe20129@users.noreply.github.com>
2023-07-07 10:49:04 +0200

    Update randomizer_item_tracker.cpp

--- soh/soh/Enhancements/randomizer/randomizer_item_tracker.cpp
+++ soh/soh/Enhancements/randomizer/randomizer_item_tracker.cpp
@@ -301,7 +301,7 @@ ItemTrackerNumbers GetItemCurrentAndMax(ItemTrackerItem item) {
             break;
         case ITEM_WALLET_ADULT:
         case ITEM_WALLET_GIANT:
-            result.currentCapacity = CUR_CAPACITY(UPG_WALLET);
+            result.currentCapacity = gSaveContext.n64ddFlag && !Flags_GetRandomizerInf(RAND_INF_HAS_WALLET) ? 0 : CUR_CAPACITY(UPG_WALLET);
             result.maxCapacity = OTRGlobals::Instance->gRandomizer->GetRandoSettingValue(RSK_SHOPSANITY) > RO_SHOPSANITY_ZERO_ITEMS ? 999 : 500;
             result.currentAmmo = gSaveContext.rupees;
             break;
@@ -517,7 +517,7 @@ void DrawItem(ItemTrackerItem item) {
         case ITEM_WALLET_ADULT:
         case ITEM_WALLET_GIANT:
             actualItemId = CUR_UPG_VALUE(UPG_WALLET) == 2 ? ITEM_WALLET_GIANT : ITEM_WALLET_ADULT;
-            hasItem = true;
+            hasItem = !gSaveContext.n64ddFlag || Flags_GetRandomizerInf(RAND_INF_HAS_WALLET);
             break;
         case ITEM_BRACELET:
         case ITEM_GAUNTLETS_SILVER:
