071f6d316e5942fd6a9bbfca8b72c93e11cd42c5
louist103 <louist103@gmail.com>
2023-07-08 20:01:12 -0400

    Please apple

--- soh/soh/Extractor/FastCrc32C.c
+++ soh/soh/Extractor/FastCrc32C.c
@@ -16,12 +16,14 @@
 #elif ((defined(__GNUC__) && defined(__x86_64__) || defined(__i386__)))
 #include <nmmintrin.h>
 #include <cpuid.h>
-#elif defined (__APPLE__)
-#include <cpuid.h>
 #elif defined(__aarch64__) && defined(__ARM_FEATURE_CRC32)
 // Nothing cause its a compiler builtin
 #else
 #define NO_CRC_INTRIN
+#endif
+
+#ifdef __APPLE__
+#include <cpuid.h>
 #endif
 
 #if defined(__aarch64__) && defined(__ARM_FEATURE_CRC32)
