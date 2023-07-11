6f0d8693797a610379b7690933c51cf8a689566a
Malkierian <malkierian@gmail.com>
2023-07-11 02:50:47 -0700

    Fix ice traps on GS tokens not triggering OnItemReceive.

--- soh/src/overlays/actors/ovl_En_Si/z_en_si.c
+++ soh/src/overlays/actors/ovl_En_Si/z_en_si.c
@@ -119,13 +119,12 @@ void func_80AFB768(EnSi* this, PlayState* play) {
                 Message_StartTextbox(play, textId, NULL);
 
                 if (gSaveContext.n64ddFlag) {
-                    if (getItemId != RG_ICE_TRAP) {
-                        Randomizer_GiveSkullReward(this, play);
-                        Audio_PlayFanfare_Rando(getItem);
-                    } else {
-                        gSaveContext.pendingIceTrapCount++;
+                    if (getItemId == RG_ICE_TRAP) {
                         Audio_PlayFanfare(NA_BGM_SMALL_ITEM_GET);
+                    } else {
+                        Audio_PlayFanfare_Rando(getItem);
                     }
+                    Randomizer_GiveSkullReward(this, play);
                 } else {
                     Audio_PlayFanfare(NA_BGM_SMALL_ITEM_GET);
                 }
@@ -157,13 +156,12 @@ void func_80AFB89C(EnSi* this, PlayState* play) {
         Message_StartTextbox(play, textId, NULL);
 
         if (gSaveContext.n64ddFlag) {
-            if (getItemId != RG_ICE_TRAP) {
-                Randomizer_GiveSkullReward(this, play);
-                Audio_PlayFanfare_Rando(getItem);
-            } else {
-                gSaveContext.pendingIceTrapCount++;
+            if (getItemId == RG_ICE_TRAP) {
                 Audio_PlayFanfare(NA_BGM_SMALL_ITEM_GET);
+            } else {
+                Audio_PlayFanfare_Rando(getItem);
             }
+            Randomizer_GiveSkullReward(this, play);
         } else {
             Audio_PlayFanfare(NA_BGM_SMALL_ITEM_GET);
         }
