2fb804b6a021881474e9182923ab846f33261e20
Pepper0ni <pepper0ni3steps@gmail.com>
2023-08-08 19:08:20 +0100

    Remove disabled special hints from the spoiler log

--- soh/soh/Enhancements/randomizer/3drando/spoiler_log.cpp
+++ soh/soh/Enhancements/randomizer/3drando/spoiler_log.cpp
@@ -749,6 +749,7 @@ static void WriteHints(int language) {
     if (ganonHintText != ""){
       jsonData["ganonHintText"] = ganonHintText;
       jsonData["lightArrowHintLoc"] = GetLightArrowHintLoc();
+
     }
     if (dampesText != ""){
       jsonData["dampeText"] = dampesText;
@@ -758,6 +759,7 @@ static void WriteHints(int language) {
       jsonData["gregText"] = gregText;
       jsonData["gregLoc"] = GetItemLocation(GREG_RUPEE)->GetName();
     }
+
     jsonData["sheikText"] = sheikText;
     jsonData["sariaText"] = sariaText;
     jsonData["sariaHintLoc"] = GetSariaHintLoc();
