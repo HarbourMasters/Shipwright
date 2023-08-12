d5a7d35357b3d0d5cdeb408b73e756c0264b9aff
Pepe20129 <72659707+Pepe20129@users.noreply.github.com>
2023-08-13 01:38:21 +0200

    Update actorViewer.cpp

--- soh/soh/Enhancements/debugger/actorViewer.cpp
+++ soh/soh/Enhancements/debugger/actorViewer.cpp
@@ -6,6 +6,7 @@
 #include <array>
 #include <bit>
 #include <map>
+#include <unordered_map>
 #include <string>
 #include <libultraship/bridge.h>
 #include <libultraship/libultraship.h>
@@ -21,6 +22,8 @@ extern PlayState* gPlayState;
 #include "textures/icon_item_static/icon_item_static.h"
 #include "textures/icon_item_24_static/icon_item_24_static.h"
 }
+
+#define DEKUNUTS_FLOWER 10
 
 typedef struct {
     u16 id;
@@ -96,6 +99,248 @@ void PopulateActorDropdown(int i, std::vector<Actor*>& data) {
     }
 }
 
+//actors that don't use params at all
+static std::vector<u16> noParamsActors = {
+    ACTOR_ARMS_HOOK,
+    ACTOR_ARROW_FIRE,
+    ACTOR_ARROW_ICE,
+    ACTOR_ARROW_LIGHT,
+    ACTOR_BG_BOM_GUARD,
+    ACTOR_BG_DY_YOSEIZO,
+    ACTOR_BG_GATE_SHUTTER,
+    ACTOR_BG_GJYO_BRIDGE,
+    ACTOR_BG_HIDAN_FSLIFT,
+    ACTOR_BG_HIDAN_RSEKIZOU,
+    ACTOR_BG_HIDAN_SYOKU,
+    ACTOR_BG_JYA_GOROIWA,
+    ACTOR_BG_MIZU_UZU,
+    ACTOR_BG_MORI_RAKKATENJO,
+    ACTOR_BG_PUSHBOX,
+    ACTOR_BG_SPOT01_FUSYA,
+    ACTOR_BG_SPOT01_IDOHASHIRA,
+    ACTOR_BG_SPOT01_IDOMIZU,
+    ACTOR_BG_SPOT01_IDOSOKO,
+    ACTOR_BG_SPOT11_OASIS,
+    ACTOR_BG_SPOT15_SAKU,
+    ACTOR_BG_SPOT18_FUTA,
+    ACTOR_BG_TOKI_SWD,
+    ACTOR_BG_TREEMOUTH,
+    ACTOR_BG_VB_SIMA,
+    ACTOR_BOSS_DODONGO,
+    ACTOR_BOSS_FD,
+    ACTOR_BOSS_GOMA,
+    ACTOR_DEMO_EXT,
+    ACTOR_DEMO_SHD,
+    ACTOR_DEMO_TRE_LGT,
+    ACTOR_DOOR_TOKI,
+    ACTOR_EFC_ERUPC,
+    ACTOR_EN_ANI,
+    ACTOR_EN_AROW_TRAP,
+    ACTOR_EN_BLKOBJ,
+    ACTOR_EN_BOM_BOWL_MAN,
+    ACTOR_EN_BOM_BOWL_PIT,
+    ACTOR_EN_BOM_CHU,
+    ACTOR_EN_BUBBLE,
+    ACTOR_EN_DIVING_GAME,
+    ACTOR_EN_DNT_DEMO,
+    ACTOR_EN_DNT_JIJI,
+    ACTOR_EN_DS,
+    ACTOR_EN_DU,
+    ACTOR_EN_EG,
+    ACTOR_EN_FU,
+    ACTOR_EN_GB,
+    ACTOR_EN_GE3,
+    ACTOR_EN_GUEST,
+    ACTOR_EN_HATA,
+    ACTOR_EN_HORSE_GANON,
+    ACTOR_EN_HORSE_LINK_CHILD,
+    ACTOR_EN_HORSE_ZELDA,
+    ACTOR_EN_HS2,
+    ACTOR_EN_JS,
+    ACTOR_EN_KAKASI,
+    ACTOR_EN_KAKASI3,
+    ACTOR_EN_MA1,
+    ACTOR_EN_MA2,
+    ACTOR_EN_MA3,
+    ACTOR_EN_MAG,
+    ACTOR_EN_MK,
+    ACTOR_EN_MS,
+    ACTOR_EN_NIW_LADY,
+    ACTOR_EN_NWC,
+    ACTOR_EN_OE2,
+    ACTOR_EN_OKARINA_EFFECT,
+    ACTOR_EN_SA,
+    ACTOR_EN_SCENE_CHANGE,
+    ACTOR_EN_SKJNEEDLE,
+    ACTOR_EN_SYATEKI_ITM,
+    ACTOR_EN_SYATEKI_MAN,
+    ACTOR_EN_TAKARA_MAN,
+    ACTOR_EN_TORYO,
+    ACTOR_EN_VASE,
+    ACTOR_EN_ZL1,
+    ACTOR_MAGIC_DARK,
+    ACTOR_MAGIC_FIRE,
+    ACTOR_OBJ_DEKUJR,
+    ACTOR_OCEFF_SPOT,
+
+    ACTOR_UNSET_1,
+    ACTOR_UNSET_3,
+    ACTOR_UNSET_5,
+    ACTOR_UNSET_6,
+    ACTOR_UNSET_17,
+    ACTOR_UNSET_1A,
+    ACTOR_UNSET_1F,
+    ACTOR_UNSET_22,
+    ACTOR_UNSET_31,
+    ACTOR_UNSET_36,
+    ACTOR_UNSET_53,
+    ACTOR_UNSET_73,
+    ACTOR_UNSET_74,
+    ACTOR_UNSET_75,
+    ACTOR_UNSET_76,
+    ACTOR_UNSET_78,
+    ACTOR_UNSET_79,
+    ACTOR_UNSET_7A,
+    ACTOR_UNSET_7B,
+    ACTOR_UNSET_7E,
+    ACTOR_UNSET_7F,
+    ACTOR_UNSET_83,
+    ACTOR_UNSET_A0,
+    ACTOR_UNSET_B2,
+    ACTOR_UNSET_CE,
+    ACTOR_UNSET_D8,
+    ACTOR_UNSET_EA,
+    ACTOR_UNSET_EB,
+    ACTOR_UNSET_F2,
+    ACTOR_UNSET_F3,
+    ACTOR_UNSET_FB,
+    ACTOR_UNSET_109,
+    ACTOR_UNSET_10D,
+    ACTOR_UNSET_10E,
+    ACTOR_UNSET_128,
+    ACTOR_UNSET_129,
+    ACTOR_UNSET_134,
+    ACTOR_UNSET_154,
+    ACTOR_UNSET_15D,
+    ACTOR_UNSET_161,
+    ACTOR_UNSET_180,
+    ACTOR_UNSET_1AA
+};
+
+static std::unordered_map<u16, std::function<s16(s16)>> actorSpecificData;
+
+void CreateActorSpecificData() {
+    if (!actorSpecificData.empty()) {
+        return;
+    }
+
+    actorSpecificData[ACTOR_EN_DEKUNUTS] = [](s16 params) -> s16 {
+        bool isFlower = params == DEKUNUTS_FLOWER;
+        s16 shotsPerRound = (params >> 8) & 0xFF;
+        if (shotsPerRound == 0xFF || shotsPerRound == 0) {
+            shotsPerRound = 1;
+        }
+        ImGui::Checkbox("Flower", &isFlower);
+        if (!isFlower) {
+            ImGui::InputScalar("Shots Per Round", ImGuiDataType_S16, &shotsPerRound);
+        }
+        
+        return isFlower ? DEKUNUTS_FLOWER : (shotsPerRound << 8);
+    };
+
+    actorSpecificData[ACTOR_EN_TITE] = [](s16 params) -> s16 {
+        static const char* items[] = { "Blue", "Red" };
+        //the + 2 is because the params are -2 & -1 instead of 0 & 1
+        int selectedItem = params + 2;
+        if (ImGui::Combo("Type", &selectedItem, items, IM_ARRAYSIZE(items))) {
+            return selectedItem - 2;
+        }
+        
+        return params;
+    };
+
+    actorSpecificData[ACTOR_EN_AM] = [](s16 params) -> s16 {
+        static const char* items[] = { "Statue", "Enemy" };
+        int selectedItem = params;
+        if (ImGui::Combo("Type", &selectedItem, items, IM_ARRAYSIZE(items))) {
+            return selectedItem;
+        }
+        
+        return params;
+    };
+
+    actorSpecificData[ACTOR_BG_ICE_TURARA] = [](s16 params) -> s16 {
+        static const char* items[] = { "Stalagmite", "Stalactite", "Stalactite (Regrow)" };
+        int selectedItem = params;
+        if (ImGui::Combo("Type", &selectedItem, items, IM_ARRAYSIZE(items))) {
+            return selectedItem;
+        }
+        
+        return params;
+    };
+
+    actorSpecificData[ACTOR_BG_BREAKWALL] = [](s16 params) -> s16 {
+        static const char* items[] = { "DC Entrance", "Wall", "KD Floor", "KD Lava Cover" };
+        int selectedItem = params;
+        if (ImGui::Combo("Type", &selectedItem, items, IM_ARRAYSIZE(items))) {
+            return selectedItem;
+        }
+        
+        return params;
+    };
+
+    actorSpecificData[ACTOR_EN_REEBA] = [](s16 params) -> s16 {
+        bool isBig = params != 0;
+        ImGui::Checkbox("Big", &isBig);
+        
+        return isBig;
+    };
+
+    actorSpecificData[ACTOR_EN_ITEM00] = [](s16 params) -> s16 {
+        bool autoCollect = params & 0x8000;
+        ImGui::Checkbox("Automatically Collect", &autoCollect);
+        u8 collectibleFlag = (params & 0x3F00) >> 8;
+        ImGui::InputScalar("Collectible Flag", ImGuiDataType_U8, &collectibleFlag);
+        if (collectibleFlag > 0x3F) {
+            collectibleFlag = 0x3F;
+        }
+        
+        static const char* items[] = {
+            "Green Rupee",
+            "Blue Rupee",
+            "Red Rupee",
+            "Recovery Heart",
+            "Bombs (A)",
+            "Arrow",
+            "Heart Piece",
+            "Heart Container",
+            "Arrows (5)",
+            "Arrows (10)",
+            "Arrows (30)",
+            "Bombs (B)",
+            "Deku Nuts (5)",
+            "Deku Stick",
+            "Magic (Large)",
+            "Magic (Small)",
+            "Deku Seeds (5)",
+            "Small Key",
+            "Flexible",
+            "Gold Rupee",
+            "Purple Rupee",
+            "Deku Shield",
+            "Hylian Shield",
+            "Zora Tunic",
+            "Goron Tunic",
+            "Bombs (Special)",
+            "Bombchus"
+        };
+
+        int selectedItem = params & 0xFF;
+        ImGui::Combo("Item", &selectedItem, items, IM_ARRAYSIZE(items));
+
+        return autoCollect * 0x8000 + (collectibleFlag << 8) + selectedItem;
+    };
+}
 
 void ActorViewerWindow::DrawElement() {
     ImGui::SetNextWindowSize(ImVec2(520, 600), ImGuiCond_FirstUseEver);
@@ -271,7 +516,18 @@ void ActorViewerWindow::DrawElement() {
 
             ImGui::Text(GetActorDescription(newActor.id).c_str());
             ImGui::InputScalar("ID", ImGuiDataType_S16, &newActor.id, &one);
-            ImGui::InputScalar("params", ImGuiDataType_S16, &newActor.params, &one);
+
+            if (std::find(noParamsActors.begin(), noParamsActors.end(), newActor.id) == noParamsActors.end()) {
+                CreateActorSpecificData();
+                if (actorSpecificData.find(newActor.id) == actorSpecificData.end()) {
+                    ImGui::InputScalar("params", ImGuiDataType_S16, &newActor.params, &one);
+                } else {
+                    DrawGroupWithBorder([&]() {
+                        ImGui::Text("Actor Specific Data");
+                        newActor.params = actorSpecificData[newActor.id](newActor.params);
+                    });
+                }
+            }
 
             ImGui::PushItemWidth(ImGui::GetFontSize() * 6);
 
