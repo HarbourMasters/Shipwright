7b46db48fdd7b2f0e4a0281edc7bc2bf8ab91e7f
Pepper0ni <pepper0ni3steps@gmail.com>
2023-08-30 01:32:30 +0100

    Fix Loading spoiler logs causing corrupt hints, remove disabled warp song shuffle text from spoiler logs

--- soh/soh/Enhancements/randomizer/3drando/spoiler_log.cpp
+++ soh/soh/Enhancements/randomizer/3drando/spoiler_log.cpp
@@ -689,12 +689,14 @@ static void WriteHints(int language) {
             unformattedGregText = GetGregHintText().GetEnglish();
             unformattedSheikText = GetSheikHintText().GetEnglish();
             unformattedSariaText = GetSariaHintText().GetEnglish();
-            jsonData["warpMinuetText"] = GetWarpMinuetText().GetEnglish();
-            jsonData["warpBoleroText"] = GetWarpBoleroText().GetEnglish();
-            jsonData["warpSerenadeText"] = GetWarpSerenadeText().GetEnglish();
-            jsonData["warpRequiemText"] = GetWarpRequiemText().GetEnglish();
-            jsonData["warpNocturneText"] = GetWarpNocturneText().GetEnglish();
-            jsonData["warpPreludeText"] = GetWarpPreludeText().GetEnglish();
+            if (Settings::ShuffleWarpSongs){
+              jsonData["warpMinuetText"] = GetWarpMinuetText().GetEnglish();
+              jsonData["warpBoleroText"] = GetWarpBoleroText().GetEnglish();
+              jsonData["warpSerenadeText"] = GetWarpSerenadeText().GetEnglish();
+              jsonData["warpRequiemText"] = GetWarpRequiemText().GetEnglish();
+              jsonData["warpNocturneText"] = GetWarpNocturneText().GetEnglish();
+              jsonData["warpPreludeText"] = GetWarpPreludeText().GetEnglish();
+            }
             jsonData["childAltar"]["hintText"] = GetChildAltarText().GetEnglish();
             jsonData["adultAltar"]["hintText"] = GetAdultAltarText().GetEnglish();
             break;
@@ -705,12 +707,14 @@ static void WriteHints(int language) {
             unformattedGregText = GetGregHintText().GetFrench();
             unformattedSheikText = GetSheikHintText().GetFrench();
             unformattedSariaText = GetSariaHintText().GetFrench();
-            jsonData["warpMinuetText"] = GetWarpMinuetText().GetFrench();
-            jsonData["warpBoleroText"] = GetWarpBoleroText().GetFrench();
-            jsonData["warpSerenadeText"] = GetWarpSerenadeText().GetFrench();
-            jsonData["warpRequiemText"] = GetWarpRequiemText().GetFrench();
-            jsonData["warpNocturneText"] = GetWarpNocturneText().GetFrench();
-            jsonData["warpPreludeText"] = GetWarpPreludeText().GetFrench();
+            if (Settings::ShuffleWarpSongs){
+              jsonData["warpMinuetText"] = GetWarpMinuetText().GetFrench();
+              jsonData["warpBoleroText"] = GetWarpBoleroText().GetFrench();
+              jsonData["warpSerenadeText"] = GetWarpSerenadeText().GetFrench();
+              jsonData["warpRequiemText"] = GetWarpRequiemText().GetFrench();
+              jsonData["warpNocturneText"] = GetWarpNocturneText().GetFrench();
+              jsonData["warpPreludeText"] = GetWarpPreludeText().GetFrench();
+            }
             jsonData["childAltar"]["hintText"] = GetChildAltarText().GetFrench();
             jsonData["adultAltar"]["hintText"] = GetAdultAltarText().GetFrench();
             break;
