0e7c6585239c0d7ea2c039b8b8cb7eaedf8928a9
Caladius <Caladius@users.noreply.github.com>
2023-08-30 13:34:06 -0400

    V1 - Chestapalooza (#2922)
    * V1- Adds additional trap variants to Chests
    
    * Create and use OnTrapProcessed hook
    
    * Update naming for Traps, extends to NPC and Token Rewards.
    
    New Hook for Rando traps.
    
    Restore Vanilla code for ice traps.
    
    * Update soh/src/overlays/actors/ovl_player_actor/z_player.c
    
    Co-authored-by: Garrett Cox <garrettjcox@gmail.com>
    
    * More tweaks, remove processed trap hook
    
    ---------
    
    Co-authored-by: Garrett Cox <garrettjcox@gmail.com>

--- soh/soh/Enhancements/mods.cpp
+++ soh/soh/Enhancements/mods.cpp
@@ -1,6 +1,7 @@
 #include "mods.h"
 #include <libultraship/bridge.h>
 #include "game-interactor/GameInteractor.h"
+#include "soh/Enhancements/randomizer/3drando/random.hpp"
 #include "tts/tts.h"
 #include "soh/Enhancements/boss-rush/BossRushTypes.h"
 #include "soh/Enhancements/enhancementTypes.h"
@@ -16,7 +17,7 @@ extern "C" {
 #include "functions.h"
 extern SaveContext gSaveContext;
 extern PlayState* gPlayState;
-
+extern void Overlay_DisplayText(float duration, const char* text);
 uint32_t ResourceMgr_IsSceneMasterQuest(s16 sceneNum);
 }
 bool performDelayedSave = false;
@@ -600,6 +601,154 @@ void RegisterMirrorModeHandler() {
     });
 }
 
+typedef enum {
+    ADD_ICE_TRAP,
+    ADD_BURN_TRAP,
+    ADD_SHOCK_TRAP,
+    ADD_KNOCK_TRAP,
+    ADD_SPEED_TRAP,
+    ADD_BOMB_TRAP,
+    ADD_VOID_TRAP,
+    ADD_AMMO_TRAP,
+    ADD_KILL_TRAP,
+    ADD_TELEPORT_TRAP,
+    ADD_TRAP_MAX
+} AltTrapType;
+
+const char* altTrapTypeCvars[] = {
+    "gAddTraps.Ice",
+    "gAddTraps.Burn",
+    "gAddTraps.Shock",
+    "gAddTraps.Knock",
+    "gAddTraps.Speed",
+    "gAddTraps.Bomb",
+    "gAddTraps.Void",
+    "gAddTraps.Ammo",
+    "gAddTraps.Kill",
+    "gAddTraps.Tele"
+};
+
+std::vector<AltTrapType> getEnabledAddTraps () {
+    std::vector<AltTrapType> enabledAddTraps;
+    for (int i = 0; i < ADD_TRAP_MAX; i++) {
+        if (CVarGetInteger(altTrapTypeCvars[i], 0)) {
+            enabledAddTraps.push_back(static_cast<AltTrapType>(i));
+        }
+    }
+    if (enabledAddTraps.size() == 0) {
+        enabledAddTraps.push_back(ADD_ICE_TRAP);
+    }
+    return enabledAddTraps;
+};
+
+void RegisterAltTrapTypes() {
+    static AltTrapType roll = ADD_TRAP_MAX;
+    static int statusTimer = -1;
+    static int eventTimer = -1;
+
+    GameInteractor::Instance->RegisterGameHook<GameInteractor::OnItemReceive>([](GetItemEntry itemEntry) {
+        if (!CVarGetInteger("gAddTraps.enabled", 0) || itemEntry.modIndex != MOD_RANDOMIZER || itemEntry.getItemId != RG_ICE_TRAP) {
+            return;
+        }
+        roll = RandomElement(getEnabledAddTraps());
+        switch (roll) {
+            case ADD_ICE_TRAP:
+                GameInteractor::RawAction::FreezePlayer();
+                break;
+            case ADD_BURN_TRAP:
+                GameInteractor::RawAction::BurnPlayer();
+                break;
+            case ADD_SHOCK_TRAP:
+                GameInteractor::RawAction::ElectrocutePlayer();
+                break;
+            case ADD_KNOCK_TRAP:
+                eventTimer = 3;
+                break;
+            case ADD_SPEED_TRAP:
+                Audio_PlaySoundGeneral(NA_SE_VO_KZ_MOVE, &D_801333D4, 4, &D_801333E0, &D_801333E0, &D_801333E8);
+                GameInteractor::State::RunSpeedModifier = -2;
+                statusTimer = 200;
+                Overlay_DisplayText(10, "Speed Decreased!");
+                break;
+            case ADD_BOMB_TRAP:
+                eventTimer = 3;
+                break;
+            case ADD_VOID_TRAP:
+                Audio_PlaySoundGeneral(NA_SE_EN_GANON_LAUGH, &D_801333D4, 4, &D_801333E0, &D_801333E0, &D_801333E8);
+                eventTimer = 3;                    
+                break;
+            case ADD_AMMO_TRAP:
+                eventTimer = 3;
+                Overlay_DisplayText(5, "Ammo Halved!");
+                break;
+            case ADD_KILL_TRAP:
+                GameInteractor::RawAction::SetPlayerHealth(0);
+                break;
+            case ADD_TELEPORT_TRAP:
+                eventTimer = 3;
+                break;
+        }
+    });
+    GameInteractor::Instance->RegisterGameHook<GameInteractor::OnPlayerUpdate>([]() {
+        Player* player = GET_PLAYER(gPlayState);
+        if (statusTimer == 0) {
+            GameInteractor::State::RunSpeedModifier = 0;
+        }
+        if (eventTimer == 0) {
+            switch (roll) {
+                case ADD_KNOCK_TRAP:
+                    GameInteractor::RawAction::KnockbackPlayer(1);
+                    break;
+                case ADD_BOMB_TRAP:
+                    GameInteractor::RawAction::SpawnActor(ACTOR_EN_BOM, 1);
+                    break;
+                case ADD_VOID_TRAP:
+                    Play_TriggerRespawn(gPlayState);
+                    break;
+                case ADD_AMMO_TRAP:
+                    AMMO(ITEM_STICK) = AMMO(ITEM_STICK) * 0.5;
+                    AMMO(ITEM_NUT) = AMMO(ITEM_NUT) * 0.5;
+                    AMMO(ITEM_SLINGSHOT) = AMMO(ITEM_SLINGSHOT) * 0.5;
+                    AMMO(ITEM_BOW) = AMMO(ITEM_BOW) * 0.5;
+                    AMMO(ITEM_BOMB) = AMMO(ITEM_BOMB) * 0.5;
+                    AMMO(ITEM_BOMBCHU) = AMMO(ITEM_BOMBCHU) * 0.5;
+                    Audio_PlaySoundGeneral(NA_SE_VO_FR_SMILE_0, &D_801333D4, 4, &D_801333E0, &D_801333E0, &D_801333E8);
+                    break;
+                case ADD_TELEPORT_TRAP:
+                    int entrance;
+                    int index = 1 + rand() % 10;
+                    switch (index) {
+                        case 1:
+                            entrance = GI_TP_DEST_SERENADE;
+                            break;
+                        case 2:
+                            entrance = GI_TP_DEST_REQUIEM;
+                            break;
+                        case 3:
+                            entrance = GI_TP_DEST_BOLERO;
+                            break;
+                        case 4:
+                            entrance = GI_TP_DEST_MINUET;
+                            break;
+                        case 5:
+                            entrance = GI_TP_DEST_NOCTURNE;
+                            break;
+                        case 6:
+                            entrance = GI_TP_DEST_PRELUDE;
+                            break;
+                        default:
+                            entrance = GI_TP_DEST_LINKSHOUSE;
+                            break;
+                    }
+                    GameInteractor::RawAction::TeleportPlayer(entrance);
+                    break;
+            }
+        }
+        statusTimer--;
+        eventTimer--;
+    });
+}
+
 void InitMods() {
     RegisterTTS();
     RegisterInfiniteMoney();
@@ -622,5 +771,6 @@ void InitMods() {
     RegisterBonkDamage();
     RegisterMenuPathFix();
     RegisterMirrorModeHandler();
+    RegisterAltTrapTypes();
     NameTag_RegisterHooks();
 }
