7f4a44a604f0e665251a4c4ac067c076e86f44af
Pepper0ni <pepper0ni3steps@gmail.com>
2023-08-30 01:32:30 +0100

    Fix Loading spoiler logs causing corrupt hints, remove disabled warp song shuffle text from spoiler logs

--- soh/soh/Enhancements/randomizer/3drando/spoiler_log.cpp
+++ soh/soh/Enhancements/randomizer/3drando/spoiler_log.cpp
@@ -680,8 +680,11 @@ static void WriteHints(int language) {
             unformattedGanonHintText = GetGanonHintText().GetEnglish();
             unformattedDampesText = GetDampeHintText().GetEnglish();
             unformattedGregText = GetGregHintText().GetEnglish();
+<<<<<<< HEAD
             unformattedSheikText = GetSheikHintText().GetEnglish();
             unformattedSariaText = GetSariaHintText().GetEnglish();
+=======
+>>>>>>> b65a351a (Fix Loading spoiler logs causing corrupt hints, remove disabled warp song shuffle text from spoiler logs)
             if (Settings::ShuffleWarpSongs){
               jsonData["warpMinuetText"] = GetWarpMinuetText().GetEnglish();
               jsonData["warpBoleroText"] = GetWarpBoleroText().GetEnglish();
@@ -698,8 +701,11 @@ static void WriteHints(int language) {
             unformattedGanonHintText = GetGanonHintText().GetFrench();
             unformattedDampesText = GetDampeHintText().GetFrench();
             unformattedGregText = GetGregHintText().GetFrench();
+<<<<<<< HEAD
             unformattedSheikText = GetSheikHintText().GetFrench();
             unformattedSariaText = GetSariaHintText().GetFrench();
+=======
+>>>>>>> b65a351a (Fix Loading spoiler logs causing corrupt hints, remove disabled warp song shuffle text from spoiler logs)
             if (Settings::ShuffleWarpSongs){
               jsonData["warpMinuetText"] = GetWarpMinuetText().GetFrench();
               jsonData["warpBoleroText"] = GetWarpBoleroText().GetFrench();
