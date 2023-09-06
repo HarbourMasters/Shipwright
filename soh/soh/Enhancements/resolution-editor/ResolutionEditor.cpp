c042a2f228323c339c0f4d34a855ae0031fbdd9d
Tina H. (sheepytina) <99330992+sheepytina@users.noreply.github.com>
2023-09-06 20:14:09 +1000

    Add Additional Settings and the accursed horizontal resolution field.
    There's still a few bugs with it that I haven't squashed, but I need to stop for now and just commit what I've got.
    
    (This is honestly causing more problems than it solves, but i'm tired of getting questions about it.)

--- soh/soh/Enhancements/resolution-editor/ResolutionEditor.cpp
+++ soh/soh/Enhancements/resolution-editor/ResolutionEditor.cpp
@@ -8,7 +8,9 @@
 namespace AdvancedResolutionSettings {
 enum setting { UPDATE_aspectRatioX, UPDATE_aspectRatioY, UPDATE_verticalPixelCount, UPDATE_integerScaleFactor };
 
-const char* aspectRatioPresetLabels[] = { "Off", "Custom", "4:3", "16:9", "5:3 (Nintendo 3DS)", "8:5 (16:10)", "21:9" };
+const char* aspectRatioPresetLabels[] = {
+    "Off", "Custom", "Original (4:3)", "Widescreen (16:9)", "Nintendo 3DS (5:3)", "16:10 (8:5)", "Ultrawide (21:9)"
+};
 const float aspectRatioPresetsX[] = { 0.0f, 12.0f, 4.0f, 16.0f, 5.0f, 16.0f, 21.0f };
 const float aspectRatioPresetsY[] = { 0.0f, 9.0f, 3.0f, 9.0f, 3.0f, 10.0f, 9.0f };
 const int default_aspectRatio = 1; // Default combo list option
@@ -26,7 +28,7 @@ void AdvancedResolutionSettingsWindow::InitElement() {
 }
 
 void AdvancedResolutionSettingsWindow::DrawElement() {
-    ImGui::SetNextWindowSize(ImVec2(444, 484), ImGuiCond_FirstUseEver);
+    ImGui::SetNextWindowSize(ImVec2(497, 513), ImGuiCond_FirstUseEver);
     if (ImGui::Begin("Advanced Resolution Settings", &mIsVisible)) {
         bool update[sizeof(setting)];
         for (unsigned short i = 0; i < sizeof(setting); i++)
@@ -50,13 +52,19 @@ void AdvancedResolutionSettingsWindow::DrawElement() {
         float* p_aspectRatioY = &aspectRatioY;
         int* p_verticalPixelCount = &verticalPixelCount;
         int* p_integerScaleFactor = &integerScaleFactor;
+        // Additional settings
+        static bool showHorizontalResField = false;
+        static int horizontalPixelCount = (verticalPixelCount / aspectRatioY) * aspectRatioX;
+        bool* p_showHorizontalResField = &showHorizontalResField;
+        int* p_horizontalPixelCount = &horizontalPixelCount;
+        bool* p_update_verticalPixelCount = &update[UPDATE_aspectRatioX];
 
 #ifdef __APPLE__
         ImGui::Text("Note: these settings may behave incorrectly on Apple Retina Displays.");
         UIWidgets::PaddedSeparator(true, true, 3.0f, 3.0f);
 #endif
 
-        // The original resolution slider (also for convenience)
+        // The original resolution slider (for convenience)
         if (UIWidgets::EnhancementSliderFloat("Internal Resolution: %d %%", "##IMul", "gInternalResolution", 0.5f, 2.0f,
                                               "", 1.0f, true, true,
                                               (CVarGetInteger("gAdvancedResolution.VerticalResolutionToggle", 0) &&
@@ -66,9 +74,8 @@ void AdvancedResolutionSettingsWindow::DrawElement() {
         }
         UIWidgets::Tooltip("Multiplies your output resolution by the value entered, as a more intensive but effective "
                            "form of anti-aliasing");
-        // N64 Mode toggle (for convenience)
-        /*
-        bool enableN64Mode = CVarGetInteger("gLowResMode", 0);
+        // N64 Mode toggle (again for convenience)
+        /* bool enableN64Mode = CVarGetInteger("gLowResMode", 0);
         if (ImGui::Checkbox("(Enhancements>Graphics) N64 Mode", &enableN64Mode)) {
             CVarSetInteger("gLowResMode", enableN64Mode);
         }*/
@@ -131,9 +138,21 @@ void AdvancedResolutionSettingsWindow::DrawElement() {
             verticalPixelCount = pixelCountPresets[item_pixelCount];
             update[UPDATE_verticalPixelCount] = true;
         }
-        if (ImGui::InputInt("Pixel Count", p_verticalPixelCount, 8, 240)) {
+        if (showHorizontalResField) {
+            // So basically we're "faking" this one.
+            if (ImGui::InputInt("Horiz. Pixel Count", p_horizontalPixelCount, 8, 320)) {
+                aspectRatioX = aspectRatioY * horizontalPixelCount / verticalPixelCount;
+                update[UPDATE_aspectRatioX] = true;
+            }
+        }
+        if (ImGui::InputInt("Vertical Pixel Count", p_verticalPixelCount, 8, 240)) {
             item_pixelCount = default_pixelCount;
             update[UPDATE_verticalPixelCount] = true;
+
+            if (showHorizontalResField) { // Account for the natural instinct to enter horizontal first.
+                aspectRatioX = aspectRatioY * horizontalPixelCount / verticalPixelCount;
+                update[UPDATE_aspectRatioX] = true;
+            }
         }
 
         UIWidgets::Spacer(0);
@@ -169,6 +188,28 @@ void AdvancedResolutionSettingsWindow::DrawElement() {
             CVarSetInteger("gAdvancedResolution.IntegerScaleFactor", integerScale_maximumBounds);
         }
 
+        UIWidgets::PaddedSeparator(true, true, 3.0f, 3.0f);
+        // Collapsible panel for additional settings
+        if (ImGui::CollapsingHeader("Additional Settings")) {
+            UIWidgets::Spacer(0);
+            if (ImGui::Checkbox("Show a horizontal resolution field.", p_showHorizontalResField)) {
+                // Force an early update on vertical pixel count
+                if (!showHorizontalResField) {
+                    if (verticalPixelCount < minVerticalPixelCount) {
+                        verticalPixelCount = minVerticalPixelCount;
+                    }
+                    if (verticalPixelCount > maxVerticalPixelCount) {
+                        verticalPixelCount = maxVerticalPixelCount;
+                    }
+                    CVarSetInteger("gAdvancedResolution.VerticalPixelCount", (int32_t)verticalPixelCount);
+                }
+                // Refresh relevant values
+                aspectRatioX = aspectRatioY * horizontalPixelCount / verticalPixelCount;
+                horizontalPixelCount = (verticalPixelCount / aspectRatioY) * aspectRatioX;
+                update[UPDATE_aspectRatioX] = showHorizontalResField;
+            }
+        }
+
         // Clamp and update CVars
         if (update[UPDATE_aspectRatioX]) {
             if (aspectRatioX < 0.0f) {
@@ -183,10 +224,13 @@ void AdvancedResolutionSettingsWindow::DrawElement() {
             CVarSetFloat("gAdvancedResolution.AspectRatioY", aspectRatioY);
         }
         if (update[UPDATE_verticalPixelCount]) {
-            if (verticalPixelCount < minVerticalPixelCount) {
+            // There's a clamp on the Libultraship side too.
+            // So clamping it here is purely visual.
+            // It does however interfere with the "Show a horizontal resolution field" setting.
+            if (verticalPixelCount < minVerticalPixelCount && !showHorizontalResField) {
                 verticalPixelCount = minVerticalPixelCount;
             }
-            if (verticalPixelCount > maxVerticalPixelCount) {
+            if (verticalPixelCount > maxVerticalPixelCount && !showHorizontalResField) {
                 verticalPixelCount = maxVerticalPixelCount;
             }
             CVarSetInteger("gAdvancedResolution.VerticalPixelCount", (int32_t)verticalPixelCount);
