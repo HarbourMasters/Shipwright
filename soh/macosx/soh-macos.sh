a129371923abc81c6a38828f751fe5b96c8825c3
Adam Bird <Archez@users.noreply.github.com>
2023-09-10 19:20:58 +0200

    Add extraction support for ByteSwapped and LittleEndian formats (#3042)
    * add extraction support for byteswapped and littleendian formats
    
    * update linux/mac scripts to handle v64 and n64

--- soh/macosx/soh-macos.sh
+++ soh/macosx/soh-macos.sh
@@ -7,6 +7,56 @@ export RESPATH="${SNAME%/MacOS*}/Resources"
 export LIBPATH="${SNAME%/MacOS*}/Frameworks"
 export DYLD_FALLBACK_LIBRARY_PATH="$LIBPATH"
 
+remap_hashes ()
+{
+	# Remap v64 and n64 hashes to their z64 has equivalent
+	# ZAPD will handle converting the data into z64 format
+	case "$ROMHASH" in
+	a9059b56e761c9034fbe02fe4c24985aaa835dac) # v64
+		ROMHASH=cee6bc3c2a634b41728f2af8da54d9bf8cc14099
+		;;
+	24708102dc504d3f375a37f4ae4e149c167dc515) # n64
+		ROMHASH=cee6bc3c2a634b41728f2af8da54d9bf8cc14099
+		;;
+	580dd0bd1b6d2c51cc20a764eece84dba558964c) # v64
+		ROMHASH=0227d7c0074f2d0ac935631990da8ec5914597b4
+		;;
+	d6342c59007e57c1194661ec6880b2f078403f4e) # n64
+		ROMHASH=0227d7c0074f2d0ac935631990da8ec5914597b4
+		;;
+	663c34f1b2c05a09e5beffe4d0dcd440f7d49dc7) # v64
+		ROMHASH=cfbb98d392e4a9d39da8285d10cbef3974c2f012
+		;;
+	24c73d378b0620a380ce5ef9f2b186c6c157a68b) # n64
+		ROMHASH=cfbb98d392e4a9d39da8285d10cbef3974c2f012
+		;;
+	973bc6fe56010a8d646166a1182a81b4f13b8cf9) # v64
+		ROMHASH=50bebedad9e0f10746a52b07239e47fa6c284d03
+		;;
+	d327752c46edc70ff3668b9514083dbbee08927c) # v64
+		ROMHASH=50bebedad9e0f10746a52b07239e47fa6c284d03
+		;;
+	ecdeb1747560834e079c22243febea7f6f26ba3b) # v64
+		ROMHASH=079b855b943d6ad8bd1eb026c0ed169ecbdac7da
+		;;
+	f19f8662ec7abee29484a272a6fda53e39efe0f1) # n64
+		ROMHASH=079b855b943d6ad8bd1eb026c0ed169ecbdac7da
+		;;
+	ab519ce04a33818ce2c39b3c514a751d807a494a) # v64
+		ROMHASH=cfecfdc58d650e71a200c81f033de4e6d617a9f6
+		;;
+	c19a34f7646305e1755249fca2071e178bd7cd00) # n64
+		ROMHASH=cfecfdc58d650e71a200c81f033de4e6d617a9f6
+		;;
+	25e8ae79ea0839ca5c984473f7460d8040c36f9c) # v64
+		ROMHASH=517bd9714c73cb96c21e7c2ef640d7b55186102f
+		;;
+	166c02770d67fcc3954c443eb400a6a3573d3fc0) # n64
+		ROMHASH=517bd9714c73cb96c21e7c2ef640d7b55186102f
+		;;
+	esac
+}
+
 if [ ! -e "$SHIP_HOME" ]; then	mkdir "$SHIP_HOME"; fi
 
 if [ ! -e "$SHIP_HOME"/mods ]; then
@@ -39,6 +89,9 @@ if [ ! -e "$SHIP_HOME"/oot.otr ] || [ ! -e "$SHIP_HOME"/oot-mq.otr ]; then
 
 			# If an invalid rom was selected, let the user know and ask to try again
 			ROMHASH="$(shasum "$DROPROM" | awk '{ print $1 }')"
+
+			remap_hashes
+
 			case "$ROMHASH" in
 				cee6bc3c2a634b41728f2af8da54d9bf8cc14099)
 					ROM_TYPE=0;;
@@ -118,6 +171,9 @@ if [ ! -e "$SHIP_HOME"/oot.otr ] || [ ! -e "$SHIP_HOME"/oot-mq.otr ]; then
 
 		# If an invalid rom was detected, let the user know
 		ROMHASH="$(shasum "$ASSETDIR"/tmp/rom.z64 | awk '{ print $1 }')"
+
+		remap_hashes
+
 		case "$ROMHASH" in
 			cee6bc3c2a634b41728f2af8da54d9bf8cc14099)
 				ROM=GC_NMQ_D
