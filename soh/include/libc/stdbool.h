32ce3d8de228bb53ca2845c1616ca797bd17c2a9
briaguya <70942617+briaguya-ai@users.noreply.github.com>
2023-09-15 01:17:24 -0400

    another oddity

--- soh/include/libc/stdbool.h
+++ soh/include/libc/stdbool.h
@@ -5,9 +5,12 @@
 
 #ifndef __cplusplus
 
+// clang-format off
+// no idea why AlignConsecutiveMacros seems to mess this one up
 #define bool    u32
 #define false   0
 #define true    1
+// clang-format on
 
 #endif /* __cplusplus */
 
