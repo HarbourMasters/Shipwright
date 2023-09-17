affd79134f426b91aa3be65dab229fc44bf83aac
Adam Bird <archez39@me.com>
2023-09-16 23:28:14 -0400

    update launch scripts, cmake and extract steps to pass soh version

--- OTRExporter/extract_assets.py
+++ OTRExporter/extract_assets.py
@@ -8,7 +8,7 @@ import struct
 import subprocess
 import argparse
 
-def BuildOTR(xmlPath, rom, zapd_exe=None, genHeaders=None):
+def BuildOTR(xmlPath, rom, zapd_exe=None, sohver=None, genHeaders=None):
     shutil.copytree("assets", "Extract/assets")
 
     if not zapd_exe:
@@ -23,7 +23,7 @@ def BuildOTR(xmlPath, rom, zapd_exe=None, genHeaders=None):
     else:
         # generate otrs, but not headers
         exec_cmd.extend(["-gsf", "0", "-se", "OTR", "--otrfile",
-                "oot-mq.otr" if Z64Rom.isMqRom(rom) else "oot.otr"])
+                "oot-mq.otr" if Z64Rom.isMqRom(rom) else "oot.otr"] + ["--sohver", sohver] if sohver else [])
 
     print(exec_cmd)
     exitValue = subprocess.call(exec_cmd)
@@ -33,13 +33,13 @@ def BuildOTR(xmlPath, rom, zapd_exe=None, genHeaders=None):
         print("Aborting...", file=os.sys.stderr)
         print("\n")
 
-def BuildSohOtr(zapd_exe=None):
+def BuildSohOtr(zapd_exe=None, sohver=None):
     shutil.copytree("assets", "Extract/assets")
 
     if not zapd_exe:
         zapd_exe = "x64\\Release\\ZAPD.exe" if sys.platform == "win32" else "../ZAPDTR/ZAPD.out"
 
-    exec_cmd = [zapd_exe, "botr", "-se", "OTR", "--norom"]
+    exec_cmd = [zapd_exe, "botr", "-se", "OTR", "--norom"] + ["--sohver", sohver] if sohver else []
 
     print(exec_cmd)
     exitValue = subprocess.call(exec_cmd)
@@ -57,6 +57,7 @@ def main():
     parser.add_argument("-v", "--verbose", help="Display rom's header checksums and their corresponding xml folder", dest="verbose", action="store_true")
     parser.add_argument("--gen-headers", help="Generate source headers to be checked in", dest="gen_headers", action="store_true")
     parser.add_argument("--norom", help="Generate only soh.otr to be bundled to the game", dest="norom", action="store_true")
+    parser.add_argument("--sohver", help="Store the SoH version in the otr", dest="sohver", type=str)
 
     args = parser.parse_args()
 
@@ -64,7 +65,7 @@ def main():
         if (os.path.exists("Extract")):
             shutil.rmtree("Extract")
             
-        BuildSohOtr(args.zapd_exe)
+        BuildSohOtr(args.zapd_exe, sohver=args.sohver)
         return
 
     roms = [ Z64Rom(args.rom) ] if args.rom else rom_chooser.chooseROM(args.verbose, args.non_interactive)
@@ -72,7 +73,7 @@ def main():
         if (os.path.exists("Extract")):
             shutil.rmtree("Extract")
 
-        BuildOTR("../soh/assets/xml/" + rom.version.xml_ver + "/", rom.file_path, zapd_exe=args.zapd_exe, genHeaders=args.gen_headers)
+        BuildOTR("../soh/assets/xml/" + rom.version.xml_ver + "/", rom.file_path, zapd_exe=args.zapd_exe, sohver=args.sohver, genHeaders=args.gen_headers)
 
 if __name__ == "__main__":
     main()
