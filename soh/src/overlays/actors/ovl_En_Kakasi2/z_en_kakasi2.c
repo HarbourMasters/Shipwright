49f14eee930433d7e16ff86571d2d40135da588e
Pepe20129 <72659707+Pepe20129@users.noreply.github.com>
2023-10-16 20:34:14 +0200

    Update z_en_kakasi2.c

--- soh/src/overlays/actors/ovl_En_Kakasi2/z_en_kakasi2.c
+++ soh/src/overlays/actors/ovl_En_Kakasi2/z_en_kakasi2.c
@@ -117,12 +117,14 @@ void func_80A90264(EnKakasi2* this, PlayState* play) {
 
     this->unk_194++;
     
-    bool hasTwoOcarinaButtons = !IS_RANDO ||
-        Flags_GetRandomizerInf(RAND_INF_HAS_OCARINA_A) +
-        Flags_GetRandomizerInf(RAND_INF_HAS_OCARINA_C_LEFT) +
-        Flags_GetRandomizerInf(RAND_INF_HAS_OCARINA_C_RIGHT) +
-        Flags_GetRandomizerInf(RAND_INF_HAS_OCARINA_C_UP) +
-        Flags_GetRandomizerInf(RAND_INF_HAS_OCARINA_C_DOWN) >= 2;
+    int ocarinaButtonCount = 0;
+    for (int i = RAND_INF_HAS_OCARINA_A; i <= RAND_INF_HAS_OCARINA_C_DOWN; i++) {
+        if (Flags_GetRandomizerInf(i)) {
+            ocarinaButtonCount++;
+        }
+    }
+
+    bool hasTwoOcarinaButtons = !IS_RANDO || ocarinaButtonCount >= 2;
 
     bool skipScarecrow = hasTwoOcarinaButtons && play->msgCtx.msgMode == MSGMODE_OCARINA_PLAYING &&
                             ((CVarGetInteger("gSkipScarecrow", 0) && gSaveContext.scarecrowSpawnSongSet) ||
