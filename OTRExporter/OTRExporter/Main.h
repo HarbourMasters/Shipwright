4a70fea67f6c1fc5b2765a5d86df51a12d06b954
louist103 <35883445+louist103@users.noreply.github.com>
2023-10-17 00:32:42 -0400

    Move ZAPDTR and OTRExporter to sub modules (#3298)

--- OTRExporter/OTRExporter/Main.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <libultraship/bridge.h>
-
-extern std::shared_ptr<LUS::Archive> otrArchive;
-extern std::map<std::string, std::vector<char>> files;
-
-void AddFile(std::string fName, std::vector<char> data);
