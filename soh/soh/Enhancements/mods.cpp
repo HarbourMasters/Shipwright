0751c59530098598046510ebddd059d5e946e236
Ralphie Morell <stratomaster64@gmail.com>
2023-10-19 00:23:05 -0400

    also remove phantom ganon's horses if he's not there

--- soh/soh/Enhancements/mods.cpp
+++ soh/soh/Enhancements/mods.cpp
@@ -1034,6 +1034,10 @@ void RegisterBossSouls() {
                 break;
             case SCENE_FOREST_TEMPLE_BOSS:
                 rand_inf = RAND_INF_PHANTOM_GANON_SOUL;
+                //Special case for Phantom Ganon's horse (and fake), as they're considered "background actors."
+                if (actual->id == ACTOR_EN_FHG) {
+                    Actor_Delete(&gPlayState->actorCtx, actual, gPlayState);
+                }
                 break;
             case SCENE_FIRE_TEMPLE_BOSS:
                 rand_inf = RAND_INF_VOLVAGIA_SOUL;
