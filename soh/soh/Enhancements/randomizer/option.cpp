2ff9340069bda2243be2f401e18a3a7c36101642
Christopher Leggett <chris@leggett.dev>
2023-11-20 10:16:54 -0500

    Fixes IDE warnings/Linter suggestions in option.*

--- soh/soh/Enhancements/randomizer/option.cpp
+++ soh/soh/Enhancements/randomizer/option.cpp
@@ -1,26 +1,25 @@
 #include "option.h"
 
 namespace Rando {
-Option Option::Bool(std::string name_, std::vector<std::string> options_, OptionCategory category_,
-                    uint8_t defaultOption_, bool defaultHidden_) {
-    return Option(false, std::move(name_), std::move(options_), category_, defaultOption_, defaultHidden_);
+Option Option::Bool(std::string name_, std::vector<std::string> options_, const OptionCategory category_,
+                    const uint8_t defaultOption_, const bool defaultHidden_) {
+    return {false, std::move(name_), std::move(options_), category_, defaultOption_, defaultHidden_};
 }
 
-Option Option::U8(std::string name_, std::vector<std::string> options_, OptionCategory category_,
-                  uint8_t defaultOption_, bool defaultHidden_) {
-    return Option(uint8_t(0), std::move(name_), std::move(options_), category_, defaultOption_, defaultHidden_);
+Option Option::U8(std::string name_, std::vector<std::string> options_, const OptionCategory category_,
+                  const uint8_t defaultOption_, const bool defaultHidden_) {
+    return {static_cast<uint8_t>(0), std::move(name_), std::move(options_), category_, defaultOption_, defaultHidden_};
 }
 
 Option Option::LogicTrick(std::string name_) {
-    return Option(false, std::move(name_), { "Disabled", "Enabled" }, OptionCategory::Setting, 0, 0);
+    return Option(false, std::move(name_), { "Disabled", "Enabled" }, OptionCategory::Setting, 0, false);
 }
 
 Option::operator bool() const {
     if (std::holds_alternative<bool>(var)) {
         return Value<bool>();
-    } else {
-        return Value<uint8_t>() != 0;
     }
+    return Value<uint8_t>() != 0;
 }
 
 size_t Option::GetOptionCount() const {
@@ -56,7 +55,7 @@ void Option::RestoreDelayedOption() {
     SetVariable();
 }
 
-void Option::SetSelectedIndex(size_t idx) {
+void Option::SetSelectedIndex(const size_t idx) {
     selectedOption = idx;
     if (selectedOption >= options.size()) {
         selectedOption = 0;
@@ -76,20 +75,20 @@ bool Option::IsHidden() const {
     return hidden;
 }
 
-bool Option::IsCategory(OptionCategory category) const {
+bool Option::IsCategory(const OptionCategory category) const {
     return category == this->category;
 }
 
-Option::Option(uint8_t var_, std::string name_, std::vector<std::string> options_, OptionCategory category_,
-               uint8_t defaultOption_, bool defaultHidden_)
+Option::Option(uint8_t var_, std::string name_, std::vector<std::string> options_, const OptionCategory category_,
+               const uint8_t defaultOption_, const bool defaultHidden_)
     : var(var_), name(std::move(name_)), options(std::move(options_)), category(category_),
       defaultOption(defaultOption_), defaultHidden(defaultHidden_) {
     selectedOption = defaultOption;
     hidden = defaultHidden;
     SetVariable();
 }
-Option::Option(bool var_, std::string name_, std::vector<std::string> options_, OptionCategory category_,
-               uint8_t defaultOption_, bool defaultHidden_)
+Option::Option(bool var_, std::string name_, std::vector<std::string> options_, const OptionCategory category_,
+               const uint8_t defaultOption_, const bool defaultHidden_)
     : var(var_), name(std::move(name_)), options(std::move(options_)), category(category_),
       defaultOption(defaultOption_), defaultHidden(defaultHidden_) {
     selectedOption = defaultOption;
@@ -97,25 +96,24 @@ Option::Option(bool var_, std::string name_, std::vector<std::string> options_, 
     SetVariable();
 }
 
-OptionGroup::OptionGroup(std::string name, std::vector<Option*> options, OptionGroupType groupType, bool printInSpoiler,
-                         OptionGroupType containsType)
+OptionGroup::OptionGroup(std::string name, std::vector<Option*> options, const OptionGroupType groupType,
+                         const bool printInSpoiler, const OptionGroupType containsType)
     : mName(std::move(name)), mOptions(std::move(options)), mGroupType(groupType), mPrintInSpoiler(printInSpoiler),
       mContainsType(containsType) {
 }
 
-OptionGroup::OptionGroup(std::string name, std::vector<OptionGroup*> subGroups, OptionGroupType groupType,
-                         bool printInSpoiler, OptionGroupType containsType)
+OptionGroup::OptionGroup(std::string name, std::vector<OptionGroup*> subGroups, const OptionGroupType groupType,
+                         const bool printInSpoiler, const OptionGroupType containsType)
     : mName(std::move(name)), mSubGroups(std::move(subGroups)), mGroupType(groupType), mPrintInSpoiler(printInSpoiler),
       mContainsType(containsType) {
 }
 
-OptionGroup OptionGroup::SubGroup(std::string name, std::vector<Option*> options, bool printInSpoiler) {
-    return OptionGroup(std::move(name), std::move(options), OptionGroupType::SUBGROUP, printInSpoiler);
+OptionGroup OptionGroup::SubGroup(std::string name, std::vector<Option*> options, const bool printInSpoiler) {
+    return {std::move(name), std::move(options), OptionGroupType::SUBGROUP, printInSpoiler};
 }
 
-OptionGroup OptionGroup::SubGroup(std::string name, std::vector<OptionGroup*> subGroups, bool printInSpoiler) {
-    return OptionGroup(std::move(name), std::move(subGroups), OptionGroupType::SUBGROUP, printInSpoiler,
-                       OptionGroupType::SUBGROUP);
+OptionGroup OptionGroup::SubGroup(std::string name, std::vector<OptionGroup*> subGroups, const bool printInSpoiler) {
+    return {std::move(name), std::move(subGroups), OptionGroupType::SUBGROUP, printInSpoiler, OptionGroupType::SUBGROUP};
 }
 
 const std::string& OptionGroup::GetName() const {
