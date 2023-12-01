f31eeddb22459b19eeb9671356b5f797b7a9cbf4
Garrett Cox <garrettjcox@gmail.com>
2023-11-30 20:53:32 -0600

    Support importing text as XML

--- soh/soh/resource/importer/TextFactory.cpp
+++ soh/soh/resource/importer/TextFactory.cpp
@@ -1,8 +1,12 @@
 #include "soh/resource/importer/TextFactory.h"
 #include "soh/resource/type/Text.h"
 #include "spdlog/spdlog.h"
+#include "include/message_data_fmt.h"
+#include "include/message_data_textbox_types.h"
 
 namespace LUS {
+void WriteFileXML(tinyxml2::XMLDocument* writer, std::shared_ptr<IResource> resource);
+
 std::shared_ptr<IResource>
 TextFactory::ReadResource(std::shared_ptr<ResourceInitData> initData, std::shared_ptr<BinaryReader> reader) {
     auto resource = std::make_shared<Text>(initData);
@@ -23,6 +27,23 @@ TextFactory::ReadResource(std::shared_ptr<ResourceInitData> initData, std::share
     }
 
     factory->ParseFileBinary(reader, resource);
+
+    // /*
+    // See WriteFileXML, this will not create the export path folders, so you will need to create them yourself or the export will fail with error code 4
+    // tinyxml2::XMLDocument doc;
+    // auto writer = doc.NewElement("Text");
+    // writer->SetAttribute("Version", resource->GetInitData()->ResourceVersion);
+    // doc.InsertFirstChild(writer);
+
+    // WriteFileXML(&doc, resource);
+
+    // auto res = doc.SaveFile(("export/" + resource->GetInitData()->Path).c_str());
+    // if (res != tinyxml2::XML_SUCCESS) {
+    //     SPDLOG_ERROR("Failed to export resource {} as XML Error:{}", resource->GetInitData()->Path, res);
+    // } else {
+    //     SPDLOG_INFO("Exported resource {} as XML", resource->GetInitData()->Path);
+    // }
+    // */
 
     return resource;
 }
@@ -67,26 +88,224 @@ void LUS::TextFactoryV0::ParseFileBinary(std::shared_ptr<BinaryReader> reader,
     }
 }
 void TextFactoryV0::ParseFileXML(tinyxml2::XMLElement* reader, std::shared_ptr<IResource> resource) {
-    std::shared_ptr<Text> txt = std::static_pointer_cast<Text>(resource);
+    std::shared_ptr<Text> textResource = std::static_pointer_cast<Text>(resource);
 
     auto child = reader->FirstChildElement();
 
     while (child != nullptr) {
         std::string childName = child->Name();
-
-        if (childName == "TextEntry") {
-            MessageEntry entry;
-            entry.id = child->IntAttribute("ID");
-            entry.textboxType = child->IntAttribute("TextboxType");
-            entry.textboxYPos = child->IntAttribute("TextboxYPos");
-            entry.msg = child->Attribute("Message");
-            entry.msg += "\x2";
-
-            txt->messages.push_back(entry);
-            int bp = 0;
+        if (childName != "TextEntry") {
+            SPDLOG_WARN("Unknown element {}. Text Resource:{}", childName, textResource->GetInitData()->Path);
+            child = child->NextSiblingElement();
+            continue;
         }
 
+        MessageEntry entry;
+        if (child->QueryUnsignedAttribute("ID", (uint*)&entry.id) != tinyxml2::XML_SUCCESS) {
+            SPDLOG_WARN("Failed to parse ID attribute. Text Resource:{}", textResource->GetInitData()->Path);
+            child = child->NextSiblingElement();
+            continue;
+        }
+
+        std::string textboxType = child->Attribute("TextboxType");
+        if (XMLToTextBoxType.contains(textboxType)) {
+            entry.textboxType = XMLToTextBoxType.at(textboxType);
+        } else {
+            SPDLOG_WARN("Unknown TextboxType {}. TextEntry:{} Text Resource:{}", textboxType, entry.id, textResource->GetInitData()->Path);
+            child = child->NextSiblingElement();
+            continue;
+        }
+
+        std::string textboxYPos = child->Attribute("TextboxYPos");
+        if (XMLToTextBoxPosition.contains(textboxYPos)) {
+            entry.textboxYPos = XMLToTextBoxPosition.at(textboxYPos);
+        } else {
+            SPDLOG_WARN("Unknown TextboxYPos {}. TextEntry:{} Text Resource:{}", textboxYPos, entry.id, textResource->GetInitData()->Path);
+            child = child->NextSiblingElement();
+            continue;
+        }
+
+        auto textNode = child->FirstChildElement();
+        int dummy = 0;
+
+        while (textNode != nullptr) {
+            std::string textNodeName = textNode->Name();
+            if (textNodeName == "TextNode") {
+
+                if (textNode->GetText() == nullptr) {
+                    entry.msg += ' ';
+                    textNode = textNode->NextSiblingElement();
+                    continue;
+                }
+
+                std::string text = textNode->GetText();
+                entry.msg += text;
+            } else if (XMLToControlCode.contains(textNodeName)) {
+                entry.msg += XMLToControlCode.at(textNodeName);
+
+                switch(XMLToControlCode.at(textNodeName)) {
+                    case MESSAGE_COLOR: {
+                        std::string value = textNode->Attribute("Value");
+                        if (!XMLToColorCode.contains(value)) {
+                            SPDLOG_WARN("Unknown color {}. TextEntry:{} Text Resource:{}", value, entry.id, textResource->GetInitData()->Path);
+                            entry.msg += XMLToColorCode.at("Default");
+                            break;
+                        }
+                        entry.msg += XMLToColorCode.at(value);
+                        break;
+                    }
+                    case MESSAGE_SHIFT:
+                    case MESSAGE_BOX_BREAK_DELAYED:
+                    case MESSAGE_FADE:
+                    case MESSAGE_ITEM_ICON:
+                    case MESSAGE_TEXT_SPEED:
+                    case MESSAGE_HIGHSCORE:
+                        if (textNode->QueryIntAttribute("Value", &dummy) != tinyxml2::XML_SUCCESS) {
+                            SPDLOG_WARN("Failed to get Value attribute. ControlNode:{} TextEntry:{} Text Resource:{}", textNodeName, entry.id, textResource->GetInitData()->Path);
+                            entry.msg += '\0';
+                            break;
+                        }
+
+                        entry.msg += textNode->IntAttribute("Value");
+                        break;
+                    case MESSAGE_TEXTID:
+                    case MESSAGE_FADE2:
+                    case MESSAGE_SFX:
+                        if (textNode->QueryIntAttribute("Value", &dummy) != tinyxml2::XML_SUCCESS) {
+                            SPDLOG_WARN("Failed to get Value attribute. ControlNode:{} TextEntry:{} Text Resource:{}", textNodeName, entry.id, textResource->GetInitData()->Path);
+                            entry.msg += '\0';
+                            entry.msg += '\0';
+                            break;
+                        }
+
+                        entry.msg += textNode->IntAttribute("Value") >> 8;
+                        entry.msg += textNode->IntAttribute("Value") & 0xFF;
+                        break;
+                    case MESSAGE_BACKGROUND:
+                        if (
+                            textNode->QueryIntAttribute("R", &dummy) != tinyxml2::XML_SUCCESS || 
+                            textNode->QueryIntAttribute("G", &dummy) != tinyxml2::XML_SUCCESS || 
+                            textNode->QueryIntAttribute("B", &dummy) != tinyxml2::XML_SUCCESS
+                        ) {
+                            SPDLOG_WARN("Failed to get RGB attributes. ControlNode:{} TextEntry:{} Text Resource:{}", textNodeName, entry.id, textResource->GetInitData()->Path);
+                            entry.msg += '\0';
+                            entry.msg += '\0';
+                            entry.msg += '\0';
+                            break;
+                        }
+
+                        entry.msg += textNode->IntAttribute("R");
+                        entry.msg += textNode->IntAttribute("G");
+                        entry.msg += textNode->IntAttribute("B");
+                        break;
+                }
+            } else {
+                SPDLOG_WARN("Unknown element {}. TextEntry:{} Text Resource:{}", textNodeName, entry.id, textResource->GetInitData()->Path);
+            }
+            textNode = textNode->NextSiblingElement();
+        }
+
+        // Replace special characters
+        for (auto [key, value] : XMLToSpecialChar) {
+            size_t pos = 0;
+            while ((pos = entry.msg.find(key)) != std::string::npos) {
+                entry.msg.replace(pos, key.size(), value);
+            }
+        }
+
+        textResource->messages.push_back(entry);
         child = child->NextSiblingElement();
+    }
+}
+
+/**
+ * This is an example implementation of how to write a V0 Text resource to XML.
+ * It is currently not in use anywhere, this will eventually be migrated to a separate system or tool.
+ * 
+ * To make use of this tool, uncomment the lines in TextFactory::ReadResource
+ */
+void WriteFileXML(tinyxml2::XMLDocument* writer, std::shared_ptr<IResource> resource) {
+    std::shared_ptr<Text> textResource = std::static_pointer_cast<Text>(resource);
+
+    auto root = writer->FirstChildElement();
+
+    for (auto& entry : textResource->messages) {
+        tinyxml2::XMLElement* textEntryNode = writer->NewElement("TextEntry");
+        textEntryNode->SetAttribute("ID", entry.id);
+        textEntryNode->SetAttribute("TextboxType", TextBoxTypeToXML.at(entry.textboxType).c_str());
+        textEntryNode->SetAttribute("TextboxYPos", TextBoxPositionToXML.at(entry.textboxYPos).c_str());
+
+        std::string currentText = "";
+        bool shouldCreateControlNode = false;
+        auto nextControlNode = writer->NewElement("ControlNode");
+
+        for (size_t i = 0; i < entry.msg.size(); i++) {
+            int skip = 0;
+
+            if (ControlCodeToXML.contains(entry.msg[i])) {
+                nextControlNode->SetName(ControlCodeToXML.at(entry.msg[i]).c_str());
+                shouldCreateControlNode = true;
+
+                switch(entry.msg[i]) {
+                    case MESSAGE_COLOR: {
+                        skip = 1;
+                        int color = entry.msg[i + 1] - 0x40;
+                        if (!ColorCodeToXML.contains(color)) {
+                            SPDLOG_WARN("Unknown color code {}. TextEntry:{} Text Resource:{}", color, entry.id, textResource->GetInitData()->Path);
+                            nextControlNode->SetAttribute("Value", ColorCodeToXML.at(MSGCOL_DEFAULT).c_str());
+                            break;
+                        }
+                        nextControlNode->SetAttribute("Value", ColorCodeToXML.at(color).c_str());
+                        break;
+                    }
+                    case MESSAGE_SHIFT:
+                    case MESSAGE_BOX_BREAK_DELAYED:
+                    case MESSAGE_FADE:
+                    case MESSAGE_ITEM_ICON:
+                    case MESSAGE_TEXT_SPEED:
+                    case MESSAGE_HIGHSCORE:
+                        skip = 1;
+                        nextControlNode->SetAttribute("Value", entry.msg[i + 1]);
+                        break;
+                    case MESSAGE_TEXTID:
+                    case MESSAGE_FADE2:
+                    case MESSAGE_SFX: {
+                        skip = 2;
+                        nextControlNode->SetAttribute("Value", entry.msg[i + 1] << 8 | entry.msg[i + 2]);
+                        break;
+                    }
+                    case MESSAGE_BACKGROUND: {
+                        skip = 3;
+                        nextControlNode->SetAttribute("R", entry.msg[i + 1]);
+                        nextControlNode->SetAttribute("G", entry.msg[i + 2]);
+                        nextControlNode->SetAttribute("B", entry.msg[i + 3]);
+                        break;
+                    }
+                }
+            } else if (SpecialCharToXML.contains(entry.msg[i])) {
+                currentText += SpecialCharToXML.at(entry.msg[i]);
+            } else {
+                currentText += entry.msg[i];
+            }
+
+            // If (we're at the end of the message or we need to create a control node) and we have text, create a text node
+            if ((shouldCreateControlNode || i == entry.msg.size() - 1) && currentText.size() > 0) {
+                auto textNode = writer->NewElement("TextNode");
+                textNode->SetText(currentText.c_str());
+                textEntryNode->InsertEndChild(textNode);
+                currentText = "";
+            }
+
+            if (shouldCreateControlNode) {
+                textEntryNode->InsertEndChild(nextControlNode);
+                nextControlNode = writer->NewElement("ControlNode");
+                shouldCreateControlNode = false;
+            }
+
+            i += skip;
+        }
+
+        root->InsertEndChild(textEntryNode);
     }
 }
 
--- soh/soh/resource/importer/TextFactory.h
+++ soh/soh/resource/importer/TextFactory.h
@@ -2,6 +2,9 @@
 
 #include "Resource.h"
 #include "ResourceFactory.h"
+#include "include/message_data_fmt.h"
+#include "include/message_data_textbox_types.h"
+#include <unordered_map>
 
 namespace LUS {
 class TextFactory : public ResourceFactory
@@ -21,3 +24,220 @@ class TextFactoryV0 : public ResourceVersionFactory
 };
 }; // namespace LUS
 
+const std::unordered_map<std::string, int> XMLToTextBoxType = {
+    { "Black",        TEXTBOX_TYPE_BLACK },
+    { "Wooden",       TEXTBOX_TYPE_WOODEN },
+    { "Blue",         TEXTBOX_TYPE_BLUE },
+    { "Ocarina",      TEXTBOX_TYPE_OCARINA },
+    { "NoneBottom",   TEXTBOX_TYPE_NONE_BOTTOM },
+    { "NoneNoShadow", TEXTBOX_TYPE_NONE_NO_SHADOW },
+    { "Credits",      TEXTBOX_TYPE_CREDITS }
+};
+
+const std::unordered_map<int, std::string> TextBoxTypeToXML = {
+    { TEXTBOX_TYPE_BLACK,          "Black" },
+    { TEXTBOX_TYPE_WOODEN,         "Wooden" },
+    { TEXTBOX_TYPE_BLUE,           "Blue" },
+    { TEXTBOX_TYPE_OCARINA,        "Ocarina" },
+    { TEXTBOX_TYPE_NONE_BOTTOM,    "NoneBottom" },
+    { TEXTBOX_TYPE_NONE_NO_SHADOW, "NoneNoShadow" },
+    { TEXTBOX_TYPE_CREDITS,        "Credits" }
+};
+
+const std::unordered_map<std::string, int> XMLToTextBoxPosition = {
+    { "Variable", TEXTBOX_POS_VARIABLE },
+    { "Top",      TEXTBOX_POS_TOP },
+    { "Middle",   TEXTBOX_POS_MIDDLE },
+    { "Bottom",   TEXTBOX_POS_BOTTOM }
+};
+
+const std::unordered_map<int, std::string> TextBoxPositionToXML = {
+    { TEXTBOX_POS_VARIABLE, "Variable" },
+    { TEXTBOX_POS_TOP,      "Top" },
+    { TEXTBOX_POS_MIDDLE,   "Middle" },
+    { TEXTBOX_POS_BOTTOM,   "Bottom" }
+};
+
+const std::unordered_map<std::string, int> XMLToControlCode = {
+    { "Newline",          MESSAGE_NEWLINE },
+    { "End",              MESSAGE_END },
+    { "BoxBreak",         MESSAGE_BOX_BREAK },
+    { "Color",            MESSAGE_COLOR },
+    { "Shift",            MESSAGE_SHIFT },
+    { "TextID",           MESSAGE_TEXTID },
+    { "QuicktextEnable",  MESSAGE_QUICKTEXT_ENABLE },
+    { "QuicktextDisable", MESSAGE_QUICKTEXT_DISABLE },
+    { "Persistent",       MESSAGE_PERSISTENT },
+    { "Event",            MESSAGE_EVENT },
+    { "BoxBreakDelayed",  MESSAGE_BOX_BREAK_DELAYED },
+    { "AwaitButtonPress", MESSAGE_AWAIT_BUTTON_PRESS },
+    { "Fade",             MESSAGE_FADE },
+    { "Name",             MESSAGE_NAME },
+    { "Ocarina",          MESSAGE_OCARINA },
+    { "Fade2",            MESSAGE_FADE2 },
+    { "SFX",              MESSAGE_SFX },
+    { "ItemIcon",         MESSAGE_ITEM_ICON },
+    { "TextSpeed",        MESSAGE_TEXT_SPEED },
+    { "Background",       MESSAGE_BACKGROUND },
+    { "MarathonTime",     MESSAGE_MARATHON_TIME },
+    { "RaceTime",         MESSAGE_RACE_TIME },
+    { "Points",           MESSAGE_POINTS },
+    { "Tokens",           MESSAGE_TOKENS },
+    { "Unskippable",      MESSAGE_UNSKIPPABLE },
+    { "TwoChoice",        MESSAGE_TWO_CHOICE },
+    { "ThreeChoice",      MESSAGE_THREE_CHOICE },
+    { "FishInfo",         MESSAGE_FISH_INFO },
+    { "Highscore",        MESSAGE_HIGHSCORE },
+    { "Time",             MESSAGE_TIME },
+};
+
+const std::unordered_map<int, std::string> ControlCodeToXML = {
+    { MESSAGE_NEWLINE,            "Newline", },
+    { MESSAGE_END,                "End", },
+    { MESSAGE_BOX_BREAK,          "BoxBreak", },
+    { MESSAGE_COLOR,              "Color", },
+    { MESSAGE_SHIFT,              "Shift", },
+    { MESSAGE_TEXTID,             "TextID", },
+    { MESSAGE_QUICKTEXT_ENABLE,   "QuicktextEnable", },
+    { MESSAGE_QUICKTEXT_DISABLE,  "QuicktextDisable",},
+    { MESSAGE_PERSISTENT,         "Persistent", },
+    { MESSAGE_EVENT,              "Event", },
+    { MESSAGE_BOX_BREAK_DELAYED,  "BoxBreakDelayed", },
+    { MESSAGE_AWAIT_BUTTON_PRESS, "AwaitButtonPress",},
+    { MESSAGE_FADE,               "Fade", },
+    { MESSAGE_NAME,               "Name", },
+    { MESSAGE_OCARINA,            "Ocarina", },
+    { MESSAGE_FADE2,              "Fade2", },
+    { MESSAGE_SFX,                "SFX", },
+    { MESSAGE_ITEM_ICON,          "ItemIcon", },
+    { MESSAGE_TEXT_SPEED,         "TextSpeed", },
+    { MESSAGE_BACKGROUND,         "Background", },
+    { MESSAGE_MARATHON_TIME,      "MarathonTime", },
+    { MESSAGE_RACE_TIME,          "RaceTime", },
+    { MESSAGE_POINTS,             "Points", },
+    { MESSAGE_TOKENS,             "Tokens", },
+    { MESSAGE_UNSKIPPABLE,        "Unskippable", },
+    { MESSAGE_TWO_CHOICE,         "TwoChoice", },
+    { MESSAGE_THREE_CHOICE,       "ThreeChoice", },
+    { MESSAGE_FISH_INFO,          "FishInfo", },
+    { MESSAGE_HIGHSCORE,          "Highscore", },
+    { MESSAGE_TIME,               "Time", },
+};
+
+const std::unordered_map<std::string, int> XMLToColorCode = {
+    { "Default",    MSGCOL_DEFAULT },
+    { "Red",        MSGCOL_RED },
+    { "Adjustable", MSGCOL_ADJUSTABLE },
+    { "Blue",       MSGCOL_BLUE },
+    { "LightBlue",  MSGCOL_LIGHTBLUE },
+    { "Purple",     MSGCOL_PURPLE },
+    { "Yellow",     MSGCOL_YELLOW },
+    { "Black",      MSGCOL_BLACK },
+};
+
+const std::unordered_map<int, std::string> ColorCodeToXML = {
+    { MSGCOL_DEFAULT,    "Default" },
+    { MSGCOL_RED,        "Red" },
+    { MSGCOL_ADJUSTABLE, "Adjustable" },
+    { MSGCOL_BLUE,       "Blue" },
+    { MSGCOL_LIGHTBLUE,  "LightBlue" },
+    { MSGCOL_PURPLE,     "Purple" },
+    { MSGCOL_YELLOW,     "Yellow" },
+    { MSGCOL_BLACK,      "Black" },
+};
+
+const std::unordered_map<std::string, std::string> XMLToSpecialChar = {
+    { "‾",             "\x7F" },
+    { "À",             "\x80" },
+    { "î",             "\x81" },
+    { "Â",             "\x82" },
+    { "Ä",             "\x83" },
+    { "Ç",             "\x84" },
+    { "È",             "\x85" },
+    { "É",             "\x86" },
+    { "Ê",             "\x87" },
+    { "Ë",             "\x88" },
+    { "Ï",             "\x89" },
+    { "Ô",             "\x8A" },
+    { "Ö",             "\x8B" },
+    { "Ù",             "\x8C" },
+    { "Û",             "\x8D" },
+    { "Ü",             "\x8E" },
+    { "ß",             "\x8F" },
+    { "à",             "\x90" },
+    { "á",             "\x91" },
+    { "â",             "\x92" },
+    { "ä",             "\x93" },
+    { "ç",             "\x94" },
+    { "è",             "\x95" },
+    { "é",             "\x96" },
+    { "ê",             "\x97" },
+    { "ë",             "\x98" },
+    { "ï",             "\x99" },
+    { "ô",             "\x9A" },
+    { "ö",             "\x9B" },
+    { "ù",             "\x9C" },
+    { "û",             "\x9D" },
+    { "ü",             "\x9E" },
+    { "[A]",           "\x9F" },
+    { "[B]",           "\xA0" },
+    { "[C]",           "\xA1" },
+    { "[L]",           "\xA2" },
+    { "[R]",           "\xA3" },
+    { "[Z]",           "\xA4" },
+    { "[C-Up]",        "\xA5" },
+    { "[C-Down]",      "\xA6" },
+    { "[C-Left]",      "\xA7" },
+    { "[C-Right]",     "\xA8" },
+    { "▼",             "\xA9" },
+    { "[Control-Pad]", "\xAA" },
+    { "[D-Pad]",       "\xAB" },
+};
+
+const std::unordered_map<int, std::string> SpecialCharToXML = {
+    { '\x7F', "‾" },
+    { '\x80', "À" },
+    { '\x81', "î" },
+    { '\x82', "Â" },
+    { '\x83', "Ä" },
+    { '\x84', "Ç" },
+    { '\x85', "È" },
+    { '\x86', "É" },
+    { '\x87', "Ê" },
+    { '\x88', "Ë" },
+    { '\x89', "Ï" },
+    { '\x8A', "Ô" },
+    { '\x8B', "Ö" },
+    { '\x8C', "Ù" },
+    { '\x8D', "Û" },
+    { '\x8E', "Ü" },
+    { '\x8F', "ß" },
+    { '\x90', "à" },
+    { '\x91', "á" },
+    { '\x92', "â" },
+    { '\x93', "ä" },
+    { '\x94', "ç" },
+    { '\x95', "è" },
+    { '\x96', "é" },
+    { '\x97', "ê" },
+    { '\x98', "ë" },
+    { '\x99', "ï" },
+    { '\x9A', "ô" },
+    { '\x9B', "ö" },
+    { '\x9C', "ù" },
+    { '\x9D', "û" },
+    { '\x9E', "ü" },
+    { '\x9F', "[A]" },
+    { '\xA0', "[B]" },
+    { '\xA1', "[C]" },
+    { '\xA2', "[L]" },
+    { '\xA3', "[R]" },
+    { '\xA4', "[Z]" },
+    { '\xA5', "[C-Up]" },
+    { '\xA6', "[C-Down]" },
+    { '\xA7', "[C-Left]" },
+    { '\xA8', "[C-Right]" },
+    { '\xA9', "▼" },
+    { '\xAA', "[Control-Pad]" },
+    { '\xAB', "[D-Pad]" },
+};
