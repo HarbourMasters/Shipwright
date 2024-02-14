74d81aab95578d396c40030175195a04a5795b18
Patrick12115 <Patrick12115@hotmail.com>
2024-02-13 19:19:41 -0500

    Rename stuff and add longshot

---
+++ soh/assets/objects/object_custom_equip/object_custom_equip.h
@@ -0,0 +1,100 @@
+#ifndef OBJECTS_OBJECT_CUSTOM_EQUIP_H
+#define OBJECTS_OBJECT_CUSTOM_EQUIP 1
+
+#include "align_asset_macro.h"
+
+// Custom Equipment Display Lists
+#define dgCustomBowDL "__OTR__objects/object_custom_equip/gCustomBowDL"
+static const ALIGN_ASSET(2) char gCustomBowDL[] = dgCustomBowDL;
+
+#define dgCustomHammerDL "__OTR__objects/object_custom_equip/gCustomHammerDL"
+static const ALIGN_ASSET(2) char gCustomHammerDL[] = dgCustomHammerDL;
+
+#define dgCustomHookshotDL "__OTR__objects/object_custom_equip/gCustomHookshotDL"
+static const ALIGN_ASSET(2) char gCustomHookshotDL[] = dgCustomHookshotDL;
+
+#define dgCustomLongshotDL "__OTR__objects/object_custom_equip/gCustomLongshotDL"
+static const ALIGN_ASSET(2) char gCustomLongshotDL[] = dgCustomLongshotDL;
+
+#define dgCustomHookshotTipDL "__OTR__objects/object_custom_equip/gCustomHookshotTipDL"
+static const ALIGN_ASSET(2) char gCustomHookshotTipDL[] = dgCustomHookshotTipDL;
+
+#define dgCustomHookshotChainDL "__OTR__objects/object_custom_equip/gCustomHookshotChainDL"
+static const ALIGN_ASSET(2) char gCustomHookshotChainDL[] = dgCustomHookshotChainDL;
+
+#define dgCustomSlingshotDL "__OTR__objects/object_custom_equip/gCustomSlingshotDL"
+static const ALIGN_ASSET(2) char gCustomSlingshotDL[] = dgCustomSlingshotDL;
+
+#define dgCustomFairyOcarinaDL "__OTR__objects/object_custom_equip/gCustomFairyOcarinaDL"
+static const ALIGN_ASSET(2) char gCustomFairyOcarinaDL[] = dgCustomFairyOcarinaDL;
+
+#define dgCustomOcarinaOfTimeDL "__OTR__objects/object_custom_equip/gCustomOcarinaOfTimeDL"
+static const ALIGN_ASSET(2) char gCustomOcarinaOfTimeDL[] = dgCustomOcarinaOfTimeDL;
+
+#define dgCustomBoomerangDL "__OTR__objects/object_custom_equip/gCustomBoomerangDL"
+static const ALIGN_ASSET(2) char gCustomBoomerangDL[] = dgCustomBoomerangDL;
+
+#define dgCustomKokiriSwordDL "__OTR__objects/object_custom_equip/gCustomKokiriSwordDL"
+static const ALIGN_ASSET(2) char gCustomKokiriSwordDL[] = dgCustomKokiriSwordDL;
+
+#define dgCustomKokiriSwordSheathDL "__OTR__objects/object_custom_equip/gCustomKokiriSwordSheathDL"
+static const ALIGN_ASSET(2) char gCustomKokiriSwordSheathDL[] = dgCustomKokiriSwordSheathDL;
+
+#define dgCustomKokiriSwordInSheathDL "__OTR__objects/object_custom_equip/gCustomKokiriSwordInSheathDL"
+static const ALIGN_ASSET(2) char gCustomKokiriSwordInSheathDL[] = dgCustomKokiriSwordInSheathDL;
+
+#define dgCustomMasterSwordDL "__OTR__objects/object_custom_equip/gCustomMasterSwordDL"
+static const ALIGN_ASSET(2) char gCustomMasterSwordDL[] = dgCustomMasterSwordDL;
+
+#define dgCustomMasterSwordSheathDL "__OTR__objects/object_custom_equip/gCustomMasterSwordSheathDL"
+static const ALIGN_ASSET(2) char gCustomMasterSwordSheathDL[] = dgCustomMasterSwordSheathDL;
+
+#define dgCustomMasterSwordInSheathDL "__OTR__objects/object_custom_equip/gCustomMasterSwordInSheathDL"
+static const ALIGN_ASSET(2) char gCustomMasterSwordInSheathDL[] = dgCustomMasterSwordInSheathDL;
+
+#define dgCustomLongswordDL "__OTR__objects/object_custom_equip/gCustomLongswordDL"
+static const ALIGN_ASSET(2) char gCustomLongswordDL[] = dgCustomLongswordDL;
+
+#define dgCustomBrokenLongswordDL "__OTR__objects/object_custom_equip/gCustomBrokenLongswordDL"
+static const ALIGN_ASSET(2) char gCustomBrokenLongswordDL[] = dgCustomBrokenLongswordDL;
+
+#define dgCustomLongswordSheathDL "__OTR__objects/object_custom_equip/gCustomLongswordSheathDL"
+static const ALIGN_ASSET(2) char gCustomLongswordSheathDL[] = dgCustomLongswordSheathDL;
+
+#define dgCustomLongswordInSheathDL "__OTR__objects/object_custom_equip/gCustomLongswordInSheathDL"
+static const ALIGN_ASSET(2) char gCustomLongswordInSheathDL[] = dgCustomLongswordInSheathDL;
+
+#define dgCustomDekuShieldDL "__OTR__objects/object_custom_equip/gCustomDekuShieldDL"
+static const ALIGN_ASSET(2) char gCustomDekuShieldDL[] = dgCustomDekuShieldDL;
+
+#define dgCustomDekuShieldOnBackDL "__OTR__objects/object_custom_equip/gCustomDekuShieldOnBackDL"
+static const ALIGN_ASSET(2) char gCustomDekuShieldOnBackDL[] = dgCustomDekuShieldOnBackDL;
+
+#define dgCustomDekuShieldOnBackScaledDL "__OTR__objects/object_custom_equip/gCustomDekuShieldOnBackScaledDL"
+static const ALIGN_ASSET(2) char gCustomDekuShieldOnBackScaledDL[] = dgCustomDekuShieldOnBackScaledDL;
+
+#define dgCustomHylianShieldDL "__OTR__objects/object_custom_equip/gCustomHylianShieldDL"
+static const ALIGN_ASSET(2) char gCustomHylianShieldDL[] = dgCustomHylianShieldDL;
+
+#define dgCustomHylianShieldOnBackDL "__OTR__objects/object_custom_equip/gCustomHylianShieldOnBackDL"
+static const ALIGN_ASSET(2) char gCustomHylianShieldOnBackDL[] = dgCustomHylianShieldOnBackDL;
+
+#define dgCustomHylianShieldOnChildBackDL "__OTR__objects/object_custom_equip/gCustomHylianShieldOnChildBackDL"
+static const ALIGN_ASSET(2) char gCustomHylianShieldOnChildBackDL[] = dgCustomHylianShieldOnChildBackDL;
+
+#define dgCustomMirrorShieldDL "__OTR__objects/object_custom_equip/gCustomMirrorShieldDL"
+static const ALIGN_ASSET(2) char gCustomMirrorShieldDL[] = dgCustomMirrorShieldDL;
+
+#define dgCustomMirrorShieldOnBackDL "__OTR__objects/object_custom_equip/gCustomMirrorShieldOnBackDL"
+static const ALIGN_ASSET(2) char gCustomMirrorShieldOnBackDL[] = dgCustomMirrorShieldOnBackDL;
+
+// Custom First Person Hand Display Lists
+#define dgCustomAdultFPSHandDL "__OTR__objects/object_custom_equip/gCustomAdultFPSHandDL"
+static const ALIGN_ASSET(2) char gCustomAdultFPSHandDL[] = dgCustomAdultFPSHandDL;
+
+#define dgCustomChildFPSHandDL "__OTR__objects/object_custom_equip/gCustomChildFPSHandDL"
+static const ALIGN_ASSET(2) char gCustomChildFPSHandDL[] = dgCustomChildFPSHandDL;
+
+
+
+#endif // OBJECTS_OBJECT_CUSTOM_EQUIP_H
