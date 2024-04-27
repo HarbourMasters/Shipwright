ce147944fc2054fe30f56e6f58d7846dd1656f65
Pepper0ni <93387759+Pepper0ni@users.noreply.github.com>
2024-04-27 16:46:48 +0100

    last cleanup

--- soh/soh/Enhancements/randomizer/3drando/custom_messages.hpp
+++ soh/soh/Enhancements/randomizer/3drando/custom_messages.hpp
@@ -7,15 +7,6 @@
 
 #include "text.hpp"
 
-/*#define QM_WHITE  0x00
-#define QM_RED    0x41
-#define QM_GREEN  0x42
-#define QM_BLUE   0x43
-#define QM_LBLUE  0x44
-#define QM_PINK   0x45
-#define QM_YELLOW 0x46
-#define QM_BLACK  0x47*/
-
 namespace CustomMessages {
 typedef struct {
     // In the true file format, offset is the offset into the QM file.
