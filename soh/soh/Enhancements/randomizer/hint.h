deaef74fcdfd66e986f49daf286cc8af007fc624
Pepper0ni <93387759+Pepper0ni@users.noreply.github.com>
2024-05-06 12:18:42 +0100

    Attempt to fix ambiguity issues and a V2 compatibility crash

--- soh/soh/Enhancements/randomizer/hint.h
+++ soh/soh/Enhancements/randomizer/hint.h
@@ -27,7 +27,7 @@ class Hint {
           std::vector<TrialKey> trials_ = {},
           bool yourPocket_ = false, 
           int num_ = 0);
-     Hint(RandomizerHint ownKey_, std::vector<CustomMessage> messages_);
+     explicit Hint(RandomizerHint ownKey_, std::vector<CustomMessage> messages_);
      Hint(RandomizerHint ownKey_, nlohmann::json json_);
      void FillGapsInData();
      void SetLocationsAsHinted() const;
