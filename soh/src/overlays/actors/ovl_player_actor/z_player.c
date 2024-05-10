db2960b580c7c15aebe6cd36ed7d5d6c82729fcc
Eric Hoey <121978037+A-Green-Spoon@users.noreply.github.com>
2024-05-09 22:04:20 -0400

    Update soh/src/overlays/actors/ovl_player_actor/z_player.c
    Co-authored-by: Garrett Cox <garrettjcox@gmail.com>

--- soh/src/overlays/actors/ovl_player_actor/z_player.c
+++ soh/src/overlays/actors/ovl_player_actor/z_player.c
@@ -12118,8 +12118,7 @@ void func_8084AEEC(Player* this, f32* arg1, f32 arg2, s16 arg3) {
             }
         // sControlInput is NULL to prevent inputs while surfacing after obtaining an underwater item so we want to ignore it for that case
         } else if (sControlInput != NULL) {
-            if (sControlInput == NULL) { // ignore if surfacing for Get Item after obtaining underwater item
-            } else if (CHECK_BTN_ALL(sControlInput->cur.button, BTN_MODIFIER1)) {
+            if (CHECK_BTN_ALL(sControlInput->cur.button, BTN_MODIFIER1)) {
                 swimMod *= CVarGetFloat(CVAR_SETTING("WalkModifier.SwimMapping1"), 1.0f);
             } else if (CHECK_BTN_ALL(sControlInput->cur.button, BTN_MODIFIER2)) {
                 swimMod *= CVarGetFloat(CVAR_SETTING("WalkModifier.SwimMapping2"), 1.0f);
