92ab46e75f53de95460ae510d400d43b36710f3a
briaguya <70942617+briaguya-ai@users.noreply.github.com>
2024-06-02 14:33:26 -0400

    this just comments out the part where we give lus the sohinputeditorwindow so it can use the default

--- soh/soh/OTRGlobals.cpp
+++ soh/soh/OTRGlobals.cpp
@@ -327,8 +327,9 @@ OTRGlobals::OTRGlobals() {
     context->InitCrashHandler();
     context->InitConsole();
 
-    auto sohInputEditorWindow = std::make_shared<SohInputEditorWindow>(CVAR_CONTROLLER_CONFIGURATION_WINDOW_OPEN, "Input Editor");
-    context->InitWindow({ sohInputEditorWindow });
+    // auto sohInputEditorWindow = std::make_shared<SohInputEditorWindow>(CVAR_CONTROLLER_CONFIGURATION_WINDOW_OPEN, "Input Editor");
+    // context->InitWindow({ sohInputEditorWindow });
+    context->InitWindow();
 
     auto overlay = context->GetInstance()->GetWindow()->GetGui()->GetGameOverlay();
     overlay->LoadFont("Press Start 2P", "fonts/PressStart2P-Regular.ttf", 12.0f);
