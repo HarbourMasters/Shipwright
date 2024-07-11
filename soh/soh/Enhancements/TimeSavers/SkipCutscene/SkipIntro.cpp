70ab77852491d3f17a3432af90b5232af2346741
Christopher Leggett <chris@leggett.dev>
2024-07-11 12:26:53 -0400

    Handle correct entrance for adult start + spawn shuffle and add comments.

--- soh/soh/Enhancements/TimeSavers/SkipCutscene/SkipIntro.cpp
+++ soh/soh/Enhancements/TimeSavers/SkipCutscene/SkipIntro.cpp
@@ -11,17 +11,30 @@ extern "C" {
 
 void SkipIntro_Register() {
     REGISTER_VB_SHOULD(VB_PLAY_TRANSITION_CS, {
-        if (CVarGetInteger(CVAR_ENHANCEMENT("TimeSavers.SkipCutscene.Intro"), IS_RANDO) ||
-            IS_RANDO && OTRGlobals::Instance->gRandoContext->GetOption(RSK_SHUFFLE_OVERWORLD_SPAWNS).Is(true)) {
+        // If we're playing rando and if starting age is adult and/or overworld spawns are shuffled we need to skip
+        // the cutscene regardless of the enhancement being on.
+        bool adultStart = OTRGlobals::Instance->gRandoContext->GetSettings()->ResolvedStartingAge() == RO_AGE_ADULT;
+        bool shuffleOverworldSpawns = OTRGlobals::Instance->gRandoContext->GetOption(RSK_SHUFFLE_OVERWORLD_SPAWNS).Is(true);
+        if (CVarGetInteger(CVAR_ENHANCEMENT("TimeSavers.SkipCutscene.Intro"), IS_RANDO) || (IS_RANDO && (adultStart || shuffleOverworldSpawns))) {
+                // Calculate spawn location. Start with vanilla, Link's house.
                 int32_t spawnEntrance = ENTR_LINKS_HOUSE_0;
+                // If we're not in rando, we can skip all of the below.
                 if (IS_RANDO) {
-                    if (OTRGlobals::Instance->gRandoContext->GetSettings()->ResolvedStartingAge() == RO_AGE_ADULT) {
+                    // If starting age is shuffled, use vanilla adult spawn/prelude warp.
+                    if (adultStart) {
                         spawnEntrance = ENTR_TEMPLE_OF_TIME_7;
                     }
-                    if (OTRGlobals::Instance->gRandoContext->GetOption(RSK_SHUFFLE_OVERWORLD_SPAWNS).Is(true)) {
+                    // If we're shuffling overworld spawns we'll need to get the Entrance Override
+                    if (shuffleOverworldSpawns) {
+                        // If we're shuffling overworld spawns the adult spawn is ENTR_HYRULE_FIELD_10 instead of
+                        // ENTR_TEMPLE_OF_TIME_7, so that spawn and Prelude don't share an entrance.
+                        if (adultStart){
+                            spawnEntrance = ENTR_HYRULE_FIELD_10;
+                        }
                         spawnEntrance = Entrance_GetOverride(spawnEntrance);
                     }
                 }
+                // Skip the intro cutscene for whatever the spawnEntrance is calculated to be.
                 if (gSaveContext.entranceIndex == spawnEntrance && gSaveContext.cutsceneIndex == 0xFFF1) {
                     gSaveContext.cutsceneIndex = 0;
                     *should = false;
