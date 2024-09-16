4b5e11ed0e3f7e36513a9049aaf028453d682971
Adam Bird <archez39@me.com>
2024-09-16 15:01:19 -0700

    remove old game over crash fix

--- soh/src/code/z_lights.c
+++ soh/src/code/z_lights.c
@@ -162,11 +162,7 @@ void Lights_BindAll(Lights* lights, LightNode* listHead, Vec3f* vec) {
 
     while (listHead != NULL) {
         info = listHead->info;
-        // OTRTODO: we do not know the root cause of the info->type value being invalid
-        // but this prevents it from crashing the game on the game over screen
-        if (info->type < 3) {
-            bindFuncs[info->type](lights, &info->params, vec);
-        }
+        bindFuncs[info->type](lights, &info->params, vec);
         listHead = listHead->next;
     }
 }
