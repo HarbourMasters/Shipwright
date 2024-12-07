1bc4b4688e11e27b4ccd209cb259d63dd0b51d1d
JordanLongstaff <jrdnlngstff@gmail.com>
2024-12-07 02:03:58 -0500

    Skip Water Temple portcullis opening cutscene

--- soh/soh/Enhancements/timesaver_hook_handlers.cpp
+++ soh/soh/Enhancements/timesaver_hook_handlers.cpp
@@ -306,6 +306,7 @@ void TimeSaverOnVanillaBehaviorHandler(GIVanillaBehavior id, bool* should, va_li
                     case ACTOR_BG_YDAN_MARUTA:
                     case ACTOR_BG_SPOT18_SHUTTER:
                     case ACTOR_BG_SPOT05_SOKO:
+                    case ACTOR_BG_SPOT06_OBJECTS:
                     case ACTOR_BG_SPOT18_BASKET:
                     case ACTOR_BG_HIDAN_CURTAIN:
                     case ACTOR_BG_MORI_HINERI:
