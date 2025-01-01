414ef04d78df609fd4593cd7ebd0b02e4f185d4e
Sonic Dreamcaster <alejandro.asenjo88@gmail.com>
2025-01-01 19:14:51 -0300

    PR review

--- soh/soh/OTRGlobals.cpp
+++ soh/soh/OTRGlobals.cpp
@@ -527,22 +527,8 @@ void OTRAudio_Thread() {
         //AudioMgr_ThreadEntry(&gAudioMgr);
         // 528 and 544 relate to 60 fps at 32 kHz 32000/60 = 533.333..
         // in an ideal world, one third of the calls should use num_samples=544 and two thirds num_samples=528
-        //#define SAMPLES_HIGH 560
-        //#define SAMPLES_LOW 528
-        // PAL values
-        //#define SAMPLES_HIGH 656
-        //#define SAMPLES_LOW 624
-
-        #if 0
-        // Values for 44100 hz
-        #define SAMPLES_HIGH 752
-        #define SAMPLES_LOW 720
-        #else
-        // Values for 32000 hz
         #define SAMPLES_HIGH 560
         #define SAMPLES_LOW 528
-
-        #endif
 
         #define AUDIO_FRAMES_PER_UPDATE (R_UPDATE_RATE > 0 ? R_UPDATE_RATE : 1 )
         #define NUM_AUDIO_CHANNELS 2
