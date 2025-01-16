8aedbc774d962add25dfe5e5ea93d5e7a024e0d6
Pepper0ni <93387759+Pepper0ni@users.noreply.github.com>
2025-01-16 20:10:42 +0000

    address reviews

--- soh/soh/Enhancements/randomizer/randomizer.cpp
+++ soh/soh/Enhancements/randomizer/randomizer.cpp
@@ -2182,8 +2182,6 @@ void RandomizerSettingsWindow::DrawElement() {
                     ImGui::SameLine();
                     ImGui::TextColored(ImVec4(1.0f, 0.0f, 0.0f, 1.0f), "Heads up! This will disable all rando settings except for entrance shuffle and starter items");
                 }
-
-                UIWidgets::PaddedSeparator();
                 ImGui::PopItemWidth();
                 ImGui::EndTable();
             }
