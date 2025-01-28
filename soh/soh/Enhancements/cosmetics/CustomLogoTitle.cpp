39603109d95bc104424d44e0a067b3d08c9e2f87
briaguya <70942617+briaguya-ai@users.noreply.github.com>
2025-01-27 20:16:31 -0500

    msvc plz

--- soh/soh/Enhancements/cosmetics/CustomLogoTitle.cpp
+++ soh/soh/Enhancements/cosmetics/CustomLogoTitle.cpp
@@ -4,6 +4,7 @@
 #include "textures/nintendo_rogo_static/nintendo_rogo_static.h"
 #include "assets/objects/gameplay_keep/gameplay_keep.h"
 #include "soh_assets.h"
+#include <algorithm>
 
 extern "C" {
 #include "macros.h"
