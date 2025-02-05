938ccf9ce0bdc3428f718f45a8108ac15962c1cb
Archez <archez39@me.com>
2025-02-05 18:27:46 -0500

    Fix save warping in links house with entrance rando

--- soh/soh/Enhancements/randomizer/randomizer_entrance.c
+++ soh/soh/Enhancements/randomizer/randomizer_entrance.c
@@ -394,7 +394,7 @@ void Entrance_SetSavewarpEntrance(void) {
     } else if (scene == SCENE_THIEVES_HIDEOUT) { // Theives hideout
         gSaveContext.entranceIndex = ENTR_THIEVES_HIDEOUT_0; // Gerudo Fortress -> Thieve's Hideout spawn 0
     } else if (scene == SCENE_LINKS_HOUSE) {
-        gSaveContext.entranceIndex = Entrance_OverrideNextIndex(ENTR_LINKS_HOUSE_CHILD_SPAWN);
+        gSaveContext.entranceIndex = ENTR_LINKS_HOUSE_CHILD_SPAWN; // Save warping in Link's house keeps the player there
     } else if (CVarGetInteger(CVAR_ENHANCEMENT("RememberSaveLocation"), 0) && scene != SCENE_FAIRYS_FOUNTAIN && scene != SCENE_GROTTOS &&
                gSaveContext.entranceIndex != ENTR_LOAD_OPENING) {
         // Use the saved entrance value with remember save location, except when in grottos/fairy fountains or if
