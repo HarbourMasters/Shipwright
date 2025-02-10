249104fe94275d8db9d5c051adf947b28f897c22
Christopher Leggett <chris@leggett.dev>
2025-02-10 18:56:49 -0500

    Initial commit for ShuffleSilverRupees files.

---
+++ soh/soh/Enhancements/randomizer/ShuffleSilverRupees.h
@@ -0,0 +1,13 @@
+#ifndef SHUFFLESILVERRUPEES_H
+#define SHUFFLESILVERRUPEES_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+void EnGSwitch_RandomizerInit(void* actorRef);
+#ifdef __cplusplus
+};
+#endif
+
+
+#endif //SHUFLESILVERRUPEES_H
