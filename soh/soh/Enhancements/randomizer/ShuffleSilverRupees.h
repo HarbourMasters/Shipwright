d10a89c500aa227db53aa1bb91233d7ae9e60bd1
Christopher Leggett <chris@leggett.dev>
2025-02-10 21:23:19 -0500

    Add Locations and Silver Counter class

--- soh/soh/Enhancements/randomizer/ShuffleSilverRupees.h
+++ soh/soh/Enhancements/randomizer/ShuffleSilverRupees.h
@@ -9,5 +9,20 @@ void EnGSwitch_RandomizerInit(void* actorRef);
 };
 #endif
 
+namespace Rando {
+    class SilverRupeeCounter {
+    public:
+        SilverRupeeCounter();
+        SilverRupeeCounter(uint8_t total, RandomizerGet randoGet);
+
+        uint8_t GetCollected() const;
+        uint8_t GetTotal() const;
+        void IncrementCollected(uint8_t amount = 1)
+    private:
+        uint8_t mCollected = 0;
+        uint8_t mTotal;
+        RandomizerGet mRandoGet;
+    }
+}
 
 #endif //SHUFLESILVERRUPEES_H
