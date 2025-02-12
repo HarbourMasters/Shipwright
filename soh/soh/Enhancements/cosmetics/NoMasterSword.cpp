75dda7b52190dc95805b82e3bd0ec31e2a7541ae
briaguya <70942617+briaguya-ai@users.noreply.github.com>
2025-02-12 08:30:39 -0500

    move vb rsk checks into conds

--- soh/soh/Enhancements/cosmetics/NoMasterSword.cpp
+++ soh/soh/Enhancements/cosmetics/NoMasterSword.cpp
@@ -66,28 +66,27 @@ void UpdateNoMSPatch() {
 // copied from z_player (right above Player_StartMode_TimeTravel)
 static Vec3f D_808546F4 = { -1.0f, 69.0f, 20.0f };
 
+#define MASTER_SWORD_SHUFFLED (OTRGlobals::Instance->gRandomizer->GetRandoSettingValue(RSK_SHUFFLE_MASTER_SWORD) != 0)
+
 void RegisterNoMasterSword() {
     // don't show link taking out master sword to put in pedestal when we don't have a master sword
-    COND_VB_SHOULD(VB_SHOW_MASTER_SWORD_TO_PLACE_IN_PEDESTAL, IS_RANDO, {
+    COND_VB_SHOULD(VB_SHOW_MASTER_SWORD_TO_PLACE_IN_PEDESTAL, IS_RANDO && MASTER_SWORD_SHUFFLED, {
         // if the vanilla condition is false respect it
         if (!*should) {
             return;
         }
 
-        if (Randomizer_GetSettingValue(RSK_SHUFFLE_MASTER_SWORD) &&
-            !CHECK_OWNED_EQUIP(EQUIP_TYPE_SWORD, EQUIP_INV_SWORD_MASTER)) {
-            
+        if (!CHECK_OWNED_EQUIP(EQUIP_TYPE_SWORD, EQUIP_INV_SWORD_MASTER)) {
             *should = false;
         }
     });
     
     // skip post pedestal animation when we don't have a master sword
-    COND_VB_SHOULD(VB_EXECUTE_PLAYER_STARTMODE_FUNC, IS_RANDO, {
+    COND_VB_SHOULD(VB_EXECUTE_PLAYER_STARTMODE_FUNC, IS_RANDO && MASTER_SWORD_SHUFFLED, {
         int32_t startMode = va_arg(args, int32_t);
         Player* player = GET_PLAYER(gPlayState);
 
-        if (startMode == PLAYER_START_MODE_TIME_TRAVEL && Randomizer_GetSettingValue(RSK_SHUFFLE_MASTER_SWORD) &&
-            !CHECK_OWNED_EQUIP(EQUIP_TYPE_SWORD, EQUIP_INV_SWORD_MASTER)) {
+        if (startMode == PLAYER_START_MODE_TIME_TRAVEL && !CHECK_OWNED_EQUIP(EQUIP_TYPE_SWORD, EQUIP_INV_SWORD_MASTER)) {
             // don't run the vanilla startMode func
             *should = false;
             
