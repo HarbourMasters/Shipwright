2527e5246133a286330ffde325825c5b68e7bf3b
Pepe20129 <72659707+Pepe20129@users.noreply.github.com>
2025-02-26 19:02:10 +0100

    Create `thieves_hideout.cpp` & move `gerudo_fortress.cpp`

---
+++ soh/soh/Enhancements/randomizer/location_access/dungeons/thieves_hideout.cpp
@@ -0,0 +1,8 @@
+#include "soh/Enhancements/randomizer/location_access.h"
+#include "soh/Enhancements/randomizer/entrance.h"
+
+using namespace Rando;
+
+void RegionTable_Init_ThievesHideout() {
+
+}
