8cfe44d7ca2faa4ccb7aca96486d17083312865b
Malkierian <malkierian@live.com>
2025-02-27 19:28:18 -0700

    Copy over advanced resolution partial and enable most options as a custom widget.

--- soh/soh/Enhancements/resolution-editor/ResolutionEditor.h
+++ soh/soh/Enhancements/resolution-editor/ResolutionEditor.h
@@ -1,16 +1,12 @@
-#pragma once
+#ifndef RESOLUTIONEDITOR_H
+#define RESOLUTIONEDITOR_H
+
 #include <libultraship/libultraship.h>
 
-namespace AdvancedResolutionSettings {
-class AdvancedResolutionSettingsWindow : public Ship::GuiWindow {
-  private:
-    bool IsDroppingFrames();
+namespace SohGui {
+bool IsDroppingFrames();
+void RegisterResolutionWidgets();
+void UpdateResolutionVars();
+} // namespace BenGui
 
-  public:
-    using GuiWindow::GuiWindow;
-
-    void InitElement() override;
-    void DrawElement() override;
-    void UpdateElement() override;
-};
-} // namespace AdvancedResolutionSettings
+#endif // RESOLUTIONEDITOR_H
--- soh/soh/SohGui/SohGui.cpp
+++ soh/soh/SohGui/SohGui.cpp
@@ -33,7 +33,6 @@
 
 #include "soh/Enhancements/game-interactor/GameInteractor.h"
 #include "soh/Enhancements/cosmetics/authenticGfxPatches.h"
-#include "soh/Enhancements/resolution-editor/ResolutionEditor.h"
 #include "soh/Enhancements/debugger/MessageViewer.h"
 #include "soh/Notification/Notification.h"
 #include "soh/Enhancements/TimeDisplay/TimeDisplay.h"
@@ -98,7 +97,6 @@ namespace SohGui {
     std::shared_ptr<TimeSplitWindow> mTimeSplitWindow;
     std::shared_ptr<PlandomizerWindow> mPlandomizerWindow;
     std::shared_ptr<RandomizerSettingsWindow> mRandomizerSettingsWindow;
-    std::shared_ptr<AdvancedResolutionSettings::AdvancedResolutionSettingsWindow> mAdvancedResolutionSettingsWindow;
     std::shared_ptr<SohModalWindow> mModalWindow;
     std::shared_ptr<Notification::Window> mNotificationWindow;
     std::shared_ptr<TimeDisplayWindow> mTimeDisplayWindow;
@@ -182,8 +180,6 @@ namespace SohGui {
         gui->AddGuiWindow(mTimeSplitWindow);
         mPlandomizerWindow = std::make_shared<PlandomizerWindow>(CVAR_WINDOW("PlandomizerEditor"), "Plandomizer Editor", ImVec2(850, 760));
         gui->AddGuiWindow(mPlandomizerWindow);
-        mAdvancedResolutionSettingsWindow = std::make_shared<AdvancedResolutionSettings::AdvancedResolutionSettingsWindow>(CVAR_WINDOW("AdvancedResolutionEditor"), "Advanced Resolution Settings", ImVec2(497, 599));
-        gui->AddGuiWindow(mAdvancedResolutionSettingsWindow);
         mModalWindow = std::make_shared<SohModalWindow>(CVAR_WINDOW("ModalWindow"), "Modal Window");
         gui->AddGuiWindow(mModalWindow);
         mModalWindow->Show();
@@ -200,7 +196,6 @@ namespace SohGui {
         
         mNotificationWindow = nullptr;
         mModalWindow = nullptr;
-        mAdvancedResolutionSettingsWindow = nullptr;
         mRandomizerSettingsWindow = nullptr;
         mItemTrackerWindow = nullptr;
         mItemTrackerSettingsWindow = nullptr;
