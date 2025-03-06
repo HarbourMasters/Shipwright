4434562da164db3a5094687b36d670e4ca7cb5c1
Malkierian <malkierian@live.com>
2025-03-05 19:07:25 -0700

    Unify cvar sectioning in time splits.

--- soh/soh/Enhancements/timesplits/TimeSplits.cpp
+++ soh/soh/Enhancements/timesplits/TimeSplits.cpp
@@ -1,5 +1,5 @@
 #include "TimeSplits.h"
-#include "soh/SohGui/UIWidgets.hpp"
+#include "soh/SohGui/UIWidgets2.hpp"
 #include "soh/Enhancements/gameplaystats.h"
 #include "soh/SaveManager.h"
 #include "soh/util.h"
@@ -784,13 +784,13 @@ void TimeSplitsDrawOptionsMenu() {
         color.g = windowColor.y * 255.0;
         color.b = windowColor.z * 255.0;
         color.a = windowColor.w * 255.0;
-        CVarSetColor("TimeSplits.WindowColor", color);
+        CVarSetColor(CVAR_ENHANCEMENT("TimeSplits.WindowColor"), color);
         Ship::Context::GetInstance()->GetWindow()->GetGui()->SaveConsoleVariablesNextFrame();
     }
     ImGui::SameLine();
     if (ImGui::Button("Reset")) {
         windowColor = { 0.0f, 0.0f, 0.0f, 1.0f };
-        CVarSetColor("TimeSplits.WindowColor", {0, 0, 0, 1});
+        CVarSetColor(CVAR_ENHANCEMENT("TimeSplits.WindowColor"), {0, 0, 0, 1});
         Ship::Context::GetInstance()->GetWindow()->GetGui()->SaveConsoleVariablesNextFrame();
     }
 
@@ -957,7 +957,7 @@ void TimeSplitWindow::DrawElement() {
     ImGui::SetWindowFontScale(timeSplitsWindowSize);
     if (!initialized) {
         Color_RGBA8 defaultColour = {0, 0, 0, 255};
-        Color_RGBA8 color = CVarGetColor("TimeSplits.WindowColor", defaultColour);
+        Color_RGBA8 color = CVarGetColor(CVAR_ENHANCEMENT("TimeSplits.WindowColor"), defaultColour);
         windowColor = {(float)color.r / 255.0f, (float)color.g / 255.0f, (float)color.b / 255.0f, (float)color.a / 255.0f};
         InitializeSplitDataFile();
         initialized = true;
