956ed7b5a697a515350fa12041974e853c027277
Christopher Leggett <chris@leggett.dev>
2025-03-10 19:01:40 -0400

    Give Randomizer Menu more screen real-estate

--- soh/soh/Enhancements/randomizer/option.cpp
+++ soh/soh/Enhancements/randomizer/option.cpp
@@ -231,6 +231,9 @@ bool Option::RenderCombobox() {
         Ship::Context::GetInstance()->GetWindow()->GetGui()->SaveConsoleVariablesNextFrame();
     }
     UIWidgets2::ComboboxOptions widgetOptions = UIWidgets2::ComboboxOptions().Color(THEME_COLOR).Tooltip(description.c_str());
+    if (this->GetKey() == RSK_LOGIC_RULES) {
+        widgetOptions = widgetOptions.LabelPosition(UIWidgets2::LabelPosition::Near).ComponentAlignment(UIWidgets2::ComponentAlignment::Right);
+    }
     widgetOptions.disabled = disabled;
     if(UIWidgets2::Combobox(name.c_str(), &selected, options, widgetOptions)) {
         CVarSetInteger(cvarName.c_str(), static_cast<int>(selected));
