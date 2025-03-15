bad9714d2745764441fc2838d8478b9fe754317d
louist103 <35883445+louist103@users.noreply.github.com>
2025-03-15 18:54:26 -0400

    Bump LUS and update ship

--- soh/soh/resource/importer/ArrayFactory.cpp
+++ soh/soh/resource/importer/ArrayFactory.cpp
@@ -4,12 +4,12 @@
 #include "graphic/Fast3D/lus_gbi.h"
 
 namespace SOH {
-std::shared_ptr<Ship::IResource> ResourceFactoryBinaryArrayV0::ReadResource(std::shared_ptr<Ship::File> file) {
-    if (!FileHasValidFormatAndReader(file)) {
+std::shared_ptr<Ship::IResource> ResourceFactoryBinaryArrayV0::ReadResource(std::shared_ptr<Ship::File> file, std::shared_ptr<Ship::ResourceInitData> initData) {
+    if (!FileHasValidFormatAndReader(file, initData)) {
         return nullptr;
     }
 
-    auto array = std::make_shared<Array>(file->InitData);
+    auto array = std::make_shared<Array>(initData);
     auto reader = std::get<std::shared_ptr<Ship::BinaryReader>>(file->Reader);
 
     array->ArrayType = (ArrayResourceType)reader->ReadUInt32();
--- soh/soh/resource/importer/BackgroundFactory.h
+++ soh/soh/resource/importer/BackgroundFactory.h
@@ -6,6 +6,6 @@
 namespace SOH {
 class ResourceFactoryBinaryBackgroundV0 : public Ship::ResourceFactoryBinary {
   public:
-    std::shared_ptr<Ship::IResource> ReadResource(std::shared_ptr<Ship::File> file) override;
+    std::shared_ptr<Ship::IResource> ReadResource(std::shared_ptr<Ship::File> file, std::shared_ptr<Ship::ResourceInitData> initData) override;
 };
 } // namespace SOH
