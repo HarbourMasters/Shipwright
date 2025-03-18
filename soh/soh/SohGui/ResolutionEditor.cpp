1190d09820aa54e9ce5a165b57237d8bdd1ec1f4
Malkierian <malkierian@gmail.com>
2025-03-17 17:20:10 -0700

    Change Resolution Editor's use of CVarGet to `GetInterpolationFPS()` in OTRGlobals.

--- soh/soh/SohGui/ResolutionEditor.cpp
+++ soh/soh/SohGui/ResolutionEditor.cpp
@@ -517,7 +517,7 @@ void UpdateResolutionVars() {
 bool IsDroppingFrames() {
     // a rather imprecise way of checking for frame drops.
     // but it's mostly there to inform the player of large drops.
-    const short targetFPS = CVarGetInteger("gInterpolationFPS", 20);
+    const short targetFPS = OTRGlobals::Instance->GetInterpolationFPS();
     const float threshold = targetFPS / 20.0f + 4.1f;
     return ImGui::GetIO().Framerate < targetFPS - threshold;
 }
