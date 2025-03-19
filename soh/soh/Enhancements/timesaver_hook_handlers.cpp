18d08c73ee2684e7548e34029cfe88f65f2bc450
JordanLongstaff <jrdnlngstff@gmail.com>
2025-03-19 11:22:20 -0400

    Change to forced NPC dialogue

--- soh/soh/Enhancements/timesaver_hook_handlers.cpp
+++ soh/soh/Enhancements/timesaver_hook_handlers.cpp
@@ -372,7 +372,7 @@ void TimeSaverOnVanillaBehaviorHandler(GIVanillaBehavior id, bool* should, va_li
             break;
         }
         case VB_SKIP_GERUDO_MEMBERSHIP_TALK: {
-            if (CVarGetInteger(CVAR_ENHANCEMENT("TimeSavers.SkipMiscInteractions"), IS_RANDO)) {
+            if (ForcedDialogIsDisabled(FORCED_DIALOG_SKIP_NPC)) {
                 *should = true;
             }
             break;
