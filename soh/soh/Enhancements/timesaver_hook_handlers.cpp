b0b24faf97be42929145b0c932c31501f6251ecf
Adam Bird <archez39@me.com>
2025-03-20 11:14:34 -0400

    prevent some timesavers applying in boss rush

--- soh/soh/Enhancements/timesaver_hook_handlers.cpp
+++ soh/soh/Enhancements/timesaver_hook_handlers.cpp
@@ -1060,6 +1060,11 @@ void TimeSaverOnSceneInitHandler(int16_t sceneNum) {
 static GetItemEntry vanillaQueuedItemEntry = GET_ITEM_NONE;
 
 void TimeSaverOnFlagSetHandler(int16_t flagType, int16_t flag) {
+    // Do nothing when in a boss rush
+    if (IS_BOSS_RUSH) {
+        return;
+    }
+
     if (CVarGetInteger(CVAR_ENHANCEMENT("TimeSavers.SkipCutscene.Story"), IS_RANDO)) {
         switch (flagType) {
             case FLAG_EVENT_CHECK_INF:
