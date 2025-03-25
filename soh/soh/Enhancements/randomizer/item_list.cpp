58269584048587a44b48ea487b0745e42837e685
Pepper0ni <93387759+Pepper0ni@users.noreply.github.com>
2025-03-25 19:40:33 +0000

    more poking

--- soh/soh/Enhancements/randomizer/item_list.cpp
+++ soh/soh/Enhancements/randomizer/item_list.cpp
@@ -404,7 +404,7 @@ void Rando::StaticData::InitItemTable() {
 
     // Init itemNameToEnum
     for (auto& item : itemTable) {
-        std::vector<std::string> strings = item.GetName().GetAllMessages();
+        std::vector<std::string> strings = item.GetName().GetAllMessages(MF_RAW);
         for (std::string string: strings){
             if (itemNameToEnum.contains(string) && string != ""){
                 if (itemNameToEnum[string] != item.GetRandomizerGet()){
