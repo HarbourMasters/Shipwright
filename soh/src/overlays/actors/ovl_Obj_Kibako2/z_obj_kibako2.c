7e5f35b3d697a3356c2f25d42d92bb64171f1388
Eric Hoey <121978037+A-Green-Spoon@users.noreply.github.com>
2025-03-26 15:00:12 -0400

    formatting sheesh

--- soh/src/overlays/actors/ovl_Obj_Kibako2/z_obj_kibako2.c
+++ soh/src/overlays/actors/ovl_Obj_Kibako2/z_obj_kibako2.c
@@ -179,7 +179,7 @@ void ObjKibako2_Update(Actor* thisx, PlayState* play) {
 void ObjKibako2_Draw(Actor* thisx, PlayState* play) {
     if (!GameInteractor_Should(VB_CRATE_SETUP_DRAW, true, thisx)) {
         return;
-        }
+    }
 
     Gfx_DrawDListOpa(play, gLargeCrateDL);
 }
