ad232f7aa6bf283b2cb859b6d0e026c271bb3a27
Pepper0ni <93387759+Pepper0ni@users.noreply.github.com>
2025-03-31 18:33:24 +0100

    change to rand inf

--- soh/src/overlays/misc/ovl_kaleido_scope/z_kaleido_item.c
+++ soh/src/overlays/misc/ovl_kaleido_scope/z_kaleido_item.c
@@ -318,7 +318,7 @@ void KaleidoScope_HandleItemCycleExtras(PlayState* play, u8 slot, bool canCycle,
 bool CanMaskSelect() {
     if (IS_RANDO) {
         return CVarGetInteger(CVAR_ENHANCEMENT("MaskSelect"), 0) 
-               && Flags_GetEventChkInf(EVENTCHKINF_OBTAINED_ZELDAS_LETTER);/* || Randomizer_GetSettingValue(RSK_SHUFFLE_CHILD_TRADE) */
+               && Flags_GetRandomizerInf(RAND_INF_ZELDAS_LETTER);/* || Randomizer_GetSettingValue(RSK_SHUFFLE_CHILD_TRADE) */
     }
 
     // only allow mask select when:
