c3b0a1c1d9793eb776aa552daa49540b42176c8e
Pepper0ni <93387759+Pepper0ni@users.noreply.github.com>
2025-04-09 17:47:53 +0100

    IC ledge to fountain child logic fixed

--- soh/soh/Enhancements/randomizer/location_access/overworld/zoras_fountain.cpp
+++ soh/soh/Enhancements/randomizer/location_access/overworld/zoras_fountain.cpp
@@ -51,8 +51,14 @@ void RegionTable_Init_ZorasFountain() {
         //Exits
         Entrance(RR_ZD_BEHIND_KING_ZORA,       []{return true;}),
         Entrance(RR_JABU_JABUS_BELLY_ENTRYWAY, []{return (logic->IsChild && logic->CanUse(RG_BOTTLE_WITH_FISH));}),
-        Entrance(RR_ICE_CAVERN_ENTRYWAY,       []{return logic->IsAdult;}),
+        Entrance(RR_ZF_LEDGE,                  []{return logic->IsAdult;}),
         Entrance(RR_ZF_GREAT_FAIRY_FOUNTAIN,   []{return logic->HasExplosives() || (ctx->GetTrickOption(RT_ZF_GREAT_FAIRY_WITHOUT_EXPLOSIVES) && logic->CanUse(RG_MEGATON_HAMMER) && logic->CanUse(RG_SILVER_GAUNTLETS));}),
+    });
+
+    areaTable[RR_ZF_LEDGE] = Region("ZF Ledge", "Zoras Fountain", {RA_ZORAS_FOUNTAIN}, NO_DAY_NIGHT_CYCLE, {}, {}, {
+        //Exits
+        Entrance(RR_ZORAS_FOUNTAIN,      []{return logic->IsAdult || logic->HasItem(RG_BRONZE_SCALE);}),
+        Entrance(RR_ICE_CAVERN_ENTRYWAY, []{return true;}),
     });
 
     areaTable[RR_ZF_GREAT_FAIRY_FOUNTAIN] = Region("ZF Great Fairy Fountain", "ZF Great Fairy Fountain", {}, NO_DAY_NIGHT_CYCLE, {}, {
