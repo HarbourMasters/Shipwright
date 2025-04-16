79550ce60cd704a831322c51a8729c3948dede43
Pepper0ni <93387759+Pepper0ni@users.noreply.github.com>
2025-04-16 16:12:26 +0100

    oops

--- soh/soh/Enhancements/custom-message/CustomMessageManager.cpp
+++ soh/soh/Enhancements/custom-message/CustomMessageManager.cpp
@@ -155,8 +155,8 @@ const std::string CustomMessage::GetFrench(MessageFormat format) const {
 }
 
 const std::string CustomMessage::GetForCurrentLanguage(MessageFormat format) const {
-    return GetForLanguage(((Language)gSaveContext.language == LANGUAGE_JPN) ? LANGUAGE_ENG : (Language)gSaveContext.language,
-                          format);
+    return GetForLanguage(
+        ((Language)gSaveContext.language == LANGUAGE_JPN) ? LANGUAGE_ENG : (Language)gSaveContext.language, format);
 }
 
 const std::string CustomMessage::GetForLanguage(uint8_t language, MessageFormat format) const {
