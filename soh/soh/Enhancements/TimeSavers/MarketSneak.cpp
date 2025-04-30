e585b76d51175f05bc667c4f95ea8b322fb95bd5
Christopher Leggett <chris@leggett.dev>
2025-04-29 22:00:41 -0400

    Fix the Yes/No spacing on a couple of messages.

--- soh/soh/Enhancements/TimeSavers/MarketSneak.cpp
+++ soh/soh/Enhancements/TimeSavers/MarketSneak.cpp
@@ -7,9 +7,9 @@ extern "C" {
 // RANDOTODO: Port the rest of the behavior for this enhancement here.
 
 void BuildNightGuardMessage(uint16_t* textId, bool* loadFromMessageTable) {
-    CustomMessage msg = CustomMessage("You look bored. Wanna go out for a&walk?\x1B&%gYes&No%w",
-                                      "Du siehst gelangweilt aus.&Willst Du einen Spaziergang machen?\x1B&%gJa&Nein%w",
-                                      "Tu as l'air de t'ennuyer. Tu veux&aller faire un tour?\x1B&%gOui&Non%w");
+    CustomMessage msg = CustomMessage("You look bored. Wanna go out for a walk?\x1B%gYes&No%w",
+                                      "Du siehst gelangweilt aus. Willst Du einen Spaziergang machen?\x1B%gJa&Nein%w",
+                                      "Tu as l'air de t'ennuyer. Tu veux aller faire un tour?\x1B%gOui&Non%w");
     msg.AutoFormat();
     msg.LoadIntoFont();
     *loadFromMessageTable = false;
--- soh/soh/Enhancements/TimeSavers/QuitFishingAtDoor.cpp
+++ soh/soh/Enhancements/TimeSavers/QuitFishingAtDoor.cpp
@@ -10,10 +10,10 @@ void BuildQuitFishingMessage(uint16_t* textId, bool* loadFromMessageTable) {
     // TODO: See about loading the vanilla message and manipulating that instead of
     // a brand new one. Might not be worth it.
     CustomMessage msg = CustomMessage(
-        "Hey! Hey!&You can't take the rod out of here!&I'm serious!^Do you want to quit?&\x1B&%gYes&No%w",
+        "Hey! Hey!&You can't take the rod out of here!&I'm serious!^Do you want to quit?\x1B%gYes&No%w",
         "Hey! Hey!&Du kannst die Angel doch nicht&einfach mitnehmen!&Ganz im Ernst!^Möchtest Du "
-        "aufhören?&\x1B&%gJa&Nein%w",
-        "Holà! Holà!&Les cannes ne sortent pas d'ici!&Je suis sérieux!^Voulez-vous arrêter?&\x1B&%gOui&Non%w");
+        "aufhören?\x1B%gJa&Nein%w",
+        "Holà! Holà!&Les cannes ne sortent pas d'ici!&Je suis sérieux!^Voulez-vous arrêter?\x1B%gOui&Non%w");
     msg.AutoFormat();
     msg.LoadIntoFont();
     *loadFromMessageTable = false;
