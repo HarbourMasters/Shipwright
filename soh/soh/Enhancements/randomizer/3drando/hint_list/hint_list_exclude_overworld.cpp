95015db6c3d71ad660c7d0e14db76b12202b8f3f
JordanLongstaff <jrdnlngstff@gmail.com>
2025-05-02 16:18:51 -0400

    Finish hint translations for fish by waterfall

--- soh/soh/Enhancements/randomizer/3drando/hint_list/hint_list_exclude_overworld.cpp
+++ soh/soh/Enhancements/randomizer/3drando/hint_list/hint_list_exclude_overworld.cpp
@@ -313,11 +313,11 @@ void StaticData::HintTable_Init_Exclude_Overworld() {
                                                  /*german*/ "Man erzählt sich, daß ein #Tauchexperiment# mit #[[1]]# belohnt würde.",
                                                  /*french*/ "Selon moi, l'#expérience de plongée# donne #[[1]]#.", {QM_RED, QM_GREEN}));
                                               // /*spanish*/ Según dicen, #bucear para un experimento# se premia con #[[1]]#.
-    // RANDOTODO: needs translation
+
     hintTextTable[RHT_ZD_FISH] = HintText(CustomMessage("They say that a #fish by a waterfall# hoards #[[1]]#.",
                                              /*german*/ "Man erzählt sich, daß ein #Fisch nahe eines Wasserfalls# #[[1]]# horte.",
-                                             /*french*/ "Selon moi, #[[1]]#.", {QM_RED, QM_GREEN}));
-                                          // /*spanish*/ Según dicen, #[[1]]#.
+                                             /*french*/ "Selon moi, un #poisson près d'une cascade# amasse #[[1]]#.", {QM_RED, QM_GREEN}));
+                                          // /*spanish*/ Según dicen, un #pez junto a una cascada# acumula #[[1]]#.
 
  
     hintTextTable[RHT_GC_ROLLING_GORON_AS_ADULT] = HintText(CustomMessage("They say that #reassuring a young Goron# is rewarded with #[[1]]#.",
