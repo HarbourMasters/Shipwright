705b32e34737172c87b47ca11616d745d74e4e51
Malkierian <malkierian@gmail.com>
2025-05-03 19:27:48 -0700

    Add Random_Float and use it in GameInteractor_RawAction.cpp. Make non-specified seed init a separate function.

--- soh/soh/Enhancements/randomizer/3drando/random.cpp
+++ soh/soh/Enhancements/randomizer/3drando/random.cpp
@@ -14,8 +14,7 @@ void Random_Init(uint32_t seed) {
     generator = boost::random::mt19937{ seed };
 }
 
-// Returns a random integer in range [min, max-1]
-uint32_t Random(int min, int max) {
+void Random_InitSeed() {
     if (!init) {
         // No seed given, get a random number from device to seed
 #if !defined(__SWITCH__) && !defined(__WIIU__)
@@ -25,7 +24,19 @@ uint32_t Random(int min, int max) {
 #endif
         Random_Init(seed);
     }
+}
+
+// Returns a random unsigned integer in range [min, max-1]
+uint32_t Random(uint32_t min, uint32_t max) {
+    Random_InitSeed();
     boost::random::uniform_int_distribution<uint32_t> distribution(min, max - 1);
+    return distribution(generator);
+}
+
+// Returns a random float in range [min, max-1]
+float Random_Float(float min, float max) {
+    Random_InitSeed();
+    boost::random::uniform_real_distribution<float> distribution(min, max - 1);
     return distribution(generator);
 }
 
