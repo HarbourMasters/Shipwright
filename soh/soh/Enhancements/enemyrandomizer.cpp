68749565b273ea4e2bc657c5ed4b22d439af6da5
Pepe20129 <72659707+Pepe20129@users.noreply.github.com>
2025-05-18 18:58:15 +0200

    Include spear moblins and exclude flare dancer in clear rooms

--- soh/soh/Enhancements/enemyrandomizer.cpp
+++ soh/soh/Enhancements/enemyrandomizer.cpp
@@ -443,19 +443,18 @@ bool IsEnemyFoundToRandomize(int16_t sceneNum, int8_t roomNum, int16_t actorId, 
 }
 
 bool IsEnemyAllowedToSpawn(int16_t sceneNum, int8_t roomNum, EnemyEntry enemy) {
-
     uint32_t isMQ = ResourceMgr_IsSceneMasterQuest(sceneNum);
 
     // Freezard - Child Link can only kill this with jump slash Deku Sticks or other equipment like bombs.
     // Beamos - Needs bombs.
     // Shell Blade & Spike - Child Link can't kill these with sword or Deku Stick.
-    // Arwing & Dark Link - Both go out of bounds way too easily, softlocking the player.
+    // Flare dancer, Arwing & Dark Link - Both go out of bounds way too easily, softlocking the player.
     // Wallmaster - Not easily visible, often makes players think they're softlocked and that there's no enemies left.
     // Club Moblin - Many issues with them falling or placing out of bounds. Maybe fixable in the future?
     bool enemiesToExcludeClearRooms = enemy.id == ACTOR_EN_FZ || enemy.id == ACTOR_EN_VM || enemy.id == ACTOR_EN_SB ||
                                       enemy.id == ACTOR_EN_NY || enemy.id == ACTOR_EN_CLEAR_TAG ||
                                       enemy.id == ACTOR_EN_WALLMAS || enemy.id == ACTOR_EN_TORCH2 ||
-                                      enemy.id == ACTOR_EN_MB;
+                                      (enemy.id == ACTOR_EN_MB && enemy.params == 0) || enemy.id == ACTOR_EN_FD;
 
     // Bari - Spawns 3 more enemies, potentially extremely difficult in timed rooms.
     bool enemiesToExcludeTimedRooms = enemiesToExcludeClearRooms || enemy.id == ACTOR_EN_VALI;
