57827579ee0faebfbf91d4bca4528d1eae22982d
JordanLongstaff <jrdnlngstff@gmail.com>
2025-06-12 01:53:02 -0400

    Fix vanilla enhancement disabler

--- soh/soh/SohGui/SohMenuEnhancements.cpp
+++ soh/soh/SohGui/SohMenuEnhancements.cpp
@@ -265,8 +265,10 @@ void SohMenu::AddMenuEnhancements() {
     AddWidget(path, "Skip Feeding Jabu-Jabu", WIDGET_CVAR_CHECKBOX)
         .CVar(CVAR_ENHANCEMENT("TimeSavers.SkipJabuJabuFish"))
         .PreFunc([](WidgetInfo& info) {
-            info.options->disabled = IS_RANDO && GameInteractor::IsSaveLoaded(true);
-            info.options->disabledTooltip = "This setting is disabled because a randomizer savefile is loaded.";
+            info.options->disabled =
+                IS_RANDO && OTRGlobals::Instance->gRandoContext->GetOption(RSK_JABU_OPEN).Is(RO_JABU_OPEN);
+            info.options->disabledTooltip =
+                "This setting is disabled because a randomizer savefile with \"Jabu-Jaby: Open\" is loaded.";
         })
         .Options(CheckboxOptions().Tooltip("Allow Link to enter Jabu-Jabu without feeding him a fish."));
 
