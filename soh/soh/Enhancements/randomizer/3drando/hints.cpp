7555e7bac3ae7db7b3a5084ffb3de7388ad22155
Pepper0ni <93387759+Pepper0ni@users.noreply.github.com>
2025-07-10 22:27:47 +0100

    I for one welcome our new clang overlords...

--- soh/soh/Enhancements/randomizer/3drando/hints.cpp
+++ soh/soh/Enhancements/randomizer/3drando/hints.cpp
@@ -648,7 +648,7 @@ void CreateStoneHints() {
     if (ctx->GetOption(RSK_SKIP_CHILD_ZELDA)) {
         ctx->GetItemLocation(RC_SONG_FROM_IMPA)->SetHintAccesible();
     }
-    if (ctx->GetOption(RSK_SELECTED_STARTING_AGE).Is(RO_AGE_ADULT)){
+    if (ctx->GetOption(RSK_SELECTED_STARTING_AGE).Is(RO_AGE_ADULT)) {
         ctx->GetItemLocation(RC_TOT_MASTER_SWORD)->SetHintAccesible();
     }
 
