742d7dfdcc929beb9d62bb451822a24499010bf4
Malkierian <malkierian@gmail.com>
2025-07-23 13:57:57 -0700

    clang

--- soh/soh/SaveManager.cpp
+++ soh/soh/SaveManager.cpp
@@ -1121,9 +1121,10 @@ void SaveManager::LoadFile(int fileNum) {
         switch (saveBlock["version"].get<int>()) {
             case 1:
                 for (auto& block : saveBlock["sections"].items()) {
-                    bool oldVanilla = block.value()["data"].empty() || block.value()["data"].contains("aat0") ||
-                                      block.value()["data"]["entrances"].empty() || 
-                                      SohUtils::IsStringEmpty(saveBlock["sections"]["sohStats"]["data"]["buildVersion"]);
+                    bool oldVanilla =
+                        block.value()["data"].empty() || block.value()["data"].contains("aat0") ||
+                        block.value()["data"]["entrances"].empty() ||
+                        SohUtils::IsStringEmpty(saveBlock["sections"]["sohStats"]["data"]["buildVersion"]);
                     std::string sectionName = block.key();
                     if (sectionName == "randomizer") {
                         bool hasStats = saveBlock["sections"].contains("sohStats");
