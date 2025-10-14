78a1072b4e31e651bcf0fce2b13c9e5bd79fbb72
nclok1405 <155463060+nclok1405@users.noreply.github.com>
2025-10-14 15:40:10 +0900

    Allow Enemy Randomizer in Debug Save

--- soh/src/code/z_actor.c
+++ soh/src/code/z_actor.c
@@ -3321,8 +3321,11 @@ int gMapLoading = 0;
 Actor* Actor_Spawn(ActorContext* actorCtx, PlayState* play, s16 actorId, f32 posX, f32 posY, f32 posZ, s16 rotX,
                    s16 rotY, s16 rotZ, s16 params, s16 canRandomize) {
 
-    uint8_t tryRandomizeEnemy = CVarGetInteger(CVAR_ENHANCEMENT("RandomizedEnemies"), 0) && gSaveContext.fileNum >= 0 &&
-                                gSaveContext.fileNum <= 2 && canRandomize;
+    uint8_t tryRandomizeEnemy =
+        CVarGetInteger(CVAR_ENHANCEMENT("RandomizedEnemies"), 0) &&
+        ((gSaveContext.fileNum >= 0 && gSaveContext.fileNum <= 2) ||
+         (gSaveContext.fileNum == 0xFF && CVarGetInteger(CVAR_DEVELOPER_TOOLS("DebugEnabled"), 0))) &&
+        canRandomize;
 
     if (tryRandomizeEnemy) {
         if (!GetRandomizedEnemy(play, &actorId, &posX, &posY, &posZ, &rotX, &rotY, &rotZ, &params)) {
