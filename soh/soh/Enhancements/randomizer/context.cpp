109a7f0e0e5f62f8090d2212c432c07121d4ebdf
Christopher Leggett <chris@leggett.dev>
2025-11-07 15:56:09 -0500

    Convert 100 GS check to new hook

--- soh/soh/Enhancements/randomizer/context.cpp
+++ soh/soh/Enhancements/randomizer/context.cpp
@@ -201,8 +201,6 @@ void Context::GenerateLocationPool() {
         // TODO: Exclude checks for some of the older shuffles from the pool too i.e. Frog Songs, Scrubs, etc.)
         if ((location.GetRandomizerCheck() == RC_TOT_MASTER_SWORD &&
              mOptions[RSK_SHUFFLE_MASTER_SWORD].Is(RO_GENERIC_OFF)) ||
-            (location.GetRandomizerCheck() == RC_KAK_100_GOLD_SKULLTULA_REWARD &&
-             mOptions[RSK_SHUFFLE_100_GS_REWARD].Is(RO_GENERIC_OFF)) ||
             (location.GetRCType() == RCTYPE_SONG_LOCATION && mOptions[RSK_SHUFFLE_SONGS].Is(RO_SONG_SHUFFLE_OFF))) {
             continue;
         }
