38bab9987bf719ac70152e71628b26d928fec33e
Christopher Leggett <chris@leggett.dev>
2025-11-07 13:35:49 -0500

    Converts pot shuffle to new hook

--- soh/soh/Enhancements/randomizer/context.cpp
+++ soh/soh/Enhancements/randomizer/context.cpp
@@ -214,29 +214,9 @@ void Context::GenerateLocationPool() {
             (location.GetRandomizerCheck() == RC_LH_HYRULE_LOACH &&
              mOptions[RSK_FISHSANITY].IsNot(RO_FISHSANITY_HYRULE_LOACH)) ||
             (location.GetRCType() == RCTYPE_FISH && !mFishsanity->GetFishLocationIncluded(&location)) ||
-            (location.GetRCType() == RCTYPE_POT && mOptions[RSK_SHUFFLE_POTS].Is(RO_SHUFFLE_POTS_OFF)) ||
             (location.GetRCType() == RCTYPE_NLTREE &&
              (!mOptions[RSK_SHUFFLE_TREES] || mOptions[RSK_LOGIC_RULES].IsNot(RO_LOGIC_NO_LOGIC)))) {
             continue;
-        }
-        if (location.IsOverworld()) {
-            // Skip stuff that is shuffled to dungeon only, i.e. tokens, pots, etc., or other checks that
-            // should not have a shuffled item.
-            if ((location.GetRCType() == RCTYPE_POT && mOptions[RSK_SHUFFLE_POTS].Is(RO_SHUFFLE_POTS_DUNGEONS))) {
-                continue;
-            }
-            // If we've gotten past all the conditions where an overworld location should not be
-            // shuffled, add it to the pool.
-            AddLocation(location.GetRandomizerCheck(), &overworldLocations);
-            AddLocation(location.GetRandomizerCheck());
-        } else { // is a dungeon check
-            auto* dungeon = GetDungeon(location.GetArea() - RCAREA_DEKU_TREE);
-            if ((location.GetRCType() == RCTYPE_POT && mOptions[RSK_SHUFFLE_POTS].Is(RO_SHUFFLE_POTS_OVERWORLD))) {
-                continue;
-            }
-            // also add to that dungeon's location list.
-            AddLocation(location.GetRandomizerCheck(), &dungeon->locations);
-            AddLocation(location.GetRandomizerCheck());
         }
     }
 }
