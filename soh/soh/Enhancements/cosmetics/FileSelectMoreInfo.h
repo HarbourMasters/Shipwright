a9b56d9e285cf37697df65d8ebb9a8ef51d3f4d6
Pepe20129 <72659707+Pepe20129@users.noreply.github.com>
2025-11-10 23:54:40 +0100

    Move to separate file

---
+++ soh/soh/Enhancements/cosmetics/FileSelectMoreInfo.h
@@ -0,0 +1,11 @@
+#include "libultraship/libultra/types.h"
+
+struct FileChooseContext;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+void DrawMoreInfo(struct FileChooseContext* thisx, s16 fileIndex, u8 alpha);
+#ifdef __cplusplus
+}
+#endif
