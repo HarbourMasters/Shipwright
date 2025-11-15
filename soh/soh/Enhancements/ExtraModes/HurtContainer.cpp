ac54b7b6768374c6dc458977a10c20293df3fa51
JordanLongstaff <jrdnlngstff@gmail.com>
2025-11-15 08:03:11 -0500

    Make mod file self-contained

--- soh/soh/Enhancements/ExtraModes/HurtContainer.cpp
+++ soh/soh/Enhancements/ExtraModes/HurtContainer.cpp
@@ -1,5 +1,4 @@
 #include "soh/Enhancements/game-interactor/GameInteractor_Hooks.h"
-#include "soh/Enhancements/mods.h"
 #include "soh/ShipInit.hpp"
 
 extern "C" {
@@ -13,7 +12,7 @@ static constexpr int32_t CVAR_HURT_CONTAINER_DEFAULT = 0;
 
 static bool hurtEnabled = false;
 
-void UpdateHurtContainerModeState() {
+static void UpdateHurtContainerModeState() {
     hurtEnabled = CVAR_HURT_CONTAINER_VALUE;
     uint16_t heartPieceContainers = gSaveContext.ship.stats.heartPieces / 4;
     uint16_t heartContainers = gSaveContext.ship.stats.heartContainers;
@@ -27,6 +26,10 @@ void UpdateHurtContainerModeState() {
 }
 
 static void RegisterHurtContainer() {
+    if (GameInteractor::IsSaveLoaded(false)) {
+        UpdateHurtContainerModeState();
+    }
+
     COND_HOOK(OnLoadGame, hurtEnabled != CVAR_HURT_CONTAINER_VALUE, [](int32_t) { UpdateHurtContainerModeState(); });
 
     COND_VB_SHOULD(VB_HEARTS_INCREASE_WITH_CONTAINERS, CVAR_HURT_CONTAINER_VALUE, {
