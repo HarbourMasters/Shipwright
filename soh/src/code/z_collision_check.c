76e45ee3629aca4df16f80be5bcb36f6bd2cb539
nclok1405 <155463060+nclok1405@users.noreply.github.com>
2025-12-05 14:50:33 +0900

    Mac-exclusive fix (use of undeclared identifier 'GameInteractor_Should')

--- soh/src/code/z_collision_check.c
+++ soh/src/code/z_collision_check.c
@@ -2,6 +2,7 @@
 #include "vt.h"
 #include "overlays/effects/ovl_Effect_Ss_HitMark/z_eff_ss_hitmark.h"
 #include "soh/Enhancements/game-interactor/GameInteractor.h"
+#include "soh/Enhancements/game-interactor/GameInteractor_Hooks.h"
 #include <assert.h>
 
 typedef s32 (*ColChkResetFunc)(PlayState*, Collider*);
