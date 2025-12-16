61d9bad155c4de46f32724e54a15b48d6ea84a84
Reppan <72985260+Jepvid@users.noreply.github.com>
2025-12-16 12:30:01 +0100

    Add include for custom equipment header

--- soh/soh/Enhancements/customequipment.cpp
+++ soh/soh/Enhancements/customequipment.cpp
@@ -3,6 +3,7 @@
 #include "objects/object_link_child/object_link_child.h"
 #include "objects/object_custom_equip/object_custom_equip.h"
 #include "soh/Enhancements/game-interactor/GameInteractor.h"
+#include "soh/Enhancements/customequipment.h"
 #include "soh/ResourceManagerHelpers.h"
 #include "soh_assets.h"
 #include "kaleido.h"
