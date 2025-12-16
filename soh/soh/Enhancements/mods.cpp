6e784d0ab771eb38fe9f0dbddf49ef204a8d7377
Reppan <72985260+Jepvid@users.noreply.github.com>
2025-12-16 12:29:45 +0100

    Add custom equipment header to mods.cpp

--- soh/soh/Enhancements/mods.cpp
+++ soh/soh/Enhancements/mods.cpp
@@ -5,6 +5,12 @@
 #include "soh/Enhancements/boss-rush/BossRush.h"
 #include "soh/Enhancements/enhancementTypes.h"
 #include <soh/Enhancements/item-tables/ItemTableManager.h>
+<<<<<<< HEAD
+=======
+#include "soh/Enhancements/timesaver_hook_handlers.h"
+#include "soh/Enhancements/randomizer/hook_handlers.h"
+#include "soh/Enhancements/customequipment.h"
+>>>>>>> a4fa6fbd0 (Add custom equipment header to mods.cpp)
 
 #include "src/overlays/actors/ovl_En_Bb/z_en_bb.h"
 #include "src/overlays/actors/ovl_En_Dekubaba/z_en_dekubaba.h"
