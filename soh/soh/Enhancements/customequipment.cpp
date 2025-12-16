8bef7b6411e856dd3add53d2d8cbc44b210302f6
Reppan <72985260+Jepvid@users.noreply.github.com>
2025-12-16 15:12:32 +0100

    Fix registration of OnAssetAltChange hook

--- soh/soh/Enhancements/customequipment.cpp
+++ soh/soh/Enhancements/customequipment.cpp
@@ -297,7 +297,7 @@ void RegisterPatchCustomEquipmentDlistsHandler() {
         UpdatePatchCustomEquipmentDlists(); 
     });
 
-   GameInteractor::Instance->RegisterGameHook<GameInteractor::OnAssetAltChange>([]() {
+    GameInteractor::Instance->RegisterGameHook<GameInteractor::OnAssetAltChange>([]() {
         UpdatePatchCustomEquipmentDlists();
     });
 
