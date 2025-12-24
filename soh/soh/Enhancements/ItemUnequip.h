500879ad1ade17467e253411680163d488e1e18b
OtherBlue <93625085+OtherBlue@users.noreply.github.com>
2025-12-24 11:22:08 -0300

    Delete unnecessary .h file
    mb I forgot to delete it

--- soh/soh/Enhancements/ItemUnequip.h
+++
@@ -1,7 +0,0 @@
-#ifndef ITEM_UNEQUIP_H
-#define ITEM_UNEQUIP_H
-
-// No header needed - this enhancement uses the VB system
-// See VB_EQUIP_ITEM_TO_C_BUTTON in GIVanillaBehavior.h
-
-#endif // ITEM_UNEQUIP_H
