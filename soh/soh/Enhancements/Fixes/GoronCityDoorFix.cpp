fb2c3e25df30ebc75b5f294f2ff283574ef87452
Demur Rumed <junkmute@hotmail.com>
2026-01-10 14:52:42 +0000

    not in rando

--- soh/soh/Enhancements/Fixes/GoronCityDoorFix.cpp
+++ soh/soh/Enhancements/Fixes/GoronCityDoorFix.cpp
@@ -4,8 +4,8 @@
 extern "C" {
 #include "functions.h"
 #include "macros.h"
+#include "variables.h"
 #include "z64save.h"
-#include "src/overlays/actors/ovl_Bg_Spot18_Shutter/z_bg_spot18_shutter.h"
 }
 
 static constexpr int32_t CVAR_GC_DOOR_FIX_DEFAULT = 0;
@@ -15,8 +15,6 @@ static constexpr int32_t CVAR_GC_DOOR_FIX_DEFAULT = 0;
 static void OnInitGCDoor(void* refActor) {
     // In the off chance that Fire Temple is completed before stopping Goron Link and getting
     // Goron Tunic / opening the city doors, open them the next time Link goes to Goron City.
-    // RefActor is needed since shutter checks inf flag during init.
-    BgSpot18Shutter* shutter = reinterpret_cast<BgSpot18Shutter*>(refActor);
     if (GameInteractor_Should(VB_GORONS_CONSIDER_FIRE_TEMPLE_FINISHED, CHECK_QUEST_ITEM(QUEST_MEDALLION_FIRE)) &&
         !Flags_GetInfTable(INFTABLE_GORON_CITY_DOORS_UNLOCKED)) {
         Flags_SetInfTable(INFTABLE_GORON_CITY_DOORS_UNLOCKED);
@@ -24,7 +22,7 @@ static void OnInitGCDoor(void* refActor) {
 }
 
 void RegisterGoronTunicFix() {
-    COND_ID_HOOK(OnActorInit, ACTOR_BG_SPOT18_SHUTTER, CVAR_GC_DOOR_FIX_VALUE, OnInitGCDoor);
+    COND_ID_HOOK(OnActorInit, ACTOR_BG_SPOT18_SHUTTER, !IS_RANDO && CVAR_GC_DOOR_FIX_VALUE, OnInitGCDoor);
 }
 
-static RegisterShipInitFunc initFunc(RegisterGoronTunicFix, { CVAR_GC_DOOR_FIX_NAME });
+static RegisterShipInitFunc initFunc(RegisterGoronTunicFix, { CVAR_GC_DOOR_FIX_NAME, "IS_RANDO" });
