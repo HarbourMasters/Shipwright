e4532f55830a78594461e46f6d48fb7b8f46a362
JordanLongstaff <jrdnlngstff@gmail.com>
2026-01-14 23:32:45 -0500

    Format

--- soh/soh/Enhancements/TimeSavers/SkipCutscene/Story/SkipOwlTravel.cpp
+++ soh/soh/Enhancements/TimeSavers/SkipCutscene/Story/SkipOwlTravel.cpp
@@ -18,16 +18,16 @@ static s16 GetEntranceIndex(s32 owlType) {
     s16 entranceIndex;
 
     switch (owlType) {
-    case 7:
-        entranceIndex = ENTR_HYRULE_FIELD_OWL_DROP;
-        break;
-    case 8:
-    case 9:
-        entranceIndex = ENTR_KAKARIKO_VILLAGE_OWL_DROP;
-        break;
-    default:
-        assert(0);
-        return ENTR_MAX;
+        case 7:
+            entranceIndex = ENTR_HYRULE_FIELD_OWL_DROP;
+            break;
+        case 8:
+        case 9:
+            entranceIndex = ENTR_KAKARIKO_VILLAGE_OWL_DROP;
+            break;
+        default:
+            assert(0);
+            return ENTR_MAX;
     }
 
     if (IS_RANDO && Randomizer_GetSettingValue(RSK_SHUFFLE_OWL_DROPS)) {
@@ -46,9 +46,9 @@ static void SkipOwlTravel(s32 owlType) {
 static void RegisterSkipOwlTravel() {
     COND_VB_SHOULD(VB_PLAY_OWL_TRAVEL_CS, CVAR_VALUE || IS_RANDO, {
         s32 owlType = va_arg(args, s32);
-    SkipOwlTravel(owlType);
-    *should = false;
-        });
+        SkipOwlTravel(owlType);
+        *should = false;
+    });
 }
 
 static RegisterShipInitFunc initFunc(RegisterSkipOwlTravel, { CVAR_NAME, "IS_RANDO" });
