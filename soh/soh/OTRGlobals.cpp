062b4b3298e5e5707bacf5b54766649466c808b7
Shishu the Dragon <183069616+ShishuTheDragon@users.noreply.github.com>
2026-01-30 12:55:28 +1300

    Fix segfault on quit

--- soh/soh/OTRGlobals.cpp
+++ soh/soh/OTRGlobals.cpp
@@ -1556,6 +1556,7 @@ extern "C" void DeinitOTR() {
     // Destroying gui here because we have shared ptrs to LUS objects which output to SPDLOG which is destroyed before
     // these shared ptrs.
     SohGui::Destroy();
+    sohFast3dWindow = nullptr;
 
     OTRGlobals::Instance->context = nullptr;
 }
