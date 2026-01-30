316c4e521c43e542d440066476cb9326a3e47a44
Anthony Barricelli <anthonyjbarricelli@gmail.com>
2026-01-30 18:59:57 -0500

    formatting

--- soh/soh/SohGui/SohMenuEnhancements.cpp
+++ soh/soh/SohGui/SohMenuEnhancements.cpp
@@ -1697,7 +1697,7 @@ void SohMenu::AddMenuEnhancements() {
             info.isHidden = !CVarGetInteger(CVAR_ENHANCEMENT("RandomizedEnemies"), 0);
         })
         .Options(CheckboxOptions().Tooltip("Dogs will be randomized and can replace enemies."))
-        .Callback([](WidgetInfo& info) { GetSelectedEnemies();});
+        .Callback([](WidgetInfo& info) { GetSelectedEnemies(); });
     AddWidget(path, "Enemy List", WIDGET_SEPARATOR).PreFunc([](WidgetInfo& info) {
         info.isHidden = !CVarGetInteger(CVAR_ENHANCEMENT("RandomizedEnemies"), 0);
     });
