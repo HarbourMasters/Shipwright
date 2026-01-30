6f45347890a49e1843450fffba568c6a694b0830
xxAtrain223 <xxAtrain223@users.noreply.github.com>
2026-01-29 21:04:49 -0600

    Ran clang format.

--- soh/soh/Enhancements/randomizer/logic_expression.cpp
+++ soh/soh/Enhancements/randomizer/logic_expression.cpp
@@ -540,18 +540,23 @@ std::string LogicExpression::Impl::GetExprErrorContext() const {
 // This macro simplifies the insertion of functions into the functionAdapters map by
 // automatically converting the function pointer or lambda into a FunctionAdapter.
 // Usage: REGISTER_FUNCTION(functionName)
-#define REGISTER_FUNCTION(fn) { #fn, LogicExpression::Impl::RegisterFunction(#fn, fn) }
+#define REGISTER_FUNCTION(fn) \
+    { #fn, LogicExpression::Impl::RegisterFunction(#fn, fn) }
 
 #define REGISTER_FUNCTION_WITH_DEFAULTS(fn, ...) \
     { #fn, LogicExpression::Impl::RegisterFunctionWithDefaults(#fn, fn, std::make_tuple(__VA_ARGS__)) }
 
-#define REGISTER_LOGIC_FUNCTION(fn) { #fn, LogicExpression::Impl::RegisterLogicFunction(#fn, &Rando::Logic::fn) }
+#define REGISTER_LOGIC_FUNCTION(fn) \
+    { #fn, LogicExpression::Impl::RegisterLogicFunction(#fn, &Rando::Logic::fn) }
 
-#define REGISTER_LOGIC_FUNCTION_WITH_DEFAULTS(fn, ...) \
-    { #fn,                                             \
-      LogicExpression::Impl::RegisterLogicFunctionWithDefaults(#fn, &Rando::Logic::fn, std::make_tuple(__VA_ARGS__)) }
+#define REGISTER_LOGIC_FUNCTION_WITH_DEFAULTS(fn, ...)                                              \
+    {                                                                                               \
+#fn, LogicExpression::Impl::RegisterLogicFunctionWithDefaults(#fn, &Rando::Logic::fn,       \
+                                                                      std::make_tuple(__VA_ARGS__)) \
+    }
 
-#define REGISTER_LOGIC_VARIABLE(var) { #var, LogicExpression::Impl::RegisterLogicVariable(#var, &Rando::Logic::var) }
+#define REGISTER_LOGIC_VARIABLE(var) \
+    { #var, LogicExpression::Impl::RegisterLogicVariable(#var, &Rando::Logic::var) }
 
 #pragma region Forwarding Functions
 static uint8_t GetOption(const RandomizerSettingKey key) {
