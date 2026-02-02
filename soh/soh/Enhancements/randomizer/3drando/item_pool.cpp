aa35afcfaceaad2be3cc185ed4d15d83961b57b9
Christopher Leggett <chris@leggett.dev>
2026-02-02 17:49:29 -0500

    Fix calculation for heart pieces/containers to add.
    This calc ends up with 8 heart containers in the pool with the default 3 starting hearts, just like the 8 heart containers in the vanilla game. Changing starting hearts results in half of the remaining hearts being filled by heart containers and half of the remaining hearts being filled by heart pieces.

--- soh/soh/Enhancements/randomizer/3drando/item_pool.cpp
+++ soh/soh/Enhancements/randomizer/3drando/item_pool.cpp
@@ -871,9 +871,13 @@ void GenerateItemPool() {
                     break;
                 case RO_ITEM_POOL_BALANCED: {
                     int heartsToPlace = maxHearts - startingHearts;
-                    int halfHearts = maxHearts >> 2;
+                    int halfHearts = heartsToPlace / 2;
                     AddFixedItemToPool(RG_HEART_CONTAINER, heartsToPlace - halfHearts, false);
                     AddFixedItemToPool(RG_PIECE_OF_HEART, halfHearts * 4, false);
+                    if (halfHearts % 2 == 1) {
+                        // If startingHearts was odd, we only have enough for 19 hearts at this point, add 4 extra heart pieces
+                        AddFixedItemToPool(RG_PIECE_OF_HEART, 4, false);
+                    }
                     break;
                 }
                 case RO_ITEM_POOL_SCARCE:
