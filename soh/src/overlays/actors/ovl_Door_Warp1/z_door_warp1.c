7190a7a12430ac3f6a5cfe3976408728224c1c08
Christopher Leggett <chris@leggett.dev>
2026-02-02 11:59:55 -0500

    Copied what 2ship did for z_door_warp1.
    Not 100% sure if it's correct but it looks right.

--- soh/src/overlays/actors/ovl_Door_Warp1/z_door_warp1.c
+++ soh/src/overlays/actors/ovl_Door_Warp1/z_door_warp1.c
@@ -1036,7 +1036,7 @@ void DoorWarp1_DrawWarp(DoorWarp1* this, PlayState* play) {
     gSPSegment(POLY_XLU_DISP++, 0x08,
                Gfx_TwoTexScrollEx(play->state.gfxCtx, 0, spEC & 0xFF, -((s16)(this->unk_19C + this->unk_19C) & 511),
                                   0x100, 0x100, 1, spEC & 0xFF, -((s16)(this->unk_19C + this->unk_19C) & 511), 0x100,
-                                  0x100, 1, -1, 1, -1));
+                                  0x100, 10, -2, 10, -2));
 
     Matrix_Translate(0.0f, this->unk_194 * 230.0f, 0.0f, MTXMODE_APPLY);
     xzScale = (((f32)this->unk_1AE * spE8) / 100.0f) + 1.0f;
@@ -1072,7 +1072,7 @@ void DoorWarp1_DrawWarp(DoorWarp1* this, PlayState* play) {
 
         gSPSegment(POLY_XLU_DISP++, 0x08,
                    Gfx_TwoTexScrollEx(play->state.gfxCtx, 0, spEC & 0xFF, -((s16)this->unk_19C & 511), 0x100, 0x100, 1,
-                                      spEC & 0xFF, -((s16)this->unk_19C & 511), 0x100, 0x100, 1, -1, 1, -1));
+                                      spEC & 0xFF, -((s16)this->unk_19C & 511), 0x100, 0x100, 10, -1, 10, -1));
 
         Matrix_Translate(0.0f, this->unk_198 * 60.0f, 0.0f, MTXMODE_APPLY);
 
