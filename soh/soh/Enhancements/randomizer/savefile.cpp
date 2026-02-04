c7eb2119ba52ee56032f21458396fece71b50a16
anthony-barricelli <104381450+anthony-barricelli@users.noreply.github.com>
2026-02-03 21:59:23 -0500

    feedback -- use enum instead of arbitrary int
    Co-authored-by: Philip Dubé <serprex@users.noreply.github.com>

--- soh/soh/Enhancements/randomizer/savefile.cpp
+++ soh/soh/Enhancements/randomizer/savefile.cpp
@@ -163,7 +163,7 @@ void SetStartingItems() {
 
     if (Randomizer_GetSettingValue(RSK_STARTING_OCARINA)) {
         INV_CONTENT(ITEM_OCARINA_FAIRY) =
-            Randomizer_GetSettingValue(RSK_STARTING_OCARINA) == 1 ? ITEM_OCARINA_FAIRY : ITEM_OCARINA_TIME;
+            Randomizer_GetSettingValue(RSK_STARTING_OCARINA) == RO_STARTING_OCARINA_FAIRY ? ITEM_OCARINA_FAIRY : ITEM_OCARINA_TIME;
     }
 
     if (Randomizer_GetSettingValue(RSK_STARTING_STICKS) && !Randomizer_GetSettingValue(RSK_SHUFFLE_DEKU_STICK_BAG)) {
