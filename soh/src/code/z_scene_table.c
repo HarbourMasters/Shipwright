11f90557b656243a4bef2e001b81ba24a7713b7a
red <tachatz214@gmail.com>
2026-02-18 20:55:50 -0600

    fix clang format

--- soh/src/code/z_scene_table.c
+++ soh/src/code/z_scene_table.c
@@ -1579,13 +1579,13 @@ void func_8009FE58(PlayState* play) {
 
         if (play->pauseCtx.state == 0) {
             if (CVarGetInteger(CVAR_SETTING("A11yNoJabuWobble"), 0) == 0) {
-                View_SetDistortionOrientation(&play->view,
-                                          ((360.00018f / 65535.0f) * (M_PI / 180.0f)) * temp * Math_CosS(D_8012A39C),
-                                          ((360.00018f / 65535.0f) * (M_PI / 180.0f)) * temp * Math_SinS(D_8012A39C),
-                                          ((360.00018f / 65535.0f) * (M_PI / 180.0f)) * temp * Math_SinS(D_8012A3A0));
+                View_SetDistortionOrientation(
+                    &play->view, ((360.00018f / 65535.0f) * (M_PI / 180.0f)) * temp * Math_CosS(D_8012A39C),
+                    ((360.00018f / 65535.0f) * (M_PI / 180.0f)) * temp * Math_SinS(D_8012A39C),
+                    ((360.00018f / 65535.0f) * (M_PI / 180.0f)) * temp * Math_SinS(D_8012A3A0));
                 View_SetDistortionScale(&play->view, 1.f + (0.79999995f * temp * Math_SinS(D_8012A3A0)),
-                                    1.f + (0.39999998f * temp * Math_CosS(D_8012A3A0)),
-                                    1.f + (1 * temp * Math_CosS(D_8012A39C)));
+                                        1.f + (0.39999998f * temp * Math_CosS(D_8012A3A0)),
+                                        1.f + (1 * temp * Math_CosS(D_8012A39C)));
                 View_SetDistortionSpeed(&play->view, 0.95f);
             }
         }
@@ -1623,7 +1623,7 @@ void func_8009FE58(PlayState* play) {
             Matrix_Scale(1.005f, sinf(D_8012A398) * 0.8f, 1.005f, MTXMODE_NEW);
         }
     } else {
-        Matrix_Scale(1.0f, 1.0f, 1.0f, MTXMODE_NEW);   
+        Matrix_Scale(1.0f, 1.0f, 1.0f, MTXMODE_NEW);
     }
 
     gSPSegment(POLY_OPA_DISP++, 0x0D, MATRIX_NEWMTX(play->state.gfxCtx));
