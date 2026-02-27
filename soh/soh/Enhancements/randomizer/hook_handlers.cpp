982adfe29885862a54a3eca970626e1e47a1f71e
Jerom Venneker <jerom.venneker@gmail.com>
2026-02-27 14:45:35 +0100

    added hook for malon return from castle

--- soh/soh/Enhancements/randomizer/hook_handlers.cpp
+++ soh/soh/Enhancements/randomizer/hook_handlers.cpp
@@ -932,6 +932,9 @@ void RandomizerOnVanillaBehaviorHandler(GIVanillaBehavior id, bool* should, va_l
                 *should = true;
             }
             break;
+        case VB_MALON_RETURN_FROM_CASTLE:
+            *should = Flags_GetEventChkInf(EVENTCHKINF_TALON_RETURNED_FROM_CASTLE) && Flags_GetEventChkInf(EVENTCHKINF_OBTAINED_POCKET_EGG);
+            break;
         case VB_MIDO_CONSIDER_DEKU_TREE_DEAD:
             *should = Flags_GetEventChkInf(EVENTCHKINF_OBTAINED_KOKIRI_EMERALD_DEKU_TREE_DEAD);
             break;
