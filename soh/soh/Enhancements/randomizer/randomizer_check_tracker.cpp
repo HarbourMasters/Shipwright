68545660e8c693e5ea94b7812a6b519b8f87b988
Philip Dubé <serprex@users.noreply.github.com>
2026-03-23 19:20:31 +0000

    nit

--- soh/soh/Enhancements/randomizer/randomizer_check_tracker.cpp
+++ soh/soh/Enhancements/randomizer/randomizer_check_tracker.cpp
@@ -1085,8 +1085,7 @@ void CheckTrackerWindow::DrawElement() {
             if (checkSearch.Draw("", ImGui::GetContentRegionAvail().x - 42)) {
                 UpdateFilters();
             }
-            std::string checkSearchText = "";
-            checkSearchText = checkSearch.InputBuf;
+            std::string checkSearchText = checkSearch.InputBuf;
             checkSearchText.erase(std::remove(checkSearchText.begin(), checkSearchText.end(), ' '),
                                   checkSearchText.end());
             ImGui::SameLine();
