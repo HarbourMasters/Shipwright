bdeead4dd985c2003ea3fe794214005302159c4a
Eric Hoey <121978037+A-Green-Spoon@users.noreply.github.com>
2026-03-31 10:02:49 -0400

    mq ganon logic fix

--- soh/soh/Enhancements/randomizer/location_access/dungeons/ganons_castle.cpp
+++ soh/soh/Enhancements/randomizer/location_access/dungeons/ganons_castle.cpp
@@ -566,7 +566,7 @@ void RegionTable_Init_GanonsCastle() {
         EVENT_ACCESS(LOGIC_SHADOW_TRIAL_FIRST_CHEST, logic->CanDetonateUprightBombFlower()),
     }, {
         //Locations
-        LOCATION(RC_GANONS_CASTLE_MQ_WONDER_SHADOW_TRIAL, (logic->CanDetonateBombFlowers() || (ctx->GetTrickOption(RT_BLUE_FIRE_MUD_WALLS) && logic->CanUse(RG_BOTTLE_WITH_BLUE_FIRE))) && (logic->EffectiveHealth() != 1 || logic->CanUse(RG_NAYRUS_LOVE))),
+        LOCATION(RC_GANONS_CASTLE_MQ_WONDER_SHADOW_TRIAL, (logic->CanDetonateBombFlowers() || (ctx->GetTrickOption(RT_BLUE_FIRE_MUD_WALLS) && logic->CanUse(RG_BOTTLE_WITH_BLUE_FIRE))) && (logic->TakeDamage() || logic->CanUse(RG_NAYRUS_LOVE))),
     }, {
         //Exits
         ENTRANCE(RR_GANONS_CASTLE_MQ_SHADOW_TRIAL_CHEST_PLATFORM, logic->IsAdult || logic->CanUse(RG_HOOKSHOT) || logic->CanUse(RG_HOVER_BOOTS)),
