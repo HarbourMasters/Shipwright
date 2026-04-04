e9ab9519a3641db2150adaa62671964cdd49149b
Paul Schwabauer <paul@schwabauer.co>
2026-04-04 17:19:59 +0200

    Revert "Fix UBSan insufficient space error in system initialization"
    This reverts commit a03a841f20796a13c2ce78d5a0fe35e883faf1be.

--- soh/src/code/code_800FC620.c
+++ soh/src/code/code_800FC620.c
@@ -11,7 +11,7 @@ typedef struct InitFunc {
 } InitFunc;
 
 // .data
-void* sInitFuncs[2] = { NULL, NULL };
+void* sInitFuncs = NULL;
 
 char sNew[] = { 'n', 'e', 'w' };
 
@@ -190,7 +190,7 @@ void func_800FCB34(void) {
         prev = initFunc;
     }
 
-    sInitFuncs[0] = prev;
+    sInitFuncs = prev;
 }
 
 void SystemHeap_Init(void* start, size_t size) {
