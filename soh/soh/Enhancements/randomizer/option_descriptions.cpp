b5d81f82c52b60646653d24f743c9b5c659d275d
Eric Hoey <121978037+A-Green-Spoon@users.noreply.github.com>
2026-04-05 16:57:44 -0400

    opt desc typo

--- soh/soh/Enhancements/randomizer/option_descriptions.cpp
+++ soh/soh/Enhancements/randomizer/option_descriptions.cpp
@@ -317,7 +317,7 @@ void Settings::CreateOptionDescriptions() {
         "Icicles will have a halo around them when carrying randomized items.";
     mOptionDescriptions[RSK_SHUFFLE_RED_ICE] =
         "Red Ice will give randomized items when melted.\n"
-        "Red Ice will have a particle effect inside when it hold a randomized item";
+        "Red Ice will have a particle effect inside it when it holds a randomized item";
     mOptionDescriptions[RSK_SHUFFLE_SIGNS] = "Signs and readable pedestals, plinths, altars, and graves will grant a "
                                              "randomized item the first time they are read. "
                                              "Signs will have a particle effect when they hold a randomized item.\n"
