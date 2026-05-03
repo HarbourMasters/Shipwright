7a522b7ac1227b0e446129c0491e61fb14e464b5
Pepe20129 <72659707+Pepe20129@users.noreply.github.com>
2026-05-03 22:35:30 +0200

    Update OTRGlobals.cpp

--- soh/soh/OTRGlobals.cpp
+++ soh/soh/OTRGlobals.cpp
@@ -791,7 +791,7 @@ void OTRGlobals::Initialize() {
     auto logLevel =
         static_cast<spdlog::level::level_enum>(CVarGetInteger(CVAR_DEVELOPER_TOOLS("LogLevel"), defaultLogLevel));
     context->InitLogging(logLevel, logLevel);
-    Ship::Context::GetInstance()->GetLogger()->set_pattern("[%H:%M:%S.%e] [%s:%#] [%l] %v");
+    Ship::Context::GetInstance()->GetLogger()->set_pattern("[%H:%M:%S.%e] [%s:%#] [%^%l%$] %v");
 
     context->InitGfxDebugger();
     context->InitFileDropMgr();
