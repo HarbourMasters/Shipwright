96662c0d5b64f09e298f818d3f2bd7127387d8fe
Unreference <87878910+unreference@users.noreply.github.com>
2026-05-14 00:01:49 -0700

    fix: Move flowerPos initialization to Init

--- soh/src/overlays/actors/ovl_En_Dnt_Nomal/z_en_dnt_nomal.c
+++ soh/src/overlays/actors/ovl_En_Dnt_Nomal/z_en_dnt_nomal.c
@@ -157,6 +157,11 @@ void EnDntNomal_Init(Actor* thisx, PlayState* play) {
     } else {
         Actor_Kill(&this->actor);
     }
+
+    // https://github.com/HarbourMasters/Shipwright/issues/2796
+    // Default flowerPos to the actor's spawn position so the flower doesn't render at the origin if the draw function
+    // is enabled before SetFlower position (which waits for ground contact) has a chance to set the real position.
+    this->flowerPos = this->actor.world.pos;
     this->actionFunc = EnDntNomal_WaitForObject;
 }
 
@@ -190,10 +195,6 @@ void EnDntNomal_WaitForObject(EnDntNomal* this, PlayState* play) {
             this->actor.draw = EnDntNomal_DrawStageScrub;
         }
 
-        // https://github.com/HarbourMasters/Shipwright/issues/2796
-        // Draw is now enabled but SetFlower hasn't run yet (it waits for ground contact).  Default flowerPos to the
-        // actor's spawn position so the flower doesn't render at the origin for the intervening frames.
-        this->flowerPos = this->actor.world.pos;
         this->actionFunc = EnDntNomal_SetFlower;
     }
 }
