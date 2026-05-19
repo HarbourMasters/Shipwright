bb5e30260556701e91dba98b1fd210d954baf5d3
Unreference <87878910+unreference@users.noreply.github.com>
2026-05-18 17:46:05 -0700

    chore(enemy-randomizer): Clang format

--- soh/soh/Enhancements/ExtraModes/EnemyRandomizer.cpp
+++ soh/soh/Enhancements/ExtraModes/EnemyRandomizer.cpp
@@ -533,8 +533,7 @@ void CustomStalfosPairFightDestroy(Actor* thisx, PlayState* play) {
 
 static void KillVoidedEnemies(void* actorRef) {
     constexpr float voidFallDistance = 200.0f;
-    if (const auto actor = static_cast<Actor*>(actorRef); actor->category == ACTORCAT_ENEMY &&
-                                                          !actor->isDrawn &&
+    if (const auto actor = static_cast<Actor*>(actorRef); actor->category == ACTORCAT_ENEMY && !actor->isDrawn &&
                                                           actor->world.pos.y < actor->home.pos.y - voidFallDistance) {
         Actor_Kill(actor);
     }
