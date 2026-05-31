df33c9786775f8768d4feda8131b964e961fa348
BassFace2761 <cuylerjh@gmail.com>
2026-05-30 23:14:14 -0400

    fix: formatting error in GIVB

--- soh/soh/Enhancements/game-interactor/vanilla-behavior/GIVanillaBehavior.h
+++ soh/soh/Enhancements/game-interactor/vanilla-behavior/GIVanillaBehavior.h
@@ -607,6 +607,15 @@ typedef enum {
     // true
     // ```
     // #### `args`
+    // - `*Player`
+    // - `*PlayState`
+    VB_DROP_Z_TARGET,
+
+    // #### `result`
+    // ```c
+    // true
+    // ```
+    // #### `args`
     // - Player*
     VB_EMPTYING_BOTTLE,
 
@@ -2966,15 +2975,6 @@ typedef enum {
     // #### `args`
     // - `*int32_t (camId)`
     VB_SHOULD_LOAD_BG_IMAGE
-
-        // #### `result`
-        // ```c
-        // true
-        // ```
-        // #### `args`
-        // - `*Player`
-        // - `*PlayState`
-        VB_DROP_Z_TARGET,
 } GIVanillaBehavior;
 
 #endif
