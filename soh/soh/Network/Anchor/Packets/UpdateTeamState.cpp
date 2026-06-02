703e02d7a1102c4ce08bbce6ef916522195ad22a
Amber Burton <parchmentwolf@gmail.com>
2026-06-02 02:29:40 -0400

    Update UpdateTeamState.cpp
    No idea if this fixed it or not... Should be merging progress instead of overwriting 1 version on weird timings.

--- soh/soh/Network/Anchor/Packets/UpdateTeamState.cpp
+++ soh/soh/Network/Anchor/Packets/UpdateTeamState.cpp
@@ -170,7 +170,7 @@ void Anchor::HandlePacket_UpdateTeamState(nlohmann::json payload) {
         }
 
         for (int i = 0; i < 14; i++) {
-            gSaveContext.eventChkInf[i] = loadedData.eventChkInf[i];
+            gSaveContext.eventChkInf[i] |= loadedData.eventChkInf[i];
         }
 
         for (int i = 0; i < 4; i++) {
@@ -179,7 +179,7 @@ void Anchor::HandlePacket_UpdateTeamState(nlohmann::json payload) {
 
         // Skip last row of infTable, don't want to sync swordless flag
         for (int i = 0; i < 29; i++) {
-            gSaveContext.infTable[i] = loadedData.infTable[i];
+            gSaveContext.infTable[i] |= loadedData.infTable[i];
         }
 
         for (int i = 0; i < ceil((RAND_INF_MAX + 15) / 16); i++) {
