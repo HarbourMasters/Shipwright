6be02cfe4b2bb2d9118c684c37e6058930c1eafe
Jordan Longstaff <JordanLongstaff@users.noreply.github.com>
2026-06-06 20:50:10 -0400

    Fix formatting

--- soh/soh/Enhancements/ArrowCycle.cpp
+++ soh/soh/Enhancements/ArrowCycle.cpp
@@ -82,10 +82,9 @@ static ArrowType GetArrowTypeForArrow(s8 itemAction) {
 static bool CanCycleArrows() {
     Player* player = GET_PLAYER(gPlayState);
 
-    return (LINK_IS_ADULT || CVarGetInteger(CVAR_CHEAT("TimelessEquipment"), 0)) &&
-           !gSaveContext.minigameState && gPlayState->sceneNum != SCENE_SHOOTING_GALLERY &&
-           !(player->stateFlags1 & PLAYER_STATE1_ON_HORSE) && player->rideActor == NULL &&
-           INV_CONTENT(SLOT_BOW) == ITEM_BOW &&
+    return (LINK_IS_ADULT || CVarGetInteger(CVAR_CHEAT("TimelessEquipment"), 0)) && !gSaveContext.minigameState &&
+           gPlayState->sceneNum != SCENE_SHOOTING_GALLERY && !(player->stateFlags1 & PLAYER_STATE1_ON_HORSE) &&
+           player->rideActor == NULL && INV_CONTENT(SLOT_BOW) == ITEM_BOW &&
            (INV_CONTENT(ITEM_ARROW_FIRE) == ITEM_ARROW_FIRE || INV_CONTENT(ITEM_ARROW_ICE) == ITEM_ARROW_ICE ||
             INV_CONTENT(ITEM_ARROW_LIGHT) == ITEM_ARROW_LIGHT);
 }
