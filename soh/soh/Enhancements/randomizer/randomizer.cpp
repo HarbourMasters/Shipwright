1f7cd709dacf0da078dc5e1d3cf1b5fac903128a
Christopher Flanagan <chris@flana.com>
2026-06-10 09:59:47 -0400

    Single bombchu bag mode should give refills

--- soh/soh/Enhancements/randomizer/randomizer.cpp
+++ soh/soh/Enhancements/randomizer/randomizer.cpp
@@ -428,9 +428,9 @@ ItemObtainability Randomizer::GetItemObtainabilityFromRandomizerGet(RandomizerGe
                 case RO_BOMBCHU_BAG_NONE:
                     return CANT_OBTAIN_MISC;
                 case RO_BOMBCHU_BAG_SINGLE:
-                    return INV_CONTENT(ITEM_BOMBCHU) == ITEM_BOMBCHU
-                               ? (infiniteUpgrades != RO_INF_UPGRADES_OFF ? CAN_OBTAIN : CANT_OBTAIN_ALREADY_HAVE)
-                               : CAN_OBTAIN;
+                    // In Single mode, the Bombchu Bag is always obtainable.
+                    // The first just grants the bag,
+                    return CAN_OBTAIN;
                 case RO_BOMBCHU_BAG_PROGRESSIVE:
                     if (Flags_GetRandomizerInf(RAND_INF_HAS_INFINITE_BOMBCHUS)) {
                         return CANT_OBTAIN_ALREADY_HAVE;
