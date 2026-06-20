5962c5a49279eb3da6db4234d7e94fda1f8bb4cf
Jonathan Peros <jperos@jenova.games>
2026-06-20 02:41:32 -0400

    Hide Rupee Dash Wallet Scaling with Rupee Dash mode

--- soh/soh/SohGui/SohMenuEnhancements.cpp
+++ soh/soh/SohGui/SohMenuEnhancements.cpp
@@ -1662,6 +1662,7 @@ void SohMenu::AddMenuEnhancements() {
         .Options(CheckboxOptions().Tooltip("Rupees reduce over time, Link suffers damage when the count hits 0."));
     AddWidget(path, "Rupee Dash Wallet Scaling", WIDGET_CVAR_CHECKBOX)
         .CVar(CVAR_ENHANCEMENT("RupeeDashScaling"))
+        .PreFunc([](WidgetInfo& info) { info.isHidden = CVarGetInteger(CVAR_ENHANCEMENT("RupeeDash"), 0) == 0; })
         .Options(CheckboxOptions().DefaultValue(true).Tooltip("The larger Link's wallet, the faster Rupees reduce."));
     AddWidget(path, "Rupee Dash Interval %d seconds", WIDGET_CVAR_SLIDER_INT)
         .CVar(CVAR_ENHANCEMENT("RupeeDashInterval"))
