39b38720fd76c549239e6ef238c318911dc9a8cf
Eric Hoey <121978037+A-Green-Spoon@users.noreply.github.com>
2026-07-07 20:54:33 -0400

    refactor to rm oob, fix scale, add masks, adj item names

--- soh/soh/Enhancements/randomizer/randomizer.cpp
+++ soh/soh/Enhancements/randomizer/randomizer.cpp
@@ -1034,6 +1034,8 @@ static std::unordered_map<RandomizerGet, GameplayStatTimestamp> randomizerGetToS
     { RG_HYLIA_LAB_KEY, TIMESTAMP_FOUND_HYLIA_LAB_KEY },
     { RG_FISHING_HOLE_KEY, TIMESTAMP_FOUND_FISHING_HOLE_KEY },
 
+    { RG_GREG_RUPEE, TIMESTAMP_FOUND_GREG },
+
     { RG_CHILD_WALLET, TIMESTAMP_FOUND_CHILD_WALLET },
     { RG_TYCOON_WALLET, TIMESTAMP_FOUND_TYCOON_WALLET },
 
@@ -1077,24 +1079,27 @@ void Randomizer_GameplayStats_SetTimestamp(uint16_t item) {
         time = 1;
     }
 
-    if (gSaveContext.ship.stats.itemTimestamp[item] == 0) {
-        if (item == RG_GANONS_CASTLE_BOSS_KEY) {
-            gSaveContext.ship.stats.itemTimestamp[ITEM_KEY_BOSS] = time;
-        } else if (item == RG_MASTER_SWORD) {
-            gSaveContext.ship.stats.itemTimestamp[ITEM_SWORD_MASTER] = time;
-        } else if (item >= RG_EMPTY_BOTTLE && item <= RG_BOTTLE_WITH_BIG_POE) {
-            gSaveContext.ship.stats.itemTimestamp[ITEM_BOTTLE] = time;
-        } else if ((item >= RG_BOMBCHU_5 && item <= RG_BOMBCHU_20) || item == RG_PROGRESSIVE_BOMBCHU_BAG) {
-            gSaveContext.ship.stats.itemTimestamp[ITEM_BOMBCHU] = time;
-        } else if (item == RG_MAGIC_SINGLE) {
-            gSaveContext.ship.stats.itemTimestamp[ITEM_SINGLE_MAGIC] = time;
-        } else if (item == RG_DOUBLE_DEFENSE) {
-            gSaveContext.ship.stats.itemTimestamp[ITEM_DOUBLE_DEFENSE] = time;
-        }
+    int16_t timestampItem = -1;
+    if (item == RG_GANONS_CASTLE_BOSS_KEY) {
+        timestampItem = ITEM_KEY_BOSS;
+    } else if (item == RG_MASTER_SWORD) {
+        timestampItem = ITEM_SWORD_MASTER;
+    } else if (item >= RG_EMPTY_BOTTLE && item <= RG_BOTTLE_WITH_BIG_POE) {
+        timestampItem = ITEM_BOTTLE;
+    } else if ((item >= RG_BOMBCHU_5 && item <= RG_BOMBCHU_20) || item == RG_PROGRESSIVE_BOMBCHU_BAG) {
+        timestampItem = ITEM_BOMBCHU;
+    } else if (item == RG_MAGIC_SINGLE) {
+        timestampItem = ITEM_SINGLE_MAGIC;
+    } else if (item == RG_DOUBLE_DEFENSE) {
+        timestampItem = ITEM_DOUBLE_DEFENSE;
+    } else if (item >= RG_KEATON_MASK && item <= RG_MASK_OF_TRUTH) {
+        timestampItem = ITEM_MASK_KEATON + (item - RG_KEATON_MASK);
+    } else if (randomizerGetToStatsTimeStamp.contains((RandomizerGet)item)) {
+        timestampItem = randomizerGetToStatsTimeStamp[(RandomizerGet)item];
     }
 
-    if (randomizerGetToStatsTimeStamp.contains((RandomizerGet)item)) {
-        gSaveContext.ship.stats.itemTimestamp[randomizerGetToStatsTimeStamp[(RandomizerGet)item]] = time;
+    if (timestampItem != -1 && gSaveContext.ship.stats.itemTimestamp[timestampItem] == 0) {
+        gSaveContext.ship.stats.itemTimestamp[timestampItem] = time;
     }
 }
 
@@ -1139,7 +1144,7 @@ extern "C" u16 Randomizer_Item_Give(PlayState* play, GetItemEntry giEntry) {
             Rupees_ChangeBy(99);
         } else if (item == RG_GREG_RUPEE) {
             Rupees_ChangeBy(1);
-            gSaveContext.ship.stats.itemTimestamp[TIMESTAMP_FOUND_GREG] = static_cast<u32>(GAMEPLAYSTAT_TOTAL_TIME);
+            // gSaveContext.ship.stats.itemTimestamp[TIMESTAMP_FOUND_GREG] = static_cast<u32>(GAMEPLAYSTAT_TOTAL_TIME);
         }
 
         return Return_Item_Entry(giEntry, RG_NONE);
