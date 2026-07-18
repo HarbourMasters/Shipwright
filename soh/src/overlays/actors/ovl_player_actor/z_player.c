da70c0d0be24160dd79c1a3b41910d001d115393
djevangelia <djevangelia>
2026-07-19 00:30:37 +0200

    remove comment

--- soh/src/overlays/actors/ovl_player_actor/z_player.c
+++ soh/src/overlays/actors/ovl_player_actor/z_player.c
@@ -5721,7 +5721,6 @@ s32 func_8083A6AC(Player* this, PlayState* play) {
             sp54 = Math3D_UDistPlaneToPos(nx, ny, nz, sp84->dist, &this->actor.world.pos);
 
             sp50 = (sPrevFloorProperty == 6);
-            // Shuffle climb check
             if (GameInteractor_Should(VB_START_CLIMB_FROM_ABOVE,
                                       (!sp50 && (func_80041DB8(&play->colCtx, sp84, sp80) & 8)))) {
                 sp50 = 1;
