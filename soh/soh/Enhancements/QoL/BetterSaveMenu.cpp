d011b182df94a2de33bc6e8ef5919858cb00c09d
Christopher Leggett <chris@leggett.dev>
2026-07-18 18:03:55 -0400

    clang-format run

--- soh/soh/Enhancements/QoL/BetterSaveMenu.cpp
+++ soh/soh/Enhancements/QoL/BetterSaveMenu.cpp
@@ -26,41 +26,41 @@ static CustomMessage continueDungeonMsg =
 extern "C" uint8_t Randomizer_GetSettingValue(RandomizerSettingKey randoSettingKey);
 
 bool IsSceneDungeon(uint8_t scene) {
-  bool result = false;
-  switch (scene) {
-    case SCENE_DEKU_TREE:
-    case SCENE_DEKU_TREE_BOSS:
-    case SCENE_DODONGOS_CAVERN:
-    case SCENE_DODONGOS_CAVERN_BOSS:
-    case SCENE_JABU_JABU:
-    case SCENE_JABU_JABU_BOSS:
-    case SCENE_FOREST_TEMPLE:
-    case SCENE_FOREST_TEMPLE_BOSS:
-    case SCENE_FIRE_TEMPLE:
-    case SCENE_FIRE_TEMPLE_BOSS:
-    case SCENE_WATER_TEMPLE:
-    case SCENE_WATER_TEMPLE_BOSS:
-    case SCENE_SPIRIT_TEMPLE:
-    case SCENE_SPIRIT_TEMPLE_BOSS:
-    case SCENE_SHADOW_TEMPLE:
-    case SCENE_SHADOW_TEMPLE_BOSS:
-    case SCENE_BOTTOM_OF_THE_WELL:
-    case SCENE_GERUDO_TRAINING_GROUND:
-    case SCENE_ICE_CAVERN:
-    case SCENE_INSIDE_GANONS_CASTLE:
-    case SCENE_GANONS_TOWER:
-    case SCENE_GANONS_TOWER_COLLAPSE_EXTERIOR:
-    case SCENE_GANONS_TOWER_COLLAPSE_INTERIOR:
-    case SCENE_GANONDORF_BOSS:
-    case SCENE_GANON_BOSS:
-    case SCENE_INSIDE_GANONS_CASTLE_COLLAPSE:
-      result = true;
-      break;
-    default:
-      result = false;
-      break;
-  }
-  return result;
+    bool result = false;
+    switch (scene) {
+        case SCENE_DEKU_TREE:
+        case SCENE_DEKU_TREE_BOSS:
+        case SCENE_DODONGOS_CAVERN:
+        case SCENE_DODONGOS_CAVERN_BOSS:
+        case SCENE_JABU_JABU:
+        case SCENE_JABU_JABU_BOSS:
+        case SCENE_FOREST_TEMPLE:
+        case SCENE_FOREST_TEMPLE_BOSS:
+        case SCENE_FIRE_TEMPLE:
+        case SCENE_FIRE_TEMPLE_BOSS:
+        case SCENE_WATER_TEMPLE:
+        case SCENE_WATER_TEMPLE_BOSS:
+        case SCENE_SPIRIT_TEMPLE:
+        case SCENE_SPIRIT_TEMPLE_BOSS:
+        case SCENE_SHADOW_TEMPLE:
+        case SCENE_SHADOW_TEMPLE_BOSS:
+        case SCENE_BOTTOM_OF_THE_WELL:
+        case SCENE_GERUDO_TRAINING_GROUND:
+        case SCENE_ICE_CAVERN:
+        case SCENE_INSIDE_GANONS_CASTLE:
+        case SCENE_GANONS_TOWER:
+        case SCENE_GANONS_TOWER_COLLAPSE_EXTERIOR:
+        case SCENE_GANONS_TOWER_COLLAPSE_INTERIOR:
+        case SCENE_GANONDORF_BOSS:
+        case SCENE_GANON_BOSS:
+        case SCENE_INSIDE_GANONS_CASTLE_COLLAPSE:
+            result = true;
+            break;
+        default:
+            result = false;
+            break;
+    }
+    return result;
 }
 
 void HandleSaveMenu(bool* should, PlayState* play) {
@@ -81,9 +81,9 @@ void HandleSaveMenu(bool* should, PlayState* play) {
                     Play_PerformSave(play);
                     pauseCtx->unk_1EC = 4;
                     if (IsSceneDungeon(gSaveContext.savedSceneNum)) {
-                      Message_StartTextbox(play, TEXT_CONTINUE_DUNGEON_MSG, NULL);
+                        Message_StartTextbox(play, TEXT_CONTINUE_DUNGEON_MSG, NULL);
                     } else {
-                      Message_StartTextbox(play, TEXT_CONTINUE_OVERWORLD_MSG, NULL);
+                        Message_StartTextbox(play, TEXT_CONTINUE_OVERWORLD_MSG, NULL);
                     }
                 } else {
                     Interface_SetDoAction(play, DO_ACTION_NONE);
@@ -125,12 +125,12 @@ void HandleSaveMenu(bool* should, PlayState* play) {
                         Play_SaveSceneFlags(play);
                         Sram_OpenSave();
                         if (!IsSceneDungeon(gSaveContext.savedSceneNum)) {
-                          if (IS_RANDO && Randomizer_GetSettingValue(RSK_SHUFFLE_OVERWORLD_SPAWNS)) {
-                            if (LINK_AGE_IN_YEARS == YEARS_ADULT) {
-                              gSaveContext.entranceIndex = ENTR_HYRULE_FIELD_10;
+                            if (IS_RANDO && Randomizer_GetSettingValue(RSK_SHUFFLE_OVERWORLD_SPAWNS)) {
+                                if (LINK_AGE_IN_YEARS == YEARS_ADULT) {
+                                    gSaveContext.entranceIndex = ENTR_HYRULE_FIELD_10;
+                                }
+                                gSaveContext.entranceIndex = Entrance_OverrideNextIndex(gSaveContext.entranceIndex);
                             }
-                            gSaveContext.entranceIndex = Entrance_OverrideNextIndex(gSaveContext.entranceIndex);
-                          }
                         }
                         pauseCtx->promptChoice = 0;
                         pauseCtx->unk_1EC = 7;
