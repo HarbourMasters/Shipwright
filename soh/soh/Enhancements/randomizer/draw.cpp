64edd9cb50448d7d19cccf68b5735a88aab2e156
TheLynk <jeanpol4226@gmail.com>
2026-08-05 20:59:37 +0200

    Update Model

--- soh/soh/Enhancements/randomizer/draw.cpp
+++ soh/soh/Enhancements/randomizer/draw.cpp
@@ -1175,43 +1175,18 @@ extern "C" void Randomizer_DrawKneePads(PlayState* play, GetItemEntry* getItemEn
 }
 
 extern "C" void Randomizer_DrawRollAbility(PlayState* play, GetItemEntry* getItemEntry) {
-    static bool initialized = false;
-    static SkelAnime skelAnime;
-    static Vec3s jointTable[ROLL_ANIMATION_LIMBS];
-    static Vec3s morphTable[ROLL_ANIMATION_LIMBS];
-    static u32 lastUpdate = 0;
-    static int currentFrame = 0;
-
-    if (!initialized) {
-        initialized = true;
-        SkelAnime_InitFlex(play, &skelAnime, (FlexSkeletonHeader*)gLinkChildSkel, NULL, jointTable, morphTable,
-                           ROLL_ANIMATION_LIMBS);
-    }
-
-    // Manual animation with captured frames
-    if (lastUpdate != play->state.frames) {
-        lastUpdate = play->state.frames;
-
-        // Advances one frame every 2 frames of play
-        if ((play->state.frames % 2) == 0) {
-            currentFrame = (currentFrame + 1) % ROLL_ANIMATION_FRAMES;
-        }
-
-        // Copies data from current frame
-        for (int i = 0; i < ROLL_ANIMATION_LIMBS; i++) {
-            jointTable[i] = rollAnimationData[currentFrame][i];
-        }
-    }
-
     OPEN_DISPS(play->state.gfxCtx);
 
     Gfx_SetupDL_25Opa(play->state.gfxCtx);
 
-    Matrix_Translate(0.0f, -30.0f, 0.0f, MTXMODE_APPLY);
+    // rotation lente pour le faire tourner comme un get-item
     Matrix_RotateY(play->gameplayFrames * 0.05f, MTXMODE_APPLY);
-    Matrix_Scale(0.01f, 0.01f, 0.01f, MTXMODE_APPLY);
+    // à ajuster : échelle + position de ton modèle
+    Matrix_Scale(10.00f, 10.00f, 10.00f, MTXMODE_APPLY);
 
-    SkelAnime_DrawFlexOpa(play, skelAnime.skeleton, jointTable, skelAnime.dListCount, NULL, NULL, NULL);
+    gSPMatrix(POLY_OPA_DISP++, Matrix_NewMtx(play->state.gfxCtx, (char*)__FILE__, __LINE__),
+              G_MTX_MODELVIEW | G_MTX_LOAD);
+    gSPDisplayList(POLY_OPA_DISP++, (Gfx*)gGiRollDL);
 
     CLOSE_DISPS(play->state.gfxCtx);
 }
