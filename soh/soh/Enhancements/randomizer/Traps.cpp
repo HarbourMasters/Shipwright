dd3e3a5a4cd51ca9ac9e4d47f3b2e5caf5d9fe73
Christopher Leggett <chris@leggett.dev>
2026-08-06 16:49:21 -0400

    Run clang format

--- soh/soh/Enhancements/randomizer/Traps.cpp
+++ soh/soh/Enhancements/randomizer/Traps.cpp
@@ -668,8 +668,8 @@ static void InitTrickNames() {
         Text{ "Force Gem" },
     };
     trickNameTable[RG_ROCS_FEATHER] = {
-        Text{ "Chicken Wing", "Chicken Wing", "Chicken Wing" }, // "Chicken Wing"
-        Text{ "Roc's Leg", "Roc's Leg", "Roc's Leg" },          // "Roc's Leg"
+        Text{ "Chicken Wing", "Chicken Wing", "Chicken Wing" },    // "Chicken Wing"
+        Text{ "Roc's Leg", "Roc's Leg", "Roc's Leg" },             // "Roc's Leg"
         Text{ "Roc's Flapper", "Roc's Flapper", "Roc's Flapper" }, // "Roc's Flapper"
     };
     trickNameTable[RG_DEATH_MOUNTAIN_CRATER_BEAN_SOUL] = {
