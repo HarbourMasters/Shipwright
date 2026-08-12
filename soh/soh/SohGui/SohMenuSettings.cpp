f5897b8f24a634684ed6b2a4b58f8ba7dbfa19ed
PurpleHato <linkvssangoku.jr@gmail.com>
2026-08-12 17:03:00 +0200

    clang, as usual, for the 3000rd time

--- soh/soh/SohGui/SohMenuSettings.cpp
+++ soh/soh/SohGui/SohMenuSettings.cpp
@@ -481,9 +481,9 @@ void SohMenu::AddMenuSettings() {
         ImGui::SeparatorText("Thank You");
         ImGui::PopStyleColor();
         ImGui::SameLine();
-        ImTextureID heartTextureId = std::dynamic_pointer_cast<Fast::Fast3dGui>(
-                                         Ship::Context::GetRawInstance()->GetWindow()->GetGui())
-                                         ->GetTextureByName((const char*)gQuestIconHeartContainerTex);
+        ImTextureID heartTextureId =
+            std::dynamic_pointer_cast<Fast::Fast3dGui>(Ship::Context::GetRawInstance()->GetWindow()->GetGui())
+                ->GetTextureByName((const char*)gQuestIconHeartContainerTex);
         ImGui::Image(heartTextureId, ImVec2(25.0f, 25.0f));
         ImGui::TextWrapped("Special thanks to our contributors, playtesters, artists, moderators, helpers, and "
                            "everyone in the larger decomp & N64 communities who make this project possible.\n\n");
