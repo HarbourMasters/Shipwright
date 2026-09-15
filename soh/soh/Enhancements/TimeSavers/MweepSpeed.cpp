759d9606029436f19211ab5538c6781c50f446d4
Jordan Longstaff <JordanLongstaff@users.noreply.github.com>
2026-09-15 11:57:17 -0400

    Make MweepSpeed hook conditional

--- soh/soh/Enhancements/TimeSavers/MweepSpeed.cpp
+++ soh/soh/Enhancements/TimeSavers/MweepSpeed.cpp
@@ -8,17 +8,20 @@ extern "C" {
 void EnKz_Mweep(EnKz* enKz, PlayState* play);
 }
 
+#define CVAR_SPEED_NAME CVAR_ENHANCEMENT("MweepSpeed")
+#define CVAR_SPEED CVarGetFloat(CVAR_SPEED_NAME, 1.0f)
+
 static void EnKz_SetSpeed(void* actorPtr) {
     EnKz* enKz = (EnKz*)actorPtr;
     if (enKz->actionFunc != EnKz_Mweep) {
         return;
     }
 
-    enKz->actor.speedXZ = 0.1f * CVarGetFloat(CVAR_ENHANCEMENT("MweepSpeed"), 1.0f);
+    enKz->actor.speedXZ = 0.1f * CVAR_SPEED;
 }
 
 static void RegisterMweepSpeed() {
-    COND_ID_HOOK(OnActorUpdate, ACTOR_EN_KZ, true, EnKz_SetSpeed);
+    COND_ID_HOOK(OnActorUpdate, ACTOR_EN_KZ, CVAR_SPEED != 1.0f, EnKz_SetSpeed);
 }
 
-static RegisterShipInitFunc initFunc(RegisterMweepSpeed);
+static RegisterShipInitFunc initFunc(RegisterMweepSpeed, { CVAR_SPEED_NAME });
