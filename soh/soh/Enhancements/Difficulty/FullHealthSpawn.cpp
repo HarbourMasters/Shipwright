1c599a56cc0d026f60edef5b5a2f1aebf389a218
Pepper0ni <93387759+Pepper0ni@users.noreply.github.com>
2026-09-22 22:37:20 +0100

    forgot to remove something

--- soh/soh/Enhancements/Difficulty/FullHealthSpawn.cpp
+++ soh/soh/Enhancements/Difficulty/FullHealthSpawn.cpp
@@ -3,14 +3,12 @@
 #include "soh/Enhancements/game-interactor/GameInteractor.h"
 #include "soh/Enhancements/game-interactor/vanilla-behavior/GIVanillaBehavior.h"
 #include "soh/ShipInit.hpp"
-#include "soh/Enhancements/enhancementTypes.h"
 #include "variables.h"
 
 extern "C" {
 extern PlayState* gPlayState;
 }
 
-static constexpr DampeDropRate CVAR_DAMPE_DROP_RATE_DEFAULT = DAMPE_NORMAL;
 #define CVAR_FULL_HEALTH_SPAWN_NAME CVAR_ENHANCEMENT("FullHealthSpawn")
 #define CVAR_FULL_HEALTH_SPAWN_VALUE CVarGetInteger(CVAR_FULL_HEALTH_SPAWN_NAME, 0)
 
