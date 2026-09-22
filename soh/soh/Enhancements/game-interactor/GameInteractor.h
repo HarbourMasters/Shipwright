d30fc192f2eb01ceea45bd1e12de61636cafbf86
David Racine <bass_dr@hotmail.com>
2026-09-21 22:40:47 -0400

    Include <version> before checking __cpp_lib_source_location (#7233)
    GCC 15's libstdc++ defines feature-test macros selectively: after
    #include <string>, __cpp_lib_source_location is not yet defined, even
    though __builtin_source_location is fully supported. This made the
    check fail on GCC 15 toolchains (including clang using libstdc++),
    disabling the Hook Debugger.
    
    Include <version> first so all feature-test macros are defined.

--- soh/soh/Enhancements/game-interactor/GameInteractor.h
+++ soh/soh/Enhancements/game-interactor/GameInteractor.h
@@ -89,6 +89,7 @@ void GameInteractor_SetTriforceHuntCreditsWarpActive(bool state);
 #include <cstring>
 #include <string>
 
+#include <version>
 #ifdef __cpp_lib_source_location
 #include <source_location>
 #else
